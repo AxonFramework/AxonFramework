a7d200d64e9156059532cbd8571f6638e8e8ae60
Allard Buijze <buijze@gmail.com>
2010-02-01 13:03:09 +0000

    Added query repository implementation

---
+++ sample/addressbook/app/src/main/java/org/axonframework/sample/app/query/ContactRepositoryImpl.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2010. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.sample.app.query;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author Allard Buijze
+ */
+public class ContactRepositoryImpl implements ContactRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    public List<ContactEntry> findAllContacts() {
+        return entityManager.createQuery("SELECT e FROM ContactEntry e")
+                .setMaxResults(250)
+                .getResultList();
+    }
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    public List<AddressEntry> findAllAddressesForContact(UUID contactIdentifier) {
+        return entityManager.createQuery("SELECT e FROM AddressEntry e WHERE e.identifier = :id")
+                .setParameter("id", contactIdentifier)
+                .setMaxResults(10)
+                .getResultList();
+    }
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    public List<AddressEntry> findAllAddressesInCity(String name, String city) {
+        return entityManager.createQuery("SELECT e FROM AddressEntry e WHERE e.name LIKE :name AND e.city LIKE :city")
+                .setParameter("name", "%" + name + "%")
+                .setParameter("name", "%" + city + "%")
+                .setMaxResults(250)
+                .getResultList();
+    }
+}
