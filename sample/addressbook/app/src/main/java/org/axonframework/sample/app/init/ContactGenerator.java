9db68a60db03aa2ee1ee4e8af4057ce2897370a5
Jettro Coenradie <jettro@gridshore.nl>
2010-11-11 11:02:35 +0000

    Sample spring mvc : Improved javadoc for data initialisation

--- sample/addressbook/app/src/main/java/org/axonframework/sample/app/init/ContactGenerator.java
+++ sample/addressbook/app/src/main/java/org/axonframework/sample/app/init/ContactGenerator.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,6 +28,8 @@ import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
+ * <p>Uses the sample api to create some sample content</p>
+ *
  * @author Allard Buijze
  */
 public class ContactGenerator implements ApplicationListener {
