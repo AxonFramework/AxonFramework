088433703e320a94c807a0d6f3bad9a123ecad62
Allard Buijze <buijze@gmail.com>
2011-01-01 19:30:29 +0000

    Fixes issue #106 Removed deprecated reference to CommandContext in the javadoc

--- core/src/main/java/org/axonframework/commandhandling/CommandCallback.java
+++ core/src/main/java/org/axonframework/commandhandling/CommandCallback.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2010. Axon Framework
+ * Copyright (c) 2011. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,26 +20,23 @@ package org.axonframework.commandhandling;
  * Interface describing a callback that is invoked when command handler execution has finished. Depending of the outcome
  * of the execution, either the {@link #onSuccess(Object)} or the {@link #onFailure(Throwable)} is called.
  *
- * @author Allard Buijze
  * @param <R> the type of result of the command handling
+ * @author Allard Buijze
  * @since 0.6
  */
 public interface CommandCallback<R> {
 
     /**
-     * Invoked when command handling execution was successful. The given <code>context</code> may contain values added
-     * by the interceptors acting on the incoming command.
+     * Invoked when command handling execution was successful.
      *
      * @param result The result of the command handling execution, if any.
      */
     void onSuccess(R result);
 
     /**
-     * Invoked when command handling execution resulted in an error.  The given <code>context</code> may contain values
-     * added by the interceptors acting on the incoming command.
+     * Invoked when command handling execution resulted in an error.
      *
      * @param cause The exception raised during command handling
      */
     void onFailure(Throwable cause);
-
 }
