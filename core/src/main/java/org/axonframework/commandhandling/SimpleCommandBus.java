c36ad1a44820705e5166bb45d8672194e72aafb4
Allard Buijze <buijze@gmail.com>
2011-05-20 08:22:19 +0000

    Update issue #160 Code in the SimpleCommandBus could be simplified

--- core/src/main/java/org/axonframework/commandhandling/SimpleCommandBus.java
+++ core/src/main/java/org/axonframework/commandhandling/SimpleCommandBus.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2010. Axon Framework
+ * Copyright (c) 2010-2011. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -119,24 +119,13 @@ public class SimpleCommandBus implements CommandBus {
             if (rollbackAttribute.rollBackOn(throwable)) {
                 unitOfWork.rollback(throwable);
             } else {
-                commitUnitOfWork(unitOfWork);
+                unitOfWork.commit();
             }
             throw throwable;
         }
 
-        commitUnitOfWork(unitOfWork);
+        unitOfWork.commit();
         return returnValue;
-    }
-
-    private void commitUnitOfWork(UnitOfWork unitOfWork) throws Throwable {
-        try {
-            unitOfWork.commit();
-        } catch (Throwable throwable) {
-            if (unitOfWork.isStarted()) {
-                unitOfWork.rollback(throwable);
-            }
-            throw throwable;
-        }
     }
 
     /**
