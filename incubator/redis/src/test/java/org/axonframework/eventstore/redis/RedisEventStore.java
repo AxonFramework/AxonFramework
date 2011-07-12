3de4a9a47b3a906afb79b7c507cea1f398060148
Allard Buijze <buijze@gmail.com>
2011-07-12 09:16:32 +0000

    Added work-in-progress for redis event store to incubator module

---
+++ incubator/redis/src/test/java/org/axonframework/eventstore/redis/RedisEventStore.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2010-2011. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.eventstore.redis;
+
+import org.axonframework.domain.AggregateIdentifier;
+import org.axonframework.domain.DomainEvent;
+import org.axonframework.domain.DomainEventStream;
+import org.axonframework.eventstore.EventSerializer;
+import org.axonframework.eventstore.EventStore;
+import org.axonframework.repository.ConcurrencyException;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.PipelineBlock;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author Allard Buijze
+ */
+public class RedisEventStore implements EventStore {
+
+    private EventSerializer eventSerializer;
+    private static final Charset UTF8 = Charset.forName("UTF-8");
+    private RedisConnectionProvider redisConnectionProvider;
+
+    @Override
+    public void appendEvents(String type, final DomainEventStream events) {
+        Jedis jedis = redisConnectionProvider.newConnection();
+        DomainEvent firstEvent = events.peek();
+        final byte[] key = (type + "." + firstEvent.getAggregateIdentifier().asString()).getBytes(UTF8);
+        jedis.watch(key);
+        Long eventCount = jedis.llen(key);
+        if ((firstEvent.getSequenceNumber() != 0 && eventCount == null)
+                || !firstEvent.getSequenceNumber().equals(eventCount)) {
+            jedis.unwatch();
+            throw new ConcurrencyException(
+                    String.format("Concurrent modification detected for Aggregate identifier [%s], sequence: [%s]",
+                                  firstEvent.getAggregateIdentifier(),
+                                  firstEvent.getSequenceNumber().toString()));
+        }
+
+        try {
+            jedis.pipelined(new PipelineBlock() {
+                @Override
+                public void execute() {
+                    multi();
+                    while (events.hasNext()) {
+                        DomainEvent domainEvent = events.next();
+
+                        rpush(new String(key, UTF8), new String(eventSerializer.serialize(domainEvent), UTF8));
+                    }
+                    exec();
+                }
+            });
+//            redis.clients.jedis.Transaction transaction = jedis.multi();
+//            while (events.hasNext()) {
+//                DomainEvent domainEvent = events.next();
+//
+//                transaction.rpush(new String(key, UTF8), new String(eventSerializer.serialize(domainEvent), UTF8));
+//            }
+//            transaction.exec();
+        } finally {
+            redisConnectionProvider.closeConnection(jedis);
+        }
+    }
+
+    @Override
+    public DomainEventStream readEvents(String type, AggregateIdentifier identifier) {
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+
+    public void setEventSerializer(EventSerializer eventSerializer) {
+        this.eventSerializer = eventSerializer;
+    }
+
+    public void setRedisConnectionProvider(RedisConnectionProvider redisConnectionProvider) {
+        this.redisConnectionProvider = redisConnectionProvider;
+    }
+}
---
+++ incubator/redis/src/test/java/org/axonframework/eventstore/redis/RedisEventStoreBenchmark.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2010-2011. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.eventstore.redis;
+
+import org.apache.commons.pool.impl.GenericObjectPool;
+import org.axonframework.domain.AggregateIdentifier;
+import org.axonframework.domain.UUIDAggregateIdentifier;
+import org.axonframework.eventstore.XStreamEventSerializer;
+import org.axonframework.integrationtests.eventstore.benchmark.AbstractEventStoreBenchmark;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * @author Allard Buijze
+ */
+public class RedisEventStoreBenchmark extends AbstractEventStoreBenchmark {
+
+    private RedisEventStore redisEventStore;
+
+    public static void main(String[] args) throws Exception {
+        AbstractEventStoreBenchmark benchmark = new RedisEventStoreBenchmark();
+        benchmark.startBenchMark();
+    }
+
+    @Override
+    protected void prepareEventStore() {
+        redisEventStore = new RedisEventStore();
+        redisEventStore.setEventSerializer(new XStreamEventSerializer());
+        PooledRedisConnectionProvider redisConnectionProvider = new PooledRedisConnectionProvider();
+        redisEventStore.setRedisConnectionProvider(redisConnectionProvider);
+        Jedis conn = redisConnectionProvider.newConnection();
+        conn.flushAll();
+        redisConnectionProvider.closeConnection(conn);
+    }
+
+    @Override
+    protected Runnable getRunnableInstance() {
+        return new RedisBenchmark();
+    }
+
+    private class RedisBenchmark implements Runnable {
+
+        @Override
+        public void run() {
+            final AggregateIdentifier aggregateId = new UUIDAggregateIdentifier();
+            int eventSequence = 0;
+            for (int t = 0; t < getTransactionCount(); t++) {
+                eventSequence = saveAndLoadLargeNumberOfEvents(aggregateId, redisEventStore, eventSequence);
+            }
+        }
+    }
+
+    private class PooledRedisConnectionProvider implements RedisConnectionProvider {
+
+        private JedisPool pool;
+
+        private PooledRedisConnectionProvider() {
+            GenericObjectPool.Config poolConfig = new GenericObjectPool.Config();
+            poolConfig.maxActive = RedisEventStoreBenchmark.this.getThreadCount();
+            poolConfig.minIdle = RedisEventStoreBenchmark.this.getThreadCount();
+            pool = new JedisPool(poolConfig, "192.168.56.101", 6379, 10000);
+        }
+
+        @Override
+        public Jedis newConnection() {
+            return pool.getResource();
+        }
+
+        public void closeConnection(Jedis toClose) {
+            pool.returnResource(toClose);
+        }
+    }
+}
