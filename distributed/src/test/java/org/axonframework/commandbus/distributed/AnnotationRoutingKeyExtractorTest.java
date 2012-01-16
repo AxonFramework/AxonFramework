fafb3fe385e61fcfcb76d4eb9393498c1cdf025f
Allard Buijze <buijze@gmail.com>
2012-01-16 18:56:09 +0100

    Additional tests

---
+++ distributed/src/test/java/org/axonframework/commandbus/distributed/AnnotationRoutingKeyExtractorTest.java
@@ -0,0 +1,14 @@
+package org.axonframework.commandbus.distributed;
+
+import org.junit.*;
+
+/**
+ * @author Allard Buijze
+ */
+public class AnnotationRoutingKeyExtractorTest {
+
+    @Test
+    public void testGetRoutingKey() throws Exception {
+
+    }
+}
---
+++ distributed/src/test/java/org/axonframework/commandbus/distributed/jgroups/JGroupsConnectorTest.java
@@ -0,0 +1,8 @@
+package org.axonframework.commandbus.distributed.jgroups;
+
+/**
+ * @author Allard Buijze
+ */
+public class JGroupsConnectorTest {
+
+}
