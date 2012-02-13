4119e84651d22c11b2d8797a0fe3879f528f8132
Allard Buijze <buijze@gmail.com>
2012-02-13 15:26:05 +0100

    Merged fixture bug fix from 1.3.x maintenance branch

---
+++ test/src/main/java/org/axonframework/test/MyAggregateDeletedEvent.java
@@ -0,0 +1,11 @@
+package org.axonframework.test;
+
+import org.axonframework.domain.AggregateDeletedEvent;
+import org.axonframework.domain.DomainEvent;
+
+/**
+ *
+ */
+public class MyAggregateDeletedEvent extends DomainEvent implements AggregateDeletedEvent{
+
+}
