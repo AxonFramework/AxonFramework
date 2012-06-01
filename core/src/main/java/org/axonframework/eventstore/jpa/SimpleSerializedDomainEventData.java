aa8bb018eed539ce5f65010a18f44bea11ffd4a3
Allard Buijze <buijze@gmail.com>
2012-06-01 16:58:18 +0200

    And again some more code style and javadoc

--- core/src/main/java/org/axonframework/eventstore/jpa/SimpleSerializedDomainEventData.java
+++ core/src/main/java/org/axonframework/eventstore/jpa/SimpleSerializedDomainEventData.java
@@ -51,9 +51,9 @@ public class SimpleSerializedDomainEventData implements SerializedDomainEventDat
      * @param payload             The serialized representation of the event
      * @param metaData            The serialized representation of the meta data
      */
-    public SimpleSerializedDomainEventData(String eventIdentifier, String aggregateIdentifier, long sequenceNumber,
-                                           String timestamp, String payloadType, String payloadRevision,
-                                           byte[] payload, byte[] metaData) { // NOSONAR
+    public SimpleSerializedDomainEventData(String eventIdentifier, String aggregateIdentifier, // NOSONAR - Long ctor
+                                           long sequenceNumber, String timestamp, String payloadType,
+                                           String payloadRevision, byte[] payload, byte[] metaData) { // NOSONAR
         this.eventIdentifier = eventIdentifier;
         this.aggregateIdentifier = aggregateIdentifier;
         this.sequenceNumber = sequenceNumber;
