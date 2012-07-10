c043613645531ccf8a6029b2e05f8c0e46ab91b1
Allard Buijze <buijze@gmail.com>
2012-07-10 20:21:24 +0200

    Added unit test for asynchronous Saga Manager

---
+++ core/src/test/java/org/axonframework/saga/annotation/AsyncAnnotatedSagaManagerTest.java
@@ -0,0 +1,214 @@
+package org.axonframework.saga.annotation;
+
+import org.axonframework.domain.EventMessage;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.saga.Saga;
+import org.axonframework.saga.repository.inmemory.InMemorySagaRepository;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.axonframework.domain.GenericEventMessage.asEventMessage;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Allard Buijze
+ */
+public class AsyncAnnotatedSagaManagerTest {
+
+    private AsyncAnnotatedSagaManager testSubject;
+    private EventBus eventBus;
+    private AsyncAnnotatedSagaManagerTest.StubInMemorySagaRepository sagaRepository;
+    private ExecutorService executorService;
+
+    @SuppressWarnings("unchecked")
+    @Before
+    public void setUp() {
+        eventBus = mock(EventBus.class);
+        testSubject = new AsyncAnnotatedSagaManager(eventBus, StubAsyncSaga.class);
+        sagaRepository = new StubInMemorySagaRepository();
+        testSubject.setSagaRepository(sagaRepository);
+        executorService = Executors.newCachedThreadPool();
+        testSubject.setExecutor(executorService);
+        testSubject.setProcessorCount(3);
+        testSubject.setBufferSize(1024 * 2);
+    }
+
+    @Test
+    public void testSingleSagaLifeCycle() throws InterruptedException {
+        testSubject.start();
+        assertEquals(0, sagaRepository.getKnownSagas());
+        List<EventMessage> publicationList = new ArrayList<EventMessage>();
+        createSimpleLifeCycle("one", "two", publicationList);
+        for (EventMessage message : publicationList) {
+            testSubject.handle(message);
+        }
+        testSubject.stop();
+        executorService.shutdown();
+        assertTrue("Service refused to stop in 1 second", executorService.awaitTermination(1, TimeUnit.SECONDS));
+        assertEquals("Incorrect known saga count", 1, sagaRepository.getKnownSagas());
+        assertEquals("Incorrect live saga count", 0, sagaRepository.getLiveSagas());
+    }
+
+    @Test
+    public void testMultipleDisconnectedSagaLifeCycle() throws InterruptedException {
+        testSubject.start();
+        assertEquals(0, sagaRepository.getKnownSagas());
+        List<EventMessage> publicationList = new ArrayList<EventMessage>();
+        for (int t=0;t<1000;t++) {
+            createSimpleLifeCycle("association-"+t, "newAssociation-"+t, publicationList);
+        }
+//        long t0 = System.currentTimeMillis();
+        for (EventMessage message : publicationList) {
+            testSubject.handle(message);
+        }
+        testSubject.stop();
+//        long t1 = System.currentTimeMillis();
+//        System.out.println("Time (ms): " + (t1-t0));
+        executorService.shutdown();
+        assertTrue("Service refused to stop in 1 second", executorService.awaitTermination(1, TimeUnit.SECONDS));
+        assertEquals("Incorrect known saga count", 1000, sagaRepository.getKnownSagas());
+        assertEquals("Incorrect live saga count", 0, sagaRepository.getLiveSagas());
+    }
+
+    private void createSimpleLifeCycle(String firstAssociation, String newAssociation, List<EventMessage> publicationList) {
+        publicationList.add(asEventMessage(new ForceCreateNewEvent(firstAssociation)));
+        publicationList.add(asEventMessage(new OptionallyCreateNewEvent(firstAssociation)));
+        publicationList.add(asEventMessage(new UpdateEvent(firstAssociation)));
+        publicationList.add(asEventMessage(new AddAssociationEvent(firstAssociation, newAssociation)));
+        publicationList.add(asEventMessage(new OptionallyCreateNewEvent(newAssociation)));
+        publicationList.add(asEventMessage(new DeleteEvent(firstAssociation)));
+    }
+
+    @After
+    public void tearDown() {
+        testSubject.stop();
+    }
+
+    public static class StubAsyncSaga extends AbstractAnnotatedSaga {
+
+        private int optionallyNewInvocations;
+        private int forceNewInvocations;
+        private int updateInvocations;
+        private int deleteInvocations;
+
+        @StartSaga(forceNew = false)
+        @SagaEventHandler(associationProperty = "association")
+        public void handleOptionallyCreateNew(OptionallyCreateNewEvent event) {
+//            optionallyNewInvocations++;
+        }
+
+        @StartSaga(forceNew = true)
+        @SagaEventHandler(associationProperty = "association")
+        public void handleOptionallyCreateNew(ForceCreateNewEvent event) {
+//            optionallyNewInvocations++;
+        }
+
+        @SagaEventHandler(associationProperty = "association")
+        public void handleAddAssociation(AddAssociationEvent event) {
+//            updateInvocations++;
+            associateWith("association", event.getNewAssociation());
+        }
+
+        @SagaEventHandler(associationProperty = "association")
+        public void handleUpdate(UpdateEvent event) {
+//            updateInvocations++;
+        }
+
+        @EndSaga
+        @SagaEventHandler(associationProperty = "association")
+        public void handleDelete(DeleteEvent event) {
+//            deleteInvocations++;
+        }
+    }
+
+    private static class OptionallyCreateNewEvent extends AbstractSagaTestEvent {
+
+        private OptionallyCreateNewEvent(String association) {
+            super(association);
+        }
+    }
+
+    private static class ForceCreateNewEvent extends AbstractSagaTestEvent {
+
+        private ForceCreateNewEvent(String association) {
+            super(association);
+        }
+    }
+
+    private static class UpdateEvent extends AbstractSagaTestEvent {
+
+        private UpdateEvent(String association) {
+            super(association);
+        }
+    }
+
+    private static class DeleteEvent extends AbstractSagaTestEvent {
+
+        private DeleteEvent(String association) {
+            super(association);
+        }
+    }
+
+    private static class AddAssociationEvent extends AbstractSagaTestEvent {
+
+        private final String newAssociation;
+
+        private AddAssociationEvent(String association, String newAssociation) {
+            super(association);
+            this.newAssociation = newAssociation;
+        }
+
+        public String getNewAssociation() {
+            return newAssociation;
+        }
+    }
+
+    private static class AbstractSagaTestEvent {
+
+        private final String association;
+
+        private AbstractSagaTestEvent(String association) {
+            this.association = association;
+        }
+
+        public String getAssociation() {
+            return association;
+        }
+    }
+
+    private class StubInMemorySagaRepository extends InMemorySagaRepository {
+
+        private AtomicInteger knownSagas = new AtomicInteger();
+        private AtomicInteger liveSagas = new AtomicInteger();
+
+        @Override
+        public void commit(Saga saga) {
+            if (!saga.isActive()) {
+                liveSagas.decrementAndGet();
+            }
+            super.commit(saga);
+        }
+
+        @Override
+        public void add(Saga saga) {
+            knownSagas.incrementAndGet();
+            liveSagas.incrementAndGet();
+            super.add(saga);
+        }
+
+        public int getKnownSagas() {
+            return knownSagas.get();
+        }
+
+        public int getLiveSagas() {
+            return liveSagas.get();
+        }
+    }
+}
