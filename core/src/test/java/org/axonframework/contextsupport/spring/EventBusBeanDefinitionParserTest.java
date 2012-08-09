4fa6b6f67959bb33d64cdae8e60f9e7be186f0c5
Allard Buijze <buijze@gmail.com>
2012-08-09 16:25:31 +0200

    Added copyright statement where it was missing

--- core/src/test/java/org/axonframework/contextsupport/spring/EventBusBeanDefinitionParserTest.java
+++ core/src/test/java/org/axonframework/contextsupport/spring/EventBusBeanDefinitionParserTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.axonframework.contextsupport.spring;
 
 import org.axonframework.eventhandling.AutowiringClusterSelector;
