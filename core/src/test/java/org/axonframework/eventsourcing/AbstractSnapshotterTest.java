4caf02eba187bd673cea1f6255c8e55ca7e27fa2
Allard Buijze <buijze@gmail.com>
2013-10-30 13:42:43 +0100

    Prevent logging of exception when duplicate snapshots are created
    The Snapshotter will not log ConcurrencyExceptions any more, as they typically
    indicate that a snapshot already exists for the given aggregateId/seqNo.
    Instead, a one-liner is logged on info level.
    
    Issue #AXON-191 Fixed

--- core/src/test/java/org/axonframework/eventsourcing/AbstractSnapshotterTest.java
+++ core/src/test/java/org/axonframework/eventsourcing/AbstractSnapshotterTest.java
@@ -17,15 +17,23 @@
 package org.axonframework.eventsourcing;
 
 import org.axonframework.common.DirectExecutor;
+import org.axonframework.common.ReflectionUtils;
 import org.axonframework.domain.DomainEventMessage;
 import org.axonframework.domain.DomainEventStream;
 import org.axonframework.domain.GenericDomainEventMessage;
 import org.axonframework.domain.MetaData;
 import org.axonframework.domain.SimpleDomainEventStream;
 import org.axonframework.eventstore.SnapshotEventStore;
+import org.axonframework.repository.ConcurrencyException;
 import org.hamcrest.Matcher;
 import org.junit.*;
 import org.mockito.*;
+import org.mockito.invocation.*;
+import org.mockito.stubbing.*;
+import org.slf4j.Logger;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import static org.mockito.Mockito.*;
 
@@ -36,6 +44,8 @@ public class AbstractSnapshotterTest {
 
     private AbstractSnapshotter testSubject;
     private SnapshotEventStore mockEventStore;
+    private Logger logger;
+    private Logger originalLogger;
 
     @Before
     public void setUp() throws Exception {
@@ -54,6 +64,15 @@ public class AbstractSnapshotterTest {
         };
         testSubject.setEventStore(mockEventStore);
         testSubject.setExecutor(DirectExecutor.INSTANCE);
+        logger = mock(Logger.class);
+        originalLogger = replaceLogger(logger);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (originalLogger != null) {
+            replaceLogger(originalLogger);
+        }
     }
 
     @Test
@@ -67,6 +86,32 @@ public class AbstractSnapshotterTest {
                                                               "Mock contents", MetaData.emptyInstance())));
         testSubject.scheduleSnapshot("test", aggregateIdentifier);
         verify(mockEventStore).appendSnapshotEvent(eq("test"), argThat(event(aggregateIdentifier, 1)));
+    }
+
+    @Test
+    public void testScheduleSnapshot_ConcurrencyExceptionIsSilenced()
+            throws NoSuchFieldException, IllegalAccessException {
+        final Object aggregateIdentifier = "aggregateIdentifier";
+        doNothing()
+                .doThrow(new ConcurrencyException("Mock"))
+                .when(mockEventStore).appendSnapshotEvent(eq("test"), isA(DomainEventMessage.class));
+        when(mockEventStore.readEvents("test", aggregateIdentifier))
+                .thenAnswer(new Answer<Object>() {
+                    @Override
+                    public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                        return new SimpleDomainEventStream(
+                                new GenericDomainEventMessage<String>(aggregateIdentifier, (long) 0,
+                                                                      "Mock contents", MetaData.emptyInstance()),
+                                new GenericDomainEventMessage<String>(aggregateIdentifier, (long) 1,
+                                                                      "Mock contents", MetaData.emptyInstance()));
+                    }
+                });
+        testSubject.scheduleSnapshot("test", aggregateIdentifier);
+
+        testSubject.scheduleSnapshot("test", aggregateIdentifier);
+        verify(mockEventStore, times(2)).appendSnapshotEvent(eq("test"), argThat(event(aggregateIdentifier, 1)));
+        verify(logger, never()).warn(anyString());
+        verify(logger, never()).error(anyString());
     }
 
     @Test
@@ -111,5 +156,17 @@ public class AbstractSnapshotterTest {
             lastSequenceNumber = eventStream.next().getSequenceNumber();
         }
         return lastSequenceNumber;
+    }
+
+    private Logger replaceLogger(Logger mockLogger) throws NoSuchFieldException, IllegalAccessException {
+        Field loggerField = AbstractSnapshotter.class.getDeclaredField("logger");
+        ReflectionUtils.ensureAccessible(loggerField);
+
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(loggerField, loggerField.getModifiers() & ~Modifier.FINAL);
+        Logger originalLogger = (Logger) loggerField.get(null);
+        loggerField.set(null, mockLogger);
+        return originalLogger;
     }
 }
