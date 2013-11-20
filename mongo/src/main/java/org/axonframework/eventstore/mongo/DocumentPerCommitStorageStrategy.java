90b57031021d7b5c21f04defeeef572ec1d8a553
Jochen Munz <jochen.munz@isar-software.com>
2013-11-20 12:06:15 +0100

    Added index to ensure replays on larger collections
    Ensuring additional index on the event collection to allow replays on mongo event stores.
    
    Issue #AXON-195 Fixed

--- mongo/src/main/java/org/axonframework/eventstore/mongo/DocumentPerCommitStorageStrategy.java
+++ mongo/src/main/java/org/axonframework/eventstore/mongo/DocumentPerCommitStorageStrategy.java
@@ -114,6 +114,11 @@ public class DocumentPerCommitStorageStrategy implements StorageStrategy {
                                              .append(CommitEntry.SEQUENCE_NUMBER_PROPERTY, 1),
                                      "uniqueAggregateIndex",
                                      true);
+
+        eventsCollection.ensureIndex(new BasicDBObject(CommitEntry.TIME_STAMP_PROPERTY, 1)
+                                             .append(CommitEntry.SEQUENCE_NUMBER_PROPERTY, 1),
+                                     "orderedEventStreamIndex",
+                                     false);
         snapshotsCollection.ensureIndex(new BasicDBObject(CommitEntry.AGGREGATE_IDENTIFIER_PROPERTY, 1)
                                              .append(CommitEntry.AGGREGATE_TYPE_PROPERTY, 1)
                                              .append(CommitEntry.SEQUENCE_NUMBER_PROPERTY, 1),
