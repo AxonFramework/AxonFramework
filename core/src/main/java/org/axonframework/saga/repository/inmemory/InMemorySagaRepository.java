ca70797f7a7bd5bcf58065505fb68735fe81d89c
Allard Buijze <buijze@gmail.com>
2014-04-09 15:25:19 +0200

    Updated copyright notice

--- core/src/main/java/org/axonframework/saga/repository/inmemory/InMemorySagaRepository.java
+++ core/src/main/java/org/axonframework/saga/repository/inmemory/InMemorySagaRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2013. Axon Framework
+ * Copyright (c) 2010-2014. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
