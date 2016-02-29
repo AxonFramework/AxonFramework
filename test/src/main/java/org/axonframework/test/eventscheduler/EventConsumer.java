AxonFramework/AxonFramework

commit 28ce88ef88150e731f0839e676b8d4e9843cdec1

Allow checked exceptions from all MessageHandlers
Checked exceptions raised by MessageHandlers are no longer wrapped by RuntimeExceptions but are instead passed on unmodified to the Unit of Work that is processing the message. This allows the RollbackConfiguration to determine whether or not to roll back the Unit of Work when an exception is raised.

core/src/main/java/org/axonframework/commandhandling/annotation/AggregateAnnotationCommandHandler.java
@@ -20,7 +20,6 @@ import org.axonframework.commandhandling.*;
 import org.axonframework.commandhandling.model.Aggregate;
 import org.axonframework.commandhandling.model.Repository;
 import org.axonframework.commandhandling.model.inspection.AggregateModel;
-import org.axonframework.commandhandling.model.inspection.CommandMessageHandler;
 import org.axonframework.commandhandling.model.inspection.ModelInspector;
 import org.axonframework.common.Assert;
 import org.axonframework.common.Registration;
@@ -42,7 +41,7 @@ import java.util.*;
  * @since 1.2
  */
 public class AggregateAnnotationCommandHandler<T> implements MessageHandler<CommandMessage<?>>,
-                                                                                   SupportedCommandNamesAware {
+        SupportedCommandNamesAware {
 
     private final Repository<T> repository;
 
@@ -71,7 +70,7 @@ public class AggregateAnnotationCommandHandler<T> implements MessageHandler<Comm
     public AggregateAnnotationCommandHandler(Class<T> aggregateType, Repository<T> repository,
                                              CommandTargetResolver commandTargetResolver) {
         this(aggregateType, repository, commandTargetResolver,
-             ClasspathParameterResolverFactory.forClass(aggregateType));
+                ClasspathParameterResolverFactory.forClass(aggregateType));
     }
 
     /**
@@ -88,7 +87,7 @@ public class AggregateAnnotationCommandHandler<T> implements MessageHandler<Comm
                                              CommandTargetResolver commandTargetResolver,
                                              ParameterResolverFactory parameterResolverFactory) {
         this(repository, commandTargetResolver,
-             ModelInspector.inspectAggregate(aggregateType, parameterResolverFactory));
+                ModelInspector.inspectAggregate(aggregateType, parameterResolverFactory));
     }
 
     /**
@@ -136,7 +135,7 @@ public class AggregateAnnotationCommandHandler<T> implements MessageHandler<Comm
         Map<String, MessageHandler<CommandMessage<?>>> handlersFound = new HashMap<>();
         AggregateCommandHandler aggregateCommandHandler = new AggregateCommandHandler();
         aggregateModel.commandHandlers().forEach((k, v) -> {
-            if (v instanceof CommandMessageHandler && ((CommandMessageHandler) v).isFactoryHandler()) {
+            if (v.isFactoryHandler()) {
                 handlersFound.put(k, new AggregateConstructorCommandHandler(v));
             } else {
                 handlersFound.put(k, aggregateCommandHandler);
@@ -180,9 +179,7 @@ public class AggregateAnnotationCommandHandler<T> implements MessageHandler<Comm
         @SuppressWarnings("unchecked")
         @Override
         public Object handle(CommandMessage<?> command, UnitOfWork<? extends CommandMessage<?>> unitOfWork) throws Exception {
-            Aggregate<T> aggregate = repository.newInstance(
-                    () -> (T) handler.handle(command, null)
-            );
+            Aggregate<T> aggregate = repository.newInstance(() -> (T) handler.handle(command, null));
             return resolveReturnValue(command, aggregate);
         }
     }
@@ -191,7 +188,7 @@ public class AggregateAnnotationCommandHandler<T> implements MessageHandler<Comm
 
         @SuppressWarnings("unchecked")
         @Override
-        public Object handle(CommandMessage<?> command, UnitOfWork<? extends CommandMessage<?>> unitOfWork) {
+        public Object handle(CommandMessage<?> command, UnitOfWork<? extends CommandMessage<?>> unitOfWork) throws Exception {
             VersionedAggregateIdentifier iv = commandTargetResolver.resolveTarget(command);
             return repository.load(iv.getIdentifier(), iv.getVersion()).handle(command);
         }

core/src/main/java/org/axonframework/commandhandling/disruptor/CommandHandlerInvoker.java
@@ -40,8 +40,8 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.WeakHashMap;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Supplier;
 
 /**
  * Component of the DisruptorCommandBus that invokes the command handler. The execution is done within a Unit Of Work.
@@ -198,7 +198,7 @@ public class CommandHandlerInvoker implements EventHandler<CommandHandlingEntry>
                 Object cachedItem = cache.get(aggregateIdentifier);
                 if (cachedItem != null && EventSourcedAggregate.class.isInstance(cachedItem)) {
                     EventSourcedAggregate<T> cachedAggregate = (EventSourcedAggregate<T>) cachedItem;
-                    aggregateRoot = cachedAggregate.map(r -> {
+                    aggregateRoot = cachedAggregate.invoke(r -> {
                         if (aggregateFactory.getAggregateType().isInstance(r)) {
                             return cachedAggregate;
                         } else {
@@ -215,7 +215,8 @@ public class CommandHandlerInvoker implements EventHandler<CommandHandlingEntry>
                     events = decorator.decorateForRead(aggregateIdentifier,
                                                        eventStore.readEvents(aggregateIdentifier));
                     if (events.hasNext()) {
-                        aggregateRoot = EventSourcedAggregate.initialize(aggregateFactory.createAggregate(aggregateIdentifier, events.peek()), model, eventBus, eventStore);
+                        aggregateRoot = EventSourcedAggregate.initialize(aggregateFactory.createAggregate(
+                                aggregateIdentifier, events.peek()), model, eventBus, eventStore);
                         aggregateRoot.initializeState(events);
                     }
                 } catch (EventStreamNotFoundException e) {
@@ -237,7 +238,7 @@ public class CommandHandlerInvoker implements EventHandler<CommandHandlingEntry>
         }
 
         @Override
-        public Aggregate<T> newInstance(Supplier<T> factoryMethod) {
+        public Aggregate<T> newInstance(Callable<T> factoryMethod) throws Exception {
             EventSourcedAggregate<T> aggregate = EventSourcedAggregate.initialize(factoryMethod, model,
                                                                                   eventBus, eventStore);
             firstLevelCache.put(aggregate, PLACEHOLDER_VALUE);

core/src/main/java/org/axonframework/commandhandling/disruptor/DisruptorCommandBus.java
@@ -25,7 +25,10 @@ import org.axonframework.common.Assert;
 import org.axonframework.common.AxonThreadFactory;
 import org.axonframework.common.Registration;
 import org.axonframework.eventhandling.EventBus;
-import org.axonframework.eventsourcing.*;
+import org.axonframework.eventsourcing.AggregateFactory;
+import org.axonframework.eventsourcing.DomainEventMessage;
+import org.axonframework.eventsourcing.DomainEventStream;
+import org.axonframework.eventsourcing.EventStreamDecorator;
 import org.axonframework.eventstore.EventStore;
 import org.axonframework.messaging.MessageDispatchInterceptor;
 import org.axonframework.messaging.MessageHandler;
@@ -37,7 +40,6 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
-import java.util.function.Supplier;
 
 import static java.lang.String.format;
 
@@ -361,7 +363,7 @@ public class DisruptorCommandBus implements CommandBus {
         }
 
         @Override
-        public Aggregate<T> newInstance(Supplier<T> factoryMethod) {
+        public Aggregate<T> newInstance(Callable<T> factoryMethod) throws Exception {
             return CommandHandlerInvoker.<T>getRepository(type).newInstance(factoryMethod);
         }
     }

core/src/main/java/org/axonframework/commandhandling/model/AbstractMessageHandler.java
@@ -88,7 +88,7 @@ public abstract class AbstractMessageHandler<T> implements MessageHandler<T>, An
     }
 
     @Override
-    public Object handle(Message<?> message, T target) {
+    public Object handle(Message<?> message, T target) throws Exception {
         try {
             if (executable instanceof Method) {
                 return ((Method) executable).invoke(target, resolveParameterValues(message));
@@ -98,10 +98,11 @@ public abstract class AbstractMessageHandler<T> implements MessageHandler<T>, An
                 throw new IllegalStateException("What kind of handler is this?");
             }
         } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
-            if (e.getCause() instanceof RuntimeException) {
-                throw (RuntimeException) e.getCause();
+            if (e.getCause() instanceof Exception) {
+                throw (Exception) e.getCause();
             }
-            throw new MessageHandlerInvocationException("Error invoking CommandHandler method", e.getCause());
+            throw new MessageHandlerInvocationException(
+                    String.format("Error handling an object of type [%s]", message.getPayloadType()), e);
         }
     }
 

core/src/main/java/org/axonframework/commandhandling/model/AbstractRepository.java
@@ -24,7 +24,7 @@ import org.axonframework.messaging.unitofwork.UnitOfWork;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Supplier;
+import java.util.concurrent.Callable;
 
 /**
  * Abstract implementation of the {@link Repository} that takes care of the dispatching of events when an aggregate is
@@ -57,7 +57,7 @@ public abstract class AbstractRepository<T, A extends Aggregate<T>> implements R
     }
 
     @Override
-    public A newInstance(Supplier<T> factoryMethod) {
+    public A newInstance(Callable<T> factoryMethod) throws Exception {
         A aggregate = doCreateNew(factoryMethod);
         aggregate.execute(root -> Assert.isTrue(aggregateType.isInstance(root),
                                                 "Unsuitable aggregate for this repository: wrong type"));
@@ -71,7 +71,7 @@ public abstract class AbstractRepository<T, A extends Aggregate<T>> implements R
         return aggregate;
     }
 
-    protected abstract A doCreateNew(Supplier<T> factoryMethod);
+    protected abstract A doCreateNew(Callable<T> factoryMethod) throws Exception;
 
     /**
      * {@inheritDoc}

core/src/main/java/org/axonframework/commandhandling/model/Aggregate.java
@@ -27,9 +27,9 @@ public interface Aggregate<T> {
 
     Long version();
 
-    Object handle(CommandMessage<?> msg);
+    Object handle(CommandMessage<?> msg) throws Exception;
 
-    <R> R map(Function<T, R> invocation);
+    <R> R invoke(Function<T, R> invocation);
 
     void execute(Consumer<T> invocation);
 

core/src/main/java/org/axonframework/commandhandling/model/AggregateLifecycle.java
@@ -22,6 +22,7 @@ import org.axonframework.messaging.unitofwork.UnitOfWork;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.Callable;
 import java.util.function.Supplier;
 
 public abstract class AggregateLifecycle {
@@ -69,17 +70,25 @@ public abstract class AggregateLifecycle {
 
     protected abstract <T> ApplyMore doApply(T payload, MetaData metaData);
 
-    protected <V> V executeWithResult(Supplier<V> callable) {
+    protected <V> V executeWithResultOrException(Callable<V> callable) throws Exception {
         AggregateLifecycle existing = CURRENT.get();
         CURRENT.set(this);
         try {
-            return callable.get();
+            return callable.call();
         } finally {
             if (existing == null) {
                 CURRENT.remove();
             } else {
                 CURRENT.set(existing);
             }
+        } 
+    }
+    
+    protected <V> V executeWithResult(Supplier<V> task) {
+        try {
+            return executeWithResultOrException(task::get);
+        } catch (Exception e) {
+            throw (RuntimeException) e;
         }
     }
 

core/src/main/java/org/axonframework/commandhandling/model/GenericJpaRepository.java
@@ -24,8 +24,7 @@ import org.axonframework.common.lock.NullLockFactory;
 import org.axonframework.eventhandling.EventBus;
 
 import javax.persistence.EntityManager;
-
-import java.util.function.Supplier;
+import java.util.concurrent.Callable;
 
 import static java.lang.String.format;
 
@@ -97,8 +96,8 @@ public class GenericJpaRepository<T> extends LockingRepository<T, AnnotatedAggre
     }
 
     @Override
-    protected AnnotatedAggregate<T> doCreateNewForLock(Supplier<T> factoryMethod) {
-        return new AnnotatedAggregate<>(factoryMethod.get(), aggregateModel(), eventBus);
+    protected AnnotatedAggregate<T> doCreateNewForLock(Callable<T> factoryMethod) throws Exception {
+        return new AnnotatedAggregate<>(factoryMethod.call(), aggregateModel(), eventBus);
     }
 
     @Override

commit b887e91f80864ce93df40a36f907bd09bff1dac0

Support MessageHandler that throws checked exceptions in saga tests.
Commit 28ce88ef88150e731f0839e676b8d4e9843cdec1 caused saga test fixture to break due to unchecked calls to MessageHandler. This commit adds "throws Exception" clause where required and intruduces a custom Consumer with a throws clause.

test/src/main/java/org/axonframework/test/eventscheduler/EventConsumer.java
@@ -1,0 +1,28 @@
+/*
+ * Copyright (c) 2010-2014. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.axonframework.test.eventscheduler;
+
+/**
+ * A consumer that allows implementations to throw a exceptions.
+ *
+ * @author Steve Chernyak
+ * @since 3.0
+ */
+@FunctionalInterface
+public interface EventConsumer<T> {
+
+    void accept(T var) throws Exception;
+}

test/src/main/java/org/axonframework/test/eventscheduler/StubEventScheduler.java
@@ -138,7 +138,7 @@ public class StubEventScheduler implements EventScheduler {
      * @param newDateTime The time to advance the "current time" of the scheduler to
      * @param eventConsumer The function to invoke for each event to trigger
      */
-    public void advanceTime(ZonedDateTime newDateTime, Consumer<EventMessage<?>> eventConsumer) {
+    public void advanceTime(ZonedDateTime newDateTime, EventConsumer<EventMessage<?>> eventConsumer) throws Exception {
         while (!scheduledEvents.isEmpty() && !scheduledEvents.first().getScheduleTime().isAfter(newDateTime)) {
             eventConsumer.accept(advanceToNextTrigger());
         }
@@ -154,7 +154,7 @@ public class StubEventScheduler implements EventScheduler {
      * @param duration The amount of time to advance the "current time" of the scheduler with
      * @param eventConsumer The function to invoke for each event to trigger
      */
-    public void advanceTime(Duration duration, Consumer<EventMessage<?>> eventConsumer) {
+    public void advanceTime(Duration duration, EventConsumer<EventMessage<?>> eventConsumer) throws Exception {
         advanceTime(currentDateTime.plus(duration), eventConsumer);
     }
 }

test/src/main/java/org/axonframework/test/saga/AnnotatedSagaTestFixture.java
@@ -89,7 +89,7 @@ public class AnnotatedSagaTestFixture implements FixtureConfiguration, Continued
     }
 
     @Override
-    public FixtureExecutionResult whenTimeElapses(Duration elapsedTime) {
+    public FixtureExecutionResult whenTimeElapses(Duration elapsedTime) throws Exception {
         try {
             fixtureExecutionResult.startRecording();
             eventScheduler.advanceTime(elapsedTime, sagaManager::handle);
@@ -100,7 +100,7 @@ public class AnnotatedSagaTestFixture implements FixtureConfiguration, Continued
     }
 
     @Override
-    public FixtureExecutionResult whenTimeAdvancesTo(ZonedDateTime newDateTime) {
+    public FixtureExecutionResult whenTimeAdvancesTo(ZonedDateTime newDateTime) throws Exception {
         try {
             fixtureExecutionResult.startRecording();
             eventScheduler.advanceTime(newDateTime, sagaManager::handle);
@@ -128,7 +128,7 @@ public class AnnotatedSagaTestFixture implements FixtureConfiguration, Continued
     }
 
     @Override
-    public ContinuedGivenState givenAPublished(Object event) {
+    public ContinuedGivenState givenAPublished(Object event) throws Exception {
         sagaManager.handle(GenericEventMessage.asEventMessage(event));
         return this;
     }
@@ -144,19 +144,19 @@ public class AnnotatedSagaTestFixture implements FixtureConfiguration, Continued
     }
 
     @Override
-    public ContinuedGivenState andThenTimeElapses(final Duration elapsedTime) {
+    public ContinuedGivenState andThenTimeElapses(final Duration elapsedTime) throws Exception {
         eventScheduler.advanceTime(elapsedTime, sagaManager::handle);
         return this;
     }
 
     @Override
-    public ContinuedGivenState andThenTimeAdvancesTo(final ZonedDateTime newDateTime) {
+    public ContinuedGivenState andThenTimeAdvancesTo(final ZonedDateTime newDateTime) throws Exception {
         eventScheduler.advanceTime(newDateTime, sagaManager::handle);
         return this;
     }
 
     @Override
-    public ContinuedGivenState andThenAPublished(Object event) {
+    public ContinuedGivenState andThenAPublished(Object event) throws Exception {
         sagaManager.handle(GenericEventMessage.asEventMessage(event));
         return this;
     }
@@ -168,7 +168,7 @@ public class AnnotatedSagaTestFixture implements FixtureConfiguration, Continued
     }
 
     @Override
-    public FixtureExecutionResult whenPublishingA(Object event) {
+    public FixtureExecutionResult whenPublishingA(Object event) throws Exception {
         try {
             fixtureExecutionResult.startRecording();
             sagaManager.handle(GenericEventMessage.asEventMessage(event));
@@ -287,13 +287,13 @@ public class AnnotatedSagaTestFixture implements FixtureConfiguration, Continued
         }
 
         @Override
-        public ContinuedGivenState published(Object... events) {
+        public ContinuedGivenState published(Object... events) throws Exception {
             publish(events);
             return AnnotatedSagaTestFixture.this;
         }
 
         @Override
-        public FixtureExecutionResult publishes(Object event) {
+        public FixtureExecutionResult publishes(Object event) throws Exception {
             try {
                 publish(event);
             } finally {
@@ -302,7 +302,7 @@ public class AnnotatedSagaTestFixture implements FixtureConfiguration, Continued
             return fixtureExecutionResult;
         }
 
-        private void publish(Object... events) {
+        private void publish(Object... events) throws Exception {
             GenericEventMessage.clock = Clock.fixed(currentTime().toInstant(),currentTime().getZone());
 
             try {

test/src/main/java/org/axonframework/test/saga/ContinuedGivenState.java
@@ -47,7 +47,7 @@ public interface ContinuedGivenState extends WhenState {
      * @param elapsedTime The amount of time that will elapse
      * @return an object that allows registration of the actual events to send
      */
-    ContinuedGivenState andThenTimeElapses(Duration elapsedTime);
+    ContinuedGivenState andThenTimeElapses(Duration elapsedTime) throws Exception;
 
     /**
      * Simulate time shifts in the current given state. This can be useful when the time between given events is of
@@ -56,7 +56,7 @@ public interface ContinuedGivenState extends WhenState {
      * @param newDateTime The time to advance the clock to
      * @return an object that allows registration of the actual events to send
      */
-    ContinuedGivenState andThenTimeAdvancesTo(ZonedDateTime newDateTime);
+    ContinuedGivenState andThenTimeAdvancesTo(ZonedDateTime newDateTime) throws Exception;
 
     /**
      * Indicates that the given <code>event</code> has been published in the past. This event is sent to the associated
@@ -65,5 +65,5 @@ public interface ContinuedGivenState extends WhenState {
      * @param event The event to publish
      * @return an object that allows chaining of more given state
      */
-    ContinuedGivenState andThenAPublished(Object event);
+    ContinuedGivenState andThenAPublished(Object event) throws Exception;
 }

test/src/main/java/org/axonframework/test/saga/FixtureConfiguration.java
@@ -130,7 +130,7 @@ public interface FixtureConfiguration {
      * @param event The event to publish
      * @return an object that allows chaining of more given state
      */
-    ContinuedGivenState givenAPublished(Object event);
+    ContinuedGivenState givenAPublished(Object event) throws Exception;
 
     /**
      * Indicates that no relevant activity has occurred in the past.

test/src/main/java/org/axonframework/test/saga/GivenAggregateEventPublisher.java
@@ -34,5 +34,5 @@ public interface GivenAggregateEventPublisher {
      * @param events The events published by the aggregate
      * @return a reference to the fixture to support a fluent interface
      */
-    ContinuedGivenState published(Object... events);
+    ContinuedGivenState published(Object... events) throws Exception;
 }

test/src/main/java/org/axonframework/test/saga/WhenAggregateEventPublisher.java
@@ -33,5 +33,5 @@ public interface WhenAggregateEventPublisher {
      * @param event The event published by the aggregate
      * @return a reference to the test results for the validation  phase
      */
-    FixtureExecutionResult publishes(Object event);
+    FixtureExecutionResult publishes(Object event) throws Exception;
 }

test/src/main/java/org/axonframework/test/saga/WhenState.java
@@ -54,7 +54,7 @@ public interface WhenState {
      * @param event the event to publish
      * @return an object allowing you to verify the test results
      */
-    FixtureExecutionResult whenPublishingA(Object event);
+    FixtureExecutionResult whenPublishingA(Object event) throws Exception;
 
     /**
      * Mimic an elapsed time with no relevant activity for the Saga. If any Events are scheduled to be published within
@@ -67,7 +67,7 @@ public interface WhenState {
      * @param elapsedTime The amount of time to elapse
      * @return an object allowing you to verify the test results
      */
-    FixtureExecutionResult whenTimeElapses(Duration elapsedTime);
+    FixtureExecutionResult whenTimeElapses(Duration elapsedTime) throws Exception;
 
     /**
      * Mimic an elapsed time with no relevant activity for the Saga. If any Events are scheduled to be published within
@@ -80,5 +80,5 @@ public interface WhenState {
      * @param newDateTime The time to advance the clock to
      * @return an object allowing you to verify the test results
      */
-    FixtureExecutionResult whenTimeAdvancesTo(ZonedDateTime newDateTime);
+    FixtureExecutionResult whenTimeAdvancesTo(ZonedDateTime newDateTime) throws Exception;
 }