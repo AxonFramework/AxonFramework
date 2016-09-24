a38b3253efbc3f5d329cea8c2eecf3472f938ec8
Andrea Ratto <aratto@topcon.com>
2016-09-24 13:14:01 +0200

    Add convenience reschedule method on EventScheduler

--- core/src/main/java/org/axonframework/eventhandling/scheduling/EventScheduler.java
+++ core/src/main/java/org/axonframework/eventhandling/scheduling/EventScheduler.java
@@ -63,4 +63,22 @@ public interface EventScheduler {
      * @throws IllegalArgumentException if the token belongs to another scheduler
      */
     void cancelSchedule(ScheduleToken scheduleToken);
+
+    /**
+     * Cancel a scheduled event and schedule another in its place.
+     * <p/>
+     * Convenience method around {@link #cancelSchedule(ScheduleToken)} and {@link #schedule(Duration, Object)}.
+     *
+     * @param scheduleToken   the token returned when the event was scheduled, might be null
+     * @param triggerDuration The amount of time to wait before publishing the event
+     * @param event           The event to publish
+     * @throws IllegalArgumentException if the token belongs to another scheduler
+     * @return the token to use when cancelling the schedule
+     */
+    default ScheduleToken reschedule(ScheduleToken scheduleToken, Duration triggerDuration, Object event) {
+        if (null != scheduleToken) {
+            cancelSchedule(scheduleToken);
+        }
+        return schedule(triggerDuration, event);
+    }
 }
