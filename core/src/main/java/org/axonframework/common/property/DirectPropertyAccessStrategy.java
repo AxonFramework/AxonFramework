473e743cb4b357aee7734a40d25e92333ed20b2b
Bojan Vukasovic <b.vukasovic@sportradar.com>
2016-12-22 15:34:34 +0100

    Updated to get only public fields.

--- core/src/main/java/org/axonframework/common/property/DirectPropertyAccessStrategy.java
+++ core/src/main/java/org/axonframework/common/property/DirectPropertyAccessStrategy.java
@@ -1,9 +1,6 @@
 package org.axonframework.common.property;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
-import static org.axonframework.common.ReflectionUtils.fieldsOf;
 
 /**
  * Implementation of PropertyAccessStrategy that scans class hierarchy to get public field named "property"
@@ -17,9 +14,9 @@ public class DirectPropertyAccessStrategy extends PropertyAccessStrategy {
 
 	@Override
 	protected <T> Property<T> propertyFor(Class<? extends T> targetClass, String property) {
-		Iterable<Field> fields = fieldsOf(targetClass);
+		Field[] fields = targetClass.getFields();
 		for(Field field : fields) {
-			if (field.getName().equals(property) && (field.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) {
+			if (field.getName().equals(property)) {
 				return new DirectlyAccessedProperty<>(field, property);
 			}
 		}
