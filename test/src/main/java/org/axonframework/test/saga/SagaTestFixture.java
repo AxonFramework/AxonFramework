a5cc929f2f2e68fdfcebdea581aa98a2c4abc8a3
Allard Buijze <buijze@gmail.com>
2016-12-24 14:03:36 +0100

    SagaFixture checks for transient modifier on fields with injected resources
    This prevents a common mistake where injected resources are accidentally serialized along with the Saga. Since they are injected after deserialization, they don't need to be serialized.
    Check can be disabled to prevent false positives.

--- test/src/main/java/org/axonframework/test/saga/SagaTestFixture.java
+++ test/src/main/java/org/axonframework/test/saga/SagaTestFixture.java
@@ -18,6 +18,7 @@ package org.axonframework.test.saga;
 
 import org.axonframework.commandhandling.gateway.CommandGatewayFactory;
 import org.axonframework.commandhandling.gateway.DefaultCommandGateway;
+import org.axonframework.common.ReflectionUtils;
 import org.axonframework.eventhandling.EventBus;
 import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.eventhandling.GenericEventMessage;
@@ -38,12 +39,17 @@ import org.axonframework.test.utils.RecordingCommandBus;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.StreamSupport;
+
+import static java.lang.String.format;
+import static org.axonframework.common.ReflectionUtils.fieldsOf;
 
 /**
  * Fixture for testing Annotated Sagas based on events and time passing. This fixture allows resources to be configured
@@ -63,6 +69,8 @@ public class SagaTestFixture<T> implements FixtureConfiguration, ContinuedGivenS
     private final RecordingCommandBus commandBus;
     private final MutableFieldFilter fieldFilters = new MutableFieldFilter();
 
+    private boolean transienceCheckEnabled = true;
+
     /**
      * Creates an instance of the AnnotatedSagaTestFixture to test sagas of the given {@code sagaType}.
      *
@@ -73,9 +81,8 @@ public class SagaTestFixture<T> implements FixtureConfiguration, ContinuedGivenS
         eventScheduler = new StubEventScheduler();
         EventBus eventBus = new SimpleEventBus();
         InMemorySagaStore sagaStore = new InMemorySagaStore();
-        SagaRepository<T> sagaRepository = new AnnotatedSagaRepository<>(sagaType, sagaStore,
-                                                                         new AutowiredResourceInjector(
-                                                                                 registeredResources));
+        SagaRepository<T> sagaRepository = new AnnotatedSagaRepository<>(
+                sagaType, sagaStore, new TransienceValidatingResourceInjector());
         sagaManager = new AnnotatedSagaManager<>(sagaType, sagaRepository);
         sagaManager.setSuppressExceptions(false);
 
@@ -100,6 +107,12 @@ public class SagaTestFixture<T> implements FixtureConfiguration, ContinuedGivenS
         } catch (Exception e) {
             throw new FixtureExecutionException("Exception occurred while handling an event", e);
         }
+    }
+
+    @Override
+    public FixtureConfiguration withTransienceCheckDisabled() {
+        this.transienceCheckEnabled = false;
+        return this;
     }
 
     @Override
@@ -130,7 +143,6 @@ public class SagaTestFixture<T> implements FixtureConfiguration, ContinuedGivenS
     @Override
     public void registerResource(Object resource) {
         registeredResources.add(resource);
-//        FixtureResourceParameterResolverFactory.registerResource(resource);
     }
 
     @Override
@@ -347,6 +359,31 @@ public class SagaTestFixture<T> implements FixtureConfiguration, ContinuedGivenS
 
         public void add(FieldFilter fieldFilter) {
             filters.add(fieldFilter);
+        }
+    }
+
+    private class TransienceValidatingResourceInjector extends AutowiredResourceInjector {
+
+        public TransienceValidatingResourceInjector() {
+            super(registeredResources);
+        }
+
+        @Override
+        public void injectResources(Object saga) {
+            super.injectResources(saga);
+            if (transienceCheckEnabled) {
+                StreamSupport.stream(fieldsOf(saga.getClass()).spliterator(), false)
+                        .filter(f -> !Modifier.isTransient(f.getModifiers()))
+                        .filter(f -> registeredResources.contains(ReflectionUtils.getFieldValue(f, saga)))
+                        .findFirst()
+                        .ifPresent(field -> {
+                            throw new AssertionError(format("Field %s.%s is injected with a resource, " +
+                                                                    "but it doesn't have the 'transient' modifier.\n" +
+                                                                    "Mark field as 'transient' or disable this check using:\n" +
+                                                                    "fixture.withTransienceCheckDisabled()",
+                                                            field.getDeclaringClass(), field.getName()));
+                        });
+            }
         }
     }
 }
---
+++ test/src/test/java/org/axonframework/test/saga/NonTransientResource.java
@@ -0,0 +1,4 @@
+package org.axonframework.test.saga;
+
+public class NonTransientResource {
+}
