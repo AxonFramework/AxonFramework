8a2fadf6573e57ef7e30c65ad32fc3c7c964bb6e
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2017-01-02 19:02:39 +0100

    FixtureConfiguration: Allow a mix of given events and commands

--- test/src/main/java/org/axonframework/test/aggregate/TestExecutor.java
+++ test/src/main/java/org/axonframework/test/aggregate/TestExecutor.java
@@ -16,7 +16,10 @@
 
 package org.axonframework.test.aggregate;
 
+import java.util.List;
 import java.util.Map;
+
+import org.axonframework.messaging.Message;
 
 /**
  * Interface describing the operations available on a test fixture in the execution stage. In this stage, there is only
@@ -50,4 +53,48 @@ public interface TestExecutor {
      * @return a ResultValidator that can be used to validate the resulting actions of the command execution
      */
     ResultValidator when(Object command, Map<String, ?> metaData);
+
+    /**
+     * Configures the given {@code domainEvents} as the "given" events. These are the events returned by the event
+     * store when an aggregate is loaded.
+     * <p/>
+     * If an item in the given {@code domainEvents} implements {@link Message}, the
+     * payload and meta data from that message are copied into a newly created Domain Event Message. Otherwise, a
+     * Domain Event Message with the item as payload and empty meta data is created.
+     *
+     * @param domainEvents the domain events the event store should return
+     * @return a TestExecutor instance that can execute the test with this configuration
+     */
+    TestExecutor andGiven(Object... domainEvents);
+
+    /**
+     * Configures the given {@code domainEvents} as the "given" events. These are the events returned by the event
+     * store when an aggregate is loaded.
+     * <p/>
+     * If an item in the list implements {@link Message}, the payload and meta data from that
+     * message are copied into a newly created Domain Event Message. Otherwise, a Domain Event Message with the item
+     * as payload and empty meta data is created.
+     *
+     * @param domainEvents the domain events the event store should return
+     * @return a TestExecutor instance that can execute the test with this configuration
+     */
+    TestExecutor andGiven(List<?> domainEvents);
+
+    /**
+     * Configures the given {@code commands} as the command that will provide the "given" events. The commands are
+     * executed, and the resulting stored events are captured.
+     *
+     * @param commands the domain events the event store should return
+     * @return a TestExecutor instance that can execute the test with this configuration
+     */
+    TestExecutor andGivenCommands(Object... commands);
+
+    /**
+     * Configures the given {@code commands} as the command that will provide the "given" events. The commands are
+     * executed, and the resulting stored events are captured.
+     *
+     * @param commands the domain events the event store should return
+     * @return a TestExecutor instance that can execute the test with this configuration
+     */
+    TestExecutor andGivenCommands(List<?> commands);
 }
