fe5bced9951dd2467715689b388b9b72f6228529
Gerard de Leeuw <gerard.deleeuw@kadaster.nl>
2017-01-11 09:29:39 +0100

    Fixed a StackOverflowError in ModelInspector that occurs when using a recursive aggregate model

--- core/src/test/java/org/axonframework/commandhandling/model/ModelInspectorTest.java
+++ core/src/test/java/org/axonframework/commandhandling/model/ModelInspectorTest.java
@@ -29,10 +29,15 @@ import org.junit.Test;
 import javax.persistence.Id;
 import java.lang.annotation.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static org.axonframework.commandhandling.GenericCommandMessage.asCommandMessage;
+import static org.axonframework.eventhandling.GenericEventMessage.asEventMessage;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ModelInspectorTest {
 
@@ -53,6 +58,69 @@ public class ModelInspectorTest {
         CommandMessage<?> message = asCommandMessage("sub");
         assertEquals(true, inspector.commandHandler(message.getCommandName()).handle(message, target));
         assertEquals(false, inspector.commandHandler(message.getCommandName()).handle(asCommandMessage("ok"), target));
+    }
+
+    @Test
+    public void testDetectAllAnnotatedHandlersInRecursiveHierarchy() throws Exception {
+        AggregateModel<SomeRecursiveRoot> inspector = ModelInspector.inspectAggregate(SomeRecursiveRoot.class);
+
+        SomeRecursiveRoot target = new SomeRecursiveRoot();
+
+        // Create a hierarchy of id's that we will use in this test.
+        // The resulting hierarchy will look as follows:
+        // root 
+        //      child0
+        //              child1
+        //                      child2
+        //                      child3
+        //              child4
+        String childId0 = SomeRecursiveEntity.childId(target.rootId, 1);
+        String childId1 = SomeRecursiveEntity.childId(childId0, 1);
+        String childId2 = SomeRecursiveEntity.childId(childId1, 1);
+        String childId3 = SomeRecursiveEntity.childId(childId1, 2);
+        String childId4 = SomeRecursiveEntity.childId(childId0, 2);
+
+        // Assert child0: the id should match and it should not have any children (yet)
+        SomeRecursiveEntity child0 = target.entity;
+        assertEquals(childId0, child0.entityId);
+        assertEquals(0, child0.children.size());
+
+        // Publish an event that is picked up by child0. It should have 1 child afterwards.
+        inspector.publish(asEventMessage(childId0), target);
+        assertEquals(1, child0.children.size());
+
+        // Assert child1: the id should match and it should not have any children (yet)
+        SomeRecursiveEntity child1 = child0.children.get(0);
+        assertNotNull(child1);
+        assertEquals(childId1, child1.entityId);
+        assertEquals(0, child1.children.size());
+
+        // Publish 2 events that are picked up by child1. It should have 2 children afterwards.
+        inspector.publish(asEventMessage(childId1), target);
+        inspector.publish(asEventMessage(childId1), target);
+        assertEquals(2, child1.children.size());
+
+        // Assert child2: the id should match and it should not have any children
+        SomeRecursiveEntity child2 = child1.children.get(0);
+        assertNotNull(child2);
+        assertEquals(childId2, child2.entityId);
+        assertEquals(0, child2.children.size());
+
+        // Assert child3: the id should match and it should not have any children
+        SomeRecursiveEntity child3 = child1.children.get(1);
+        assertNotNull(child3);
+        assertEquals(childId3, child3.entityId);
+        assertEquals(0, child3.children.size());
+
+        // Publish an event that is picked up by child0. It should have 2 children afterwards.
+        inspector.publish(asEventMessage(childId0), target);
+        assertEquals(2, child0.children.size());
+
+        // Assert child4: the id should match and it should not have any children
+        SomeRecursiveEntity child4 = child0.children.get(1);
+        assertNotNull(child4);
+        assertEquals(childId4, child4.entityId);
+        assertEquals(0, child4.children.size());
     }
 
     @Test
@@ -168,6 +236,37 @@ public class ModelInspectorTest {
         @EventHandler
         public void handle(AtomicLong value) {
             value.incrementAndGet();
+        }
+    }
+
+    private static class SomeRecursiveRoot {
+
+        private String rootId = "root";
+
+        @AggregateMember
+        private SomeRecursiveEntity entity = new SomeRecursiveEntity(SomeRecursiveEntity.childId(rootId, 1));
+    }
+
+    private static class SomeRecursiveEntity {
+
+        private final String entityId;
+
+        @AggregateMember
+        private List<SomeRecursiveEntity> children = new ArrayList<>();
+
+        public SomeRecursiveEntity(String entityId) {
+            this.entityId = entityId;
+        }
+
+        public static String childId(String parent, int number) {
+            return String.format("child #%d of %s", number, parent);
+        }
+
+        @EventHandler
+        public void handle(String targetEntityId) {
+            if (Objects.equals(entityId, targetEntityId)) {
+                children.add(new SomeRecursiveEntity(childId(this.entityId, children.size() + 1)));
+            }
         }
     }
 
