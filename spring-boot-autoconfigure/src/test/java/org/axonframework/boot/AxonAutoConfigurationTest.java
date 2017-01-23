ba2966a2414f3e00c3c55276218a7497966b74a0
Allard Buijze <buijze@gmail.com>
2017-01-23 14:02:51 +0100

    CorrelationDataProviders are now automatically configured based on their availability in the Application Context
    When at least one Spring Bean of type CorrelationDataProvider is available in the application context, they are automatically configured as the CorrelationDataProviders to use for all messaging components. If none is present, Axon defaults to the MessageOriginProvider.
    
    Issue #240 Fixed

--- spring-boot-autoconfigure/src/test/java/org/axonframework/boot/AxonAutoConfigurationTest.java
+++ spring-boot-autoconfigure/src/test/java/org/axonframework/boot/AxonAutoConfigurationTest.java
@@ -3,13 +3,17 @@ package org.axonframework.boot;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.config.Configurer;
 import org.axonframework.eventhandling.EventBus;
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.eventhandling.saga.SagaEventHandler;
 import org.axonframework.eventhandling.tokenstore.TokenStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
+import org.axonframework.messaging.correlation.CorrelationDataProvider;
+import org.axonframework.messaging.correlation.SimpleCorrelationDataProvider;
 import org.axonframework.serialization.Serializer;
+import org.axonframework.spring.config.AxonConfiguration;
 import org.axonframework.spring.stereotype.Aggregate;
 import org.axonframework.spring.stereotype.Saga;
 import org.junit.Test;
@@ -26,18 +30,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-@ContextConfiguration(classes = {DataSourceAutoConfiguration.class,
-        AxonAutoConfiguration.class,
-        AxonAutoConfigurationTest.Context.class})
+@ContextConfiguration(classes = {
+        AxonAutoConfigurationTest.Context.class,
+        DataSourceAutoConfiguration.class,
+        AxonAutoConfiguration.class})
 @RunWith(SpringRunner.class)
 public class AxonAutoConfigurationTest {
 
     @Autowired
     private ApplicationContext applicationContext;
 
+    @Autowired
+    private Configurer configurer;
+
+    @Autowired
+    private AxonConfiguration configuration;
+
     @Test
     public void testContextInitialization() throws Exception {
         assertNotNull(applicationContext);
+        assertNotNull(configurer);
 
         assertNotNull(applicationContext.getBean(CommandBus.class));
         assertNotNull(applicationContext.getBean(EventBus.class));
@@ -47,6 +59,8 @@ public class AxonAutoConfigurationTest {
         assertEquals(0, applicationContext.getBeansOfType(TokenStore.class).size());
         assertNotNull(applicationContext.getBean(Context.MySaga.class));
         assertNotNull(applicationContext.getBean(Context.MyAggregate.class));
+
+        assertEquals(2, configuration.correlationDataProviders().size());
     }
 
     @Configuration
@@ -55,6 +69,16 @@ public class AxonAutoConfigurationTest {
         @Bean
         public EventStorageEngine storageEngine() {
             return new InMemoryEventStorageEngine();
+        }
+
+        @Bean
+        public CorrelationDataProvider correlationData1() {
+            return new SimpleCorrelationDataProvider("key1");
+        }
+
+        @Bean
+        public CorrelationDataProvider correlationData2() {
+            return new SimpleCorrelationDataProvider("key2");
         }
 
         @Aggregate
