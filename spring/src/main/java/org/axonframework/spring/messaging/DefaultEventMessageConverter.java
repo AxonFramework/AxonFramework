3d12ba66af9036e9ae9ab75ba46b5d113c839f61
Allard Buijze <buijze@gmail.com>
2017-04-07 11:20:22 +0200

    Fixed missing copyright statements

--- spring/src/main/java/org/axonframework/spring/messaging/DefaultEventMessageConverter.java
+++ spring/src/main/java/org/axonframework/spring/messaging/DefaultEventMessageConverter.java
@@ -1,10 +1,19 @@
-package org.axonframework.spring.messaging;
+/*
+ * Copyright (c) 2010-2017. Axon Framework
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
+package org.axonframework.spring.messaging;
 
 import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.eventhandling.GenericEventMessage;
@@ -14,6 +23,12 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.util.NumberUtils;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * An {@link EventMessageConverter} that will convert an Axon event message into a Spring message by:
