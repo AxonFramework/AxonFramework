899858f8545302916db394e8702e14c40b3008d8
Allard Buijze <buijze@gmail.com>
2017-05-18 20:11:04 +0200

    EventLoggingInterceptor should be able to handle UnitOfWork not tied to a Message

--- core/src/main/java/org/axonframework/eventhandling/interceptors/EventLoggingInterceptor.java
+++ core/src/main/java/org/axonframework/eventhandling/interceptors/EventLoggingInterceptor.java
@@ -69,10 +69,14 @@ public class EventLoggingInterceptor implements MessageDispatchInterceptor<Event
     @Override
     public BiFunction<Integer, EventMessage<?>, EventMessage<?>> handle(List<EventMessage<?>> messages) {
         StringBuilder sb = new StringBuilder(String.format("Events published: [%s]",
-                messages.stream().map(m -> m.getPayloadType().getSimpleName()).collect(Collectors.joining(", "))));
+                                                           messages.stream().map(m -> m.getPayloadType().getSimpleName()).collect(Collectors.joining(", "))));
         CurrentUnitOfWork.ifStarted(unitOfWork -> {
             Message<?> message = unitOfWork.getMessage();
-            sb.append(String.format(" while processing a [%s]", message.getPayloadType().getSimpleName()));
+            if (message == null) {
+                sb.append(" while processing an operation not tied to an incoming message");
+            } else {
+                sb.append(String.format(" while processing a [%s]", message.getPayloadType().getSimpleName()));
+            }
             ExecutionResult executionResult = unitOfWork.getExecutionResult();
             if (executionResult != null) {
                 if (executionResult.isExceptionResult()) {
@@ -82,7 +86,7 @@ public class EventLoggingInterceptor implements MessageDispatchInterceptor<Event
                     sb.append(String.format(" which failed with a [%s]", exception.getClass().getSimpleName()));
                 } else if (executionResult.getResult() != null) {
                     sb.append(String.format(" which yielded a [%s] return value",
-                            executionResult.getResult().getClass().getSimpleName()));
+                                            executionResult.getResult().getClass().getSimpleName()));
                 }
             }
         });
