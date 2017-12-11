5253171abe2f104fd57a800d2d24057d4ef39124
Milan <Savic>
2017-12-11 22:16:54 +0100

    Added auto configuration for Routing Strategy (set to Annotation Routing Strategy). Also, if there is no @TargetIdentifier set on command payload no error will be triggered, instead, random key policy will be used.
    Resolves #450.

--- core/src/main/java/org/axonframework/commandhandling/distributed/AnnotationRoutingStrategy.java
+++ core/src/main/java/org/axonframework/commandhandling/distributed/AnnotationRoutingStrategy.java
@@ -67,7 +67,7 @@ public class AnnotationRoutingStrategy extends AbstractRoutingStrategy {
      *                       targeted aggregate.
      */
     public AnnotationRoutingStrategy(Class<? extends Annotation> annotationType) {
-        this(annotationType, UnresolvedRoutingKeyPolicy.ERROR);
+        this(annotationType, UnresolvedRoutingKeyPolicy.RANDOM_KEY);
     }
 
     /**
