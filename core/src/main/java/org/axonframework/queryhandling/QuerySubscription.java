8695781d3c04a4b737c61614b16265245ce0454a
smcvb <smcvbeelen@gmail.com>
2018-01-23 11:29:57 +0100

    Introduce a QuerySubscription for subscribing query handlers
    Introduce a QuerySubscription for subscribing query handlers to a
    QueryBus
    
    [#463]

---
+++ core/src/main/java/org/axonframework/queryhandling/QuerySubscription.java
@@ -0,0 +1,36 @@
+package org.axonframework.queryhandling;
+
+import org.axonframework.messaging.MessageHandler;
+
+/**
+ * Encapsulates the identifying fields of a Query Handler when one is subscribed to the
+ * {@link org.axonframework.queryhandling.QueryBus}. As such contains the response type of the query handler and the
+ * complete handler itself.
+ * The first is typically used by the QueryBus to select the right query handler when a query comes in.
+ * The latter is used to perform the actual query.
+ *
+ * @param <R> the type of response this query subscription contains
+ */
+class QuerySubscription<R> {
+
+    private final Class<R> responseType;
+    private final MessageHandler<? super QueryMessage<?, R>> queryHandler;
+
+    QuerySubscription(Class<R> responseType,
+                      MessageHandler<? super QueryMessage<?, R>> queryHandler) {
+        this.responseType = responseType;
+        this.queryHandler = queryHandler;
+    }
+
+    public Class<R> getResponseType() {
+        return responseType;
+    }
+
+    public boolean canHandle(ResponseType queryResponseType) {
+        return queryResponseType.matches(responseType);
+    }
+
+    public MessageHandler<? super QueryMessage<?, R>> getQueryHandler() {
+        return queryHandler;
+    }
+}
