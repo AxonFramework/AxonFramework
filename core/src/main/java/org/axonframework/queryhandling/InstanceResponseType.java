f3dc2af8c4f4f60cfe6df368a7aacd30d3744384
smcvb <smcvbeelen@gmail.com>
2018-01-26 14:17:28 +0100

    Major stab at most of the matches possibilities
    Major stab at most of the matches possibilities, which should be more
    split over Instance and List
    
    [#463]

--- core/src/main/java/org/axonframework/queryhandling/InstanceResponseType.java
+++ core/src/main/java/org/axonframework/queryhandling/InstanceResponseType.java
@@ -1,6 +1,13 @@
 package org.axonframework.queryhandling;
 
+import com.coekie.gentyref.GenericTypeReflector;
+
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
+import java.util.Arrays;
 
 /**
  * @param <R>
@@ -17,7 +24,73 @@ public class InstanceResponseType<R> implements ResponseType<R> {
 
     @Override
     public boolean matches(Type responseType) {
-        return false;
+        return isCollectionOfExpectedType(responseType) ||
+                isArrayOfExpectedType(responseType) ||
+                isGenericArrayOfExpectedType(responseType) ||
+                isGenericAssignableFrom(responseType) ||
+                isAssignableFrom(responseType);
+    }
+
+    private boolean isCollectionOfExpectedType(Type responseType) {
+        boolean isParameterizedType = isParameterizedType(responseType);
+        if (!isParameterizedType) {
+            return false;
+        }
+
+        Type[] actualTypeArguments = ((ParameterizedType) responseType).getActualTypeArguments();
+        return hasOneTypeArgumentWhichMatches(actualTypeArguments);
+    }
+
+    private boolean isParameterizedType(Type responseType) {
+        return responseType instanceof ParameterizedType;
+    }
+
+    private boolean hasOneTypeArgumentWhichMatches(Type[] typeArguments) {
+        boolean hasOneTypeArgument = typeArguments.length == 1;
+        if (!hasOneTypeArgument) {
+            return false;
+        }
+
+        Type responseType = typeArguments[0];
+        return isAssignableFrom(responseType) ||
+                isGenericAssignableFrom(responseType) ||
+                isWildcardTypeWithMatchingUpperBound(responseType);
+    }
+
+    private boolean isWildcardTypeWithMatchingUpperBound(Type responseType) {
+        boolean isWildcardType = isWildcardType(responseType);
+        if (!isWildcardType) {
+            return false;
+        }
+
+        Type[] upperBounds = ((WildcardType) responseType).getUpperBounds();
+        return Arrays.stream(upperBounds).anyMatch(this::isAssignableFrom) ||
+                Arrays.stream(upperBounds).anyMatch(this::isGenericAssignableFrom);
+    }
+
+    private boolean isWildcardType(Type responseType) {
+        return responseType instanceof WildcardType;
+    }
+
+    private boolean isArrayOfExpectedType(Type responseType) {
+        return responseType instanceof Class &&
+                ((Class) responseType).isArray() &&
+                isAssignableFrom(GenericTypeReflector.getArrayComponentType(responseType));
+    }
+
+    private boolean isGenericArrayOfExpectedType(Type responseType) {
+        return responseType instanceof GenericArrayType &&
+                isGenericAssignableFrom(((GenericArrayType) responseType).getGenericComponentType());
+    }
+
+    private boolean isGenericAssignableFrom(Type responseType) {
+        return responseType instanceof TypeVariable &&
+                Arrays.stream(((TypeVariable) responseType).getBounds())
+                      .anyMatch(this::isAssignableFrom);
+    }
+
+    private boolean isAssignableFrom(Type responseType) {
+        return responseType instanceof Class && expectedResponseType.isAssignableFrom((Class) responseType);
     }
 
     @Override
