ce8bee74a2ab7b21f68a7f09de5f39f475fb4a94
nklmish <coding.aurora@gmail.com>
2018-03-02 14:23:41 +0100

    replacing deprecated method

--- spring/src/test/java/org/axonframework/spring/eventhandling/scheduling/quartz/QuartzTableMaker.java
+++ spring/src/test/java/org/axonframework/spring/eventhandling/scheduling/quartz/QuartzTableMaker.java
@@ -25,6 +25,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
@@ -87,12 +88,12 @@ public class QuartzTableMaker implements ApplicationContextAware {
         List<String> statements = new LinkedList<>();
         try {
             LineNumberReader lnr = new LineNumberReader(resource.getReader());
-            String script = JdbcTestUtils.readScript(lnr);
-            char delimiter = ';';
-            if (!JdbcTestUtils.containsSqlScriptDelimiters(script, delimiter)) {
-                delimiter = '\n';
+            String delimiter = ";";
+            String script = ScriptUtils.readScript(lnr, "--", delimiter);
+            if (!ScriptUtils.containsSqlScriptDelimiters(script, delimiter)) {
+                delimiter = "\n";
             }
-            JdbcTestUtils.splitSqlScript(script, delimiter, statements);
+            ScriptUtils.splitSqlScript(script, delimiter, statements);
             for (String statement : statements) {
                 this.entityManager.createNativeQuery(statement).executeUpdate();
             }
