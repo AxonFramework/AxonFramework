9df829ce2edd59e3260af5a9e731c74833b0f743
smcvb <smcvbeelen@gmail.com>
2018-03-11 17:02:59 +0100

    Add javadoc
    Add javadoc
    
    #236

--- spring-boot-autoconfigure/src/main/java/org/axonframework/boot/autoconfig/MetricsAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/axonframework/boot/autoconfig/MetricsAutoConfiguration.java
@@ -2,16 +2,18 @@ package org.axonframework.boot.autoconfig;
 
 import com.codahale.metrics.MetricRegistry;
 import org.axonframework.metrics.GlobalMetricRegistry;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Auto configuration to set up Metrics for the infrastructure components.
+ *
+ * @author Steven van Beelen
+ * @since 3.2
+ */
 @Configuration
-//@AutoConfigureAfter(RoutingStrategyAutoConfiguration.class)
-//@AutoConfigureBefore(AxonAutoConfiguration.class)
 public class MetricsAutoConfiguration {
 
     @Bean
@@ -20,5 +22,4 @@ public class MetricsAutoConfiguration {
     public GlobalMetricRegistry globalMetricRegistry(MetricRegistry metricRegistry) {
         return new GlobalMetricRegistry(metricRegistry);
     }
-
 }
