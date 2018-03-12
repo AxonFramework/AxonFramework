da7bef228cda6852840ac6fa1a828dc39a95e806
Allard Buijze <allard.buijze@axoniq.io>
2018-03-12 17:42:49 +0100

    Fixed auto-generation of IDE support files
    Signed-off-by: Allard Buijze <allard.buijze@axoniq.io>

--- spring-boot-autoconfigure/src/main/java/org/axonframework/boot/MetricsProperties.java
+++ spring-boot-autoconfigure/src/main/java/org/axonframework/boot/MetricsProperties.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,6 +28,24 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class MetricsProperties {
 
     private AutoConfiguration autoConfiguration = new AutoConfiguration();
+
+    /**
+     * Retrieves the AutoConfiguration settings for Metrics
+     *
+     * @return the AutoConfiguration settings for Metrics
+     */
+    public AutoConfiguration getAutoConfiguration() {
+        return autoConfiguration;
+    }
+
+    /**
+     * Defines the AutoConfiguration settings for Metrics.
+     *
+     * @param autoConfiguration the AutoConfiguration settings for Metrics.
+     */
+    public void setAutoConfiguration(AutoConfiguration autoConfiguration) {
+        this.autoConfiguration = autoConfiguration;
+    }
 
     /**
      * Auto configuration specific properties around Metrics.
--- spring-boot-autoconfigure/src/main/java/org/axonframework/boot/autoconfig/MetricsAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/axonframework/boot/autoconfig/MetricsAutoConfiguration.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +17,7 @@
 package org.axonframework.boot.autoconfig;
 
 import com.codahale.metrics.MetricRegistry;
+import org.axonframework.boot.MetricsProperties;
 import org.axonframework.metrics.GlobalMetricRegistry;
 import org.axonframework.metrics.MetricsConfigurerModule;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
@@ -24,6 +25,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -39,6 +41,7 @@ import org.springframework.context.annotation.Configuration;
         "com.codahale.metrics.MetricRegistry",
         "org.axonframework.metrics.GlobalMetricRegistry"
 })
+@EnableConfigurationProperties(MetricsProperties.class)
 public class MetricsAutoConfiguration {
 
     @Bean
