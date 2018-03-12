ef7cc50e473412a1d1efae5bc83433a67be959f5
smcvb <smcvbeelen@gmail.com>
2018-03-12 09:56:04 +0100

    Add ModuleConfigurer interface
    Add ModuleConfigurer interface
    
    #236

---
+++ core/src/main/java/org/axonframework/config/ModuleConfigurer.java
@@ -0,0 +1,18 @@
+package org.axonframework.config;
+
+/**
+ * Interface describing a configurer for a module in the Axon Configuration API. Allows the registration of modules on
+ * the {@link org.axonframework.config.Configurer} like the {@link org.axonframework.monitoring.MessageMonitor}.
+ *
+ * @author Steven van Beelen
+ * @since 3.2
+ */
+public interface ModuleConfigurer {
+
+    /**
+     * Configure this module to the given global {@link org.axonframework.config.Configurer}.
+     *
+     * @param configurer a {@link org.axonframework.config.Configurer} instance to configure this module with
+     */
+    void configureModule(Configurer configurer);
+}
