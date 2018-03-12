8498197cf85382e90d961897ba0bdbc0b143d5b3
smcvb <smcvbeelen@gmail.com>
2018-03-12 15:15:02 +0100

    Force update copyright notices
    Force update copyright notices
    
    #488

--- core/src/main/java/org/axonframework/commandhandling/distributed/SimpleMember.java
+++ core/src/main/java/org/axonframework/commandhandling/distributed/SimpleMember.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2010-2016. Axon Framework
+ * Copyright (c) 2010-2018. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,6 +24,7 @@ import java.util.function.Consumer;
  * An endpoint in the network for a command handling service.
  *
  * @param <E> The type of the identifier of this entry in the set of nodes
+ *
  */
 public class SimpleMember<E> implements Member {
 
