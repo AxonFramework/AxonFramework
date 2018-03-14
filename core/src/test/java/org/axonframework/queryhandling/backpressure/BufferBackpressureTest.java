9bc6dce827bc27b574d551f361e44513356e194b
Milan <Savic>
2018-03-14 16:56:53 +0100

    Fixed codacy issues.

--- core/src/test/java/org/axonframework/queryhandling/backpressure/BufferBackpressureTest.java
+++ core/src/test/java/org/axonframework/queryhandling/backpressure/BufferBackpressureTest.java
@@ -22,7 +22,6 @@ import org.axonframework.queryhandling.UpdateHandler;
 import org.junit.*;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
