cac48540b25b03ae7ae7c59e992c57f06077c787
sara pellegrini <sara.pellegrini@gmail.com>
2018-03-23 15:35:03 +0100

    Axon Framework 3.2

--- src/test/java/io/axoniq/axonhub/client/query/AxonHubQueryBusTest.java
+++ src/test/java/io/axoniq/axonhub/client/query/AxonHubQueryBusTest.java
@@ -28,24 +28,22 @@ import org.axonframework.messaging.MetaData;
 import org.axonframework.queryhandling.GenericQueryMessage;
 import org.axonframework.queryhandling.QueryMessage;
 import org.axonframework.queryhandling.SimpleQueryBus;
+import org.axonframework.queryhandling.responsetypes.InstanceResponseType;
 import org.axonframework.serialization.xml.XStreamSerializer;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 /**
  * Author: marc
  */
 public class AxonHubQueryBusTest {
+
     private AxonHubQueryBus queryBus;
     private DummyMessagePlatformServer dummyMessagePlatformServer;
     private XStreamSerializer ser;
@@ -87,9 +85,9 @@ public class AxonHubQueryBusTest {
 
     @Test
     public void query() throws Exception {
-        QueryMessage<String, String> queryMessage = new GenericQueryMessage<>("Hello, World", String.class);
+        QueryMessage<String, String> queryMessage = new GenericQueryMessage<>("Hello, World", new InstanceResponseType(String.class));
 
-        assertEquals("test", queryBus.query(queryMessage).get());
+        assertEquals("test", queryBus.query(queryMessage).get().getPayload());
     }
 
     @Test
@@ -131,24 +129,23 @@ public class AxonHubQueryBusTest {
         inboundStreamObserverRef.get().onNext(inboundMessage);
 
         response.close();
-
     }
 
     @Test
-    public void queryAll() throws Exception {
-        QueryMessage<String, String> queryMessage = new GenericQueryMessage<>("Hello, World", String.class)
+    public void scatterGather() throws Exception {
+        QueryMessage<String, String> queryMessage = new GenericQueryMessage<>("Hello, World",
+                                                                              new InstanceResponseType(String.class))
                 .andMetaData(MetaData.with("repeat", 10).and("interval", 10));
 
-        assertEquals(10, queryBus.queryAll(queryMessage, 2, TimeUnit.SECONDS).count());
+        assertEquals(10, queryBus.scatterGather(queryMessage, 12, TimeUnit.SECONDS).count());
     }
 
     @Test
-    public void queryAllTimeout() throws Exception {
-        QueryMessage<String, String> queryMessage = new GenericQueryMessage<>("Hello, World", String.class)
+    public void scatterGatherTimeout() throws Exception {
+        QueryMessage<String, String> queryMessage = new GenericQueryMessage<>("Hello, World",
+                                                                              new InstanceResponseType(String.class))
                 .andMetaData(MetaData.with("repeat", 10).and("interval", 100));
 
-        assertTrue(8 > queryBus.queryAll(queryMessage, 550, TimeUnit.MILLISECONDS).count());
+        assertTrue(8 > queryBus.scatterGather(queryMessage, 550, TimeUnit.MILLISECONDS).count());
     }
-
-
 }
