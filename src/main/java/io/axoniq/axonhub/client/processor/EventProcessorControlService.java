7ceb3f742aae56478399ec66ba1351358ac2c09b
sara pellegrini <sara.pellegrini@gmail.com>
2018-03-28 10:05:48 +0200

    Event processor control API - Removed unused proto messages

--- src/main/java/io/axoniq/axonhub/client/processor/EventProcessorControlService.java
+++ src/main/java/io/axoniq/axonhub/client/processor/EventProcessorControlService.java
@@ -1,15 +1,14 @@
 package io.axoniq.axonhub.client.processor;
 
 import io.axoniq.axonhub.client.PlatformConnectionManager;
-import io.axoniq.platform.grpc.EventProcessorPaused;
-import io.axoniq.platform.grpc.EventProcessorStarted;
 import io.axoniq.platform.grpc.PauseEventProcessor;
-import io.axoniq.platform.grpc.PlatformInboundInstruction;
 import io.axoniq.platform.grpc.PlatformOutboundInstruction;
 import io.axoniq.platform.grpc.StartEventProcessor;
 
 import static io.axoniq.platform.grpc.PlatformOutboundInstruction.RequestCase.PAUSE_EVENT_PROCESSOR;
 import static io.axoniq.platform.grpc.PlatformOutboundInstruction.RequestCase.STAR_EVENT_PROCESSOR;
+
+
 
 /**
  * Created by Sara Pellegrini on 09/03/2018.
@@ -30,9 +29,6 @@ public class EventProcessorControlService {
     public void init(){
         this.platformConnectionManager.onOutboundInstruction(PAUSE_EVENT_PROCESSOR, this::pauseProcessor);
         this.platformConnectionManager.onOutboundInstruction(STAR_EVENT_PROCESSOR, this::startProcessor);
-
-        this.eventProcessorController.onPause(this::notifyProcessorPaused);
-        this.eventProcessorController.onStart(this::notifyProcessorStarted);
     }
 
     public void pauseProcessor(PlatformOutboundInstruction platformOutboundInstruction) {
@@ -47,21 +43,7 @@ public class EventProcessorControlService {
         eventProcessorController.startProcessor(processorName);
     }
 
-    public void notifyProcessorStarted(String processorName) {
-        EventProcessorStarted.Builder message = EventProcessorStarted.newBuilder().setProcessorName(processorName);
-        PlatformInboundInstruction instruction = PlatformInboundInstruction.newBuilder()
-                                                                           .setEventProcessorStarted(message)
-                                                                           .build();
-        platformConnectionManager.send(instruction);
-    }
 
-    public void notifyProcessorPaused(String processorName) {
-        EventProcessorPaused.Builder message = EventProcessorPaused.newBuilder().setProcessorName(processorName);
-        PlatformInboundInstruction instruction = PlatformInboundInstruction.newBuilder()
-                                                                           .setEventProcessorPaused(message)
-                                                                           .build();
-        platformConnectionManager.send(instruction);
-    }
 
 
 
