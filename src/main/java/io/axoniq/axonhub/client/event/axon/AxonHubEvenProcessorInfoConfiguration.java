15c92ffc90d3ac81b41499a8044225fba295d40c
sara pellegrini <sara.pellegrini@gmail.com>
2018-04-03 11:45:42 +0200

    Event Processor API - Module configuration

---
+++ src/main/java/io/axoniq/axonhub/client/event/axon/AxonHubEvenProcessorInfoConfiguration.java
@@ -0,0 +1,52 @@
+package io.axoniq.axonhub.client.event.axon;
+
+import io.axoniq.axonhub.client.AxonHubConfiguration;
+import io.axoniq.axonhub.client.PlatformConnectionManager;
+import io.axoniq.axonhub.client.processor.EventProcessorControlService;
+import io.axoniq.axonhub.client.processor.EventProcessorController;
+import io.axoniq.axonhub.client.processor.grpc.GrpcEventProcessorInfoSource;
+import io.axoniq.axonhub.client.processor.schedule.ScheduledEventProcessorInfoSource;
+import org.axonframework.config.Configuration;
+import org.axonframework.config.EventHandlingConfiguration;
+import org.axonframework.config.ModuleConfiguration;
+
+/**
+ * Created by Sara Pellegrini on 03/04/2018.
+ * sara.pellegrini@gmail.com
+ */
+public class AxonHubEvenProcessorInfoConfiguration implements ModuleConfiguration {
+
+    private final EventProcessorControlService eventProcessorControlService;
+
+    private final ScheduledEventProcessorInfoSource processorInfoSource;
+
+    public AxonHubEvenProcessorInfoConfiguration(
+            EventHandlingConfiguration eventHandlingConfiguration,
+            PlatformConnectionManager connectionManager,
+            AxonHubConfiguration configuration) {
+        EventProcessorController processorController = new EventProcessorController(eventHandlingConfiguration);
+        this.eventProcessorControlService = new EventProcessorControlService(connectionManager, processorController);
+
+        GrpcEventProcessorInfoSource delegate = new GrpcEventProcessorInfoSource(
+                eventHandlingConfiguration,
+                connectionManager);
+        this.processorInfoSource = new ScheduledEventProcessorInfoSource(
+                configuration.getProcessorsNotificationInitialDelay(),
+                configuration.getProcessorsNotificationRate(),
+                delegate);
+    }
+
+    @Override
+    public void initialize(Configuration config) {
+    }
+
+    @Override
+    public void start() {
+        processorInfoSource.start();
+        eventProcessorControlService.init();
+    }
+
+    @Override
+    public void shutdown() {
+    }
+}
