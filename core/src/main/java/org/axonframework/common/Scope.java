ca9c50db82f1e03fc94fefa5d7b8122073e7dc89
smcvb <smcvbeelen@gmail.com>
2018-06-06 14:24:57 +0200

    Small adjustments
    -Set class level javadoc
    -Add logger
    -Introduce static getCurrentScope() function
    -Rename start() to startScope() for consistency with endScope()
    -Rename end() to endScope(), as SagaLifecycle already specifies and
    end()
    -Add a scope == empty check, to clear out the ThreadLocal LinkedList
    when it is empty
    -Add todo to unchecked exception, to potentially fix it
    
    #220

--- core/src/main/java/org/axonframework/common/Scope.java
+++ core/src/main/java/org/axonframework/common/Scope.java
@@ -1,23 +1,45 @@
 package org.axonframework.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Deque;
 import java.util.LinkedList;
 
 /**
- * TODO add javadoc
+ * Describes functionality off processes which can be 'in scope', like the
+ * {@link org.axonframework.commandhandling.model.AggregateLifecycle} or
+ * {@link org.axonframework.eventhandling.saga.SagaLifecycle}.
+ *
+ * @author Steven van Beelen
+ * @since 3.3
  */
 public abstract class Scope {
 
+    private static final Logger logger = LoggerFactory.getLogger(Scope.class);
+
     private static final ThreadLocal<Deque<Scope>> CURRENT_SCOPE = ThreadLocal.withInitial(LinkedList::new);
 
-    public void start() {
+    @SuppressWarnings("unchecked") // TODO figure out if we can make this a checked call
+    public static <S extends Scope> S getCurrentScope() {
+        return (S) CURRENT_SCOPE.get().getFirst();
+    }
+
+    public void startScope() {
         CURRENT_SCOPE.get().push(this);
     }
 
-    public void end() {
+    public void endScope() {
         if (this != CURRENT_SCOPE.get().peek()) {
-            throw new IllegalStateException("");
+            throw new IllegalStateException(
+                    "Incorrectly trying to end another Scope then which Scope the calling process is contained in."
+            );
         }
         CURRENT_SCOPE.get().pop();
+
+        if (CURRENT_SCOPE.get().isEmpty()) {
+            logger.info("Clearing out ThreadLocal current Scope, as no Scopes are present");
+            CURRENT_SCOPE.remove();
+        }
     }
 }
