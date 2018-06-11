e91d2d69c2ef24d08a5cf0a26c3a9435011deb24
smcvb <smcvbeelen@gmail.com>
2018-06-11 09:45:26 +0200

    Introduce a halve finished SimpleDeadlineManager implementation
    -Reintroduce DeadlineManager interface
    -Replace DeadlineTargetLoader for a list of ScopeAware components
    -Change all constructor calls according to ScopeAware component list
    i.o. DeadlineTargetLoader
    -Update all constructor javadoc according to ScopeAware component list
    i.o. DeadlineTargetLoader
    -Change DeadlineTask to contain deadlineName
    -Remove generateScheduleId(), as the DeadlineManager now takes care of
    that
    -Adjust cancelSchedule(ScheduleToken) to cancelSchedule(String, String)
    -Override cancelAll(String), but don't implement it yet
    -Ugly implement the calls to the scope aware components, and add todo to
     fix it
    
    #220

--- core/src/main/java/org/axonframework/deadline/SimpleDeadlineManager.java
+++ core/src/main/java/org/axonframework/deadline/SimpleDeadlineManager.java
@@ -16,12 +16,11 @@
 
 package org.axonframework.deadline;
 
-import org.axonframework.common.IdentifierFactory;
 import org.axonframework.common.transaction.NoTransactionManager;
 import org.axonframework.common.transaction.Transaction;
 import org.axonframework.common.transaction.TransactionManager;
-import org.axonframework.eventhandling.scheduling.ScheduleToken;
-import org.axonframework.eventhandling.scheduling.java.SimpleScheduleToken;
+import org.axonframework.messaging.ExecutionException;
+import org.axonframework.messaging.ScopeAware;
 import org.axonframework.messaging.ScopeDescriptor;
 import org.axonframework.messaging.unitofwork.DefaultUnitOfWork;
 import org.axonframework.messaging.unitofwork.UnitOfWork;
@@ -29,10 +28,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
-import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
 
+import static org.axonframework.common.Assert.isFalse;
 import static org.axonframework.common.Assert.notNull;
 
 /**
@@ -45,129 +46,157 @@ import static org.axonframework.common.Assert.notNull;
  * </p>
  *
  * @author Milan Savic
+ * @author Steven van Beelen
  * @since 3.3
  */
-// TODO fix this
-public class SimpleDeadlineManager /*implements DeadlineManager */{
+public class SimpleDeadlineManager implements DeadlineManager {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleDeadlineManager.class);
+    private static final Logger logger = LoggerFactory.getLogger(SimpleDeadlineManager.class);
 
+    private final List<ScopeAware> scopeAwareComponents;
     private final ScheduledExecutorService scheduledExecutorService;
     private final TransactionManager transactionManager;
+
     private final Map<String, Future<?>> tokens = new ConcurrentHashMap<>();
 
-    private final DeadlineTargetLoader deadlineTargetLoader;
-
     /**
-     * Initializes SimpleDeadlineManager with {@code deadlineTargetLoader}. {@link NoTransactionManager} is used as
+     * Initializes SimpleDeadlineManager with {@code scopeAwareComponents} which will load and send messages to
+     * {@link org.axonframework.messaging.Scope} implementing components. {@link NoTransactionManager} is used as
      * transaction manager and {@link Executors#newSingleThreadScheduledExecutor()} is used as scheduled executor
      * service.
      *
-     * @param deadlineTargetLoader Responsible for loading the end target capable of handling the deadline message when
-     *                             deadline is not met
+     * @param scopeAwareComponents a {@link List} of {@link ScopeAware} components which are able to load and send
+     *                             Messages to components which implement {@link org.axonframework.messaging.Scope}
      */
-    public SimpleDeadlineManager(DeadlineTargetLoader deadlineTargetLoader) {
-        this(Executors.newSingleThreadScheduledExecutor(), NoTransactionManager.INSTANCE, deadlineTargetLoader);
+    public SimpleDeadlineManager(ScopeAware... scopeAwareComponents) {
+        this(Arrays.asList(scopeAwareComponents));
     }
 
     /**
-     * Initializes SimpleDeadlineManager with {@code transactionManager} and {@code deadlineTargetLoader}. {@link
-     * Executors#newSingleThreadScheduledExecutor()} is used as scheduled executor service.
+     * Initializes SimpleDeadlineManager with {@code scopeAwareComponents} which will load and send messages to
+     * {@link org.axonframework.messaging.Scope} implementing components. {@link NoTransactionManager} is used as
+     * transaction manager and {@link Executors#newSingleThreadScheduledExecutor()} is used as scheduled executor
+     * service.
      *
+     * @param scopeAwareComponents a {@link List} of {@link ScopeAware} components which are able to load and send
+     *                             Messages to components which implement {@link org.axonframework.messaging.Scope}
+     */
+    public SimpleDeadlineManager(List<ScopeAware> scopeAwareComponents) {
+        this(scopeAwareComponents, Executors.newSingleThreadScheduledExecutor(), NoTransactionManager.INSTANCE);
+    }
+
+    /**
+     * Initializes SimpleDeadlineManager with {@code transactionManager} and {@code scopeAwareComponents} which will
+     * load and send messages to {@link org.axonframework.messaging.Scope} implementing components.
+     * {@link Executors#newSingleThreadScheduledExecutor()} is used as scheduled executor service.
+     *
+     * @param scopeAwareComponents a {@link List} of {@link ScopeAware} components which are able to load and send
+     *                             Messages to components which implement {@link org.axonframework.messaging.Scope}
      * @param transactionManager   The transaction manager used to manage transaction during processing of {@link
      *                             DeadlineMessage} when deadline is not met
-     * @param deadlineTargetLoader Responsible for loading the end target capable of handling the deadline message when
-     *                             deadline is not met
      */
-    public SimpleDeadlineManager(TransactionManager transactionManager, DeadlineTargetLoader deadlineTargetLoader) {
-        this(Executors.newSingleThreadScheduledExecutor(), transactionManager, deadlineTargetLoader);
+    public SimpleDeadlineManager(List<ScopeAware> scopeAwareComponents, TransactionManager transactionManager) {
+        this(scopeAwareComponents, Executors.newSingleThreadScheduledExecutor(), transactionManager);
     }
 
     /**
-     * Initializes SimpleDeadlineManager.
+     * Initializes a SimpleDeadlineManager to handle the process around scheduling and triggering a
+     * {@link DeadlineMessage}
      *
+     * @param scopeAwareComponents     a {@link List} of {@link ScopeAware} components which are able to load and send
+     *                                 Messages to components which implement {@link org.axonframework.messaging.Scope}
      * @param scheduledExecutorService Java's service used for scheduling and triggering deadlines
      * @param transactionManager       The transaction manager used to manage transaction during processing of {@link
      *                                 DeadlineMessage} when deadline is not met
-     * @param deadlineTargetLoader     Responsible for loading the end target capable of handling the deadline message
-     *                                 when deadline is not met
      */
-    public SimpleDeadlineManager(ScheduledExecutorService scheduledExecutorService,
-                                 TransactionManager transactionManager,
-                                 DeadlineTargetLoader deadlineTargetLoader) {
+    public SimpleDeadlineManager(List<ScopeAware> scopeAwareComponents,
+                                 ScheduledExecutorService scheduledExecutorService,
+                                 TransactionManager transactionManager) {
+        isFalse(
+                scopeAwareComponents.isEmpty(),
+                () -> "cannot process deadline messages without scope aware components to send them too"
+        );
         notNull(scheduledExecutorService, () -> "scheduledExecutorService may not be null");
         notNull(transactionManager, () -> "transactionManager may not be null");
-        notNull(deadlineTargetLoader, () -> "deadlineTargetLoader may not be null");
+
         this.scheduledExecutorService = scheduledExecutorService;
         this.transactionManager = transactionManager;
-        this.deadlineTargetLoader = deadlineTargetLoader;
+        this.scopeAwareComponents = scopeAwareComponents;
     }
 
-//    @Override
-    public void schedule(Instant triggerDateTime, ScopeDescriptor deadlineScope,
-                         Object deadlineInfo, ScheduleToken scheduleToken) {
-        schedule(Duration.between(Instant.now(), triggerDateTime), deadlineScope, deadlineInfo, scheduleToken);
+    @Override
+    public void schedule(Duration triggerDuration,
+                         String deadlineName,
+                         Object messageOrPayload,
+                         ScopeDescriptor deadlineScope,
+                         String scheduleId) {
+        ScheduledFuture<?> scheduledFuture = scheduledExecutorService.schedule(
+                new DeadlineTask(scheduleId, deadlineName, messageOrPayload, deadlineScope),
+                triggerDuration.toMillis(),
+                TimeUnit.MILLISECONDS
+        );
+        // TODO store deadlineName for cancelAll() and cancelSchedule()
+        tokens.put(scheduleId, scheduledFuture);
     }
 
-//    @Override
-    public void schedule(Duration triggerDuration, ScopeDescriptor deadlineScope,
-                         Object deadlineInfo, ScheduleToken scheduleToken) {
-        SimpleScheduleToken simpleScheduleToken = convert(scheduleToken);
-        ScheduledFuture<?> future = scheduledExecutorService.schedule(new DeadlineTask(simpleScheduleToken.getTokenId(),
-                                                                                       deadlineInfo,
-                                                                                       deadlineScope),
-                                                                      triggerDuration.toMillis(),
-                                                                      TimeUnit.MILLISECONDS);
-        tokens.put(simpleScheduleToken.getTokenId(), future);
-    }
-
-
-
-//    @Override
-    public ScheduleToken generateScheduleId() {
-        return new SimpleScheduleToken(IdentifierFactory.getInstance().generateIdentifier());
-    }
-
-//    @Override
-    public void cancelSchedule(ScheduleToken scheduleToken) {
-        Future<?> future = tokens.remove(convert(scheduleToken).getTokenId());
+    @Override
+    public void cancelSchedule(String deadlineName, String scheduleId) {
+        Future<?> future = tokens.remove(scheduleId);
         if (future != null) {
             future.cancel(false);
         }
     }
 
-    private SimpleScheduleToken convert(ScheduleToken scheduleToken) {
-        if (!SimpleScheduleToken.class.isInstance(scheduleToken)) {
-            throw new IllegalArgumentException("The given ScheduleToken was not provided by this scheduler");
-        }
-        return (SimpleScheduleToken) scheduleToken;
+    @Override
+    public void cancelAll(String deadlineName) {
+        // TODO implement
     }
 
     private class DeadlineTask implements Runnable {
 
         private final String tokenId;
-        private final Object deadlineInfo;
+        private final String deadlineName;
+        private final Object messageOrPayload;
         private final ScopeDescriptor deadlineScope;
 
-        private DeadlineTask(String tokenId, Object deadlineInfo, ScopeDescriptor deadlineScope) {
+        private DeadlineTask(String tokenId,
+                             String deadlineName,
+                             Object messageOrPayload,
+                             ScopeDescriptor deadlineScope) {
             this.tokenId = tokenId;
-            this.deadlineInfo = deadlineInfo;
+            this.deadlineName = deadlineName;
+            this.messageOrPayload = messageOrPayload;
             this.deadlineScope = deadlineScope;
         }
 
         @Override
         public void run() {
-            DeadlineMessage<?> deadlineMessage = GenericDeadlineMessage.asDeadlineMessage(deadlineInfo);
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("Triggered deadline");
+            DeadlineMessage<?> deadlineMessage =
+                    GenericDeadlineMessage.asDeadlineMessage(deadlineName, messageOrPayload);
+            if (logger.isDebugEnabled()) {
+                logger.debug("Triggered deadline");
             }
+
             try {
                 UnitOfWork<DeadlineMessage<?>> unitOfWork = new DefaultUnitOfWork<>(null);
                 Transaction transaction = transactionManager.startTransaction();
                 unitOfWork.onCommit(u -> transaction.commit());
                 unitOfWork.onRollback(u -> transaction.rollback());
-//                unitOfWork.execute(() -> deadlineTargetLoader.load(deadlineScope).handle(deadlineMessage));
+                //Make this call somewhat cleaner, as this's ugly
+                unitOfWork.execute(() -> scopeAwareComponents.stream()
+                                                             .filter(scopeAwareComponent -> scopeAwareComponent
+                                                                     .canResolve(
+                                                                             deadlineScope))
+                                                             .forEach(scopeAwareComponent -> {
+                                                                 try {
+                                                                     scopeAwareComponent.send(deadlineMessage,
+                                                                                              deadlineScope);
+                                                                 } catch (Exception e) {
+                                                                     throw new ExecutionException("", e);
+                                                                 }
+                                                             }));
             } finally {
+                //TODO Adjust remove call to take deadlineName into account
                 tokens.remove(tokenId);
             }
         }
