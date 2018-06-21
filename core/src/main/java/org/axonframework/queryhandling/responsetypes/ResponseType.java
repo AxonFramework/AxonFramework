d4793ccf6f5e8ff0c32af77c2cc174651f5bead9
Allard Buijze <allard.buijze@axoniq.io>
2018-06-21 14:12:16 +0200

    Promoted "getExpectedResponseType" to ResponseType interface

--- core/src/main/java/org/axonframework/queryhandling/responsetypes/ResponseType.java
+++ core/src/main/java/org/axonframework/queryhandling/responsetypes/ResponseType.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010-2018. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.axonframework.queryhandling.responsetypes;
 
 import java.io.Serializable;
@@ -51,4 +67,6 @@ public interface ResponseType<R> extends Serializable {
      * @return a {@link java.lang.Class} representing the type of the payload to be contained in the response message
      */
     Class<R> responseMessagePayloadType();
+
+    Class<?> getExpectedResponseType();
 }
