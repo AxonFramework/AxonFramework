0b4b9a4a66bf84eeee79b593e749b9a041da9f1c
Allard Buijze <allard.buijze@axoniq.io>
2018-06-29 15:22:45 +0200

    Added MySQL to build script to test MySQL specific issues

--- core/src/test/java/org/axonframework/eventsourcing/eventstore/jdbc/MysqlJdbcEventStorageEngineTest.java
+++ core/src/test/java/org/axonframework/eventsourcing/eventstore/jdbc/MysqlJdbcEventStorageEngineTest.java
@@ -1,15 +1,22 @@
+/*
+ * Copyright (c) 2010-2018. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.axonframework.eventsourcing.eventstore.jdbc;
 
-import static junit.framework.TestCase.assertEquals;
-import static org.axonframework.eventsourcing.eventstore.EventStoreTestUtils.createEvent;
-import static org.junit.Assert.assertFalse;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Properties;
-import java.util.UUID;
-import java.util.function.Predicate;
-
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import org.axonframework.common.jdbc.PersistenceExceptionResolver;
 import org.axonframework.common.transaction.NoTransactionManager;
 import org.axonframework.eventsourcing.eventstore.DomainEventData;
@@ -17,14 +24,23 @@ import org.axonframework.eventsourcing.eventstore.jpa.SQLErrorCodesResolver;
 import org.axonframework.serialization.upcasting.event.EventUpcaster;
 import org.axonframework.serialization.upcasting.event.NoOpEventUpcaster;
 import org.axonframework.serialization.xml.XStreamSerializer;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.function.Predicate;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.axonframework.eventsourcing.eventstore.EventStoreTestUtils.createEvent;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Tests the JdbcEventStorageEngine using the MySQL database.
- * 
+ *
  * @author Albert Attard (JavaCreed)
  */
 public class MysqlJdbcEventStorageEngineTest {
@@ -37,15 +53,19 @@ public class MysqlJdbcEventStorageEngineTest {
     public void setUp() throws Exception {
         /* Load the DB properties */
         final Properties properties = new Properties();
-        properties.load(getClass().getResourceAsStream("/mysql.temp.database.properties"));
+        properties.load(getClass().getResourceAsStream("/mysql.test.database.properties"));
 
         dataSource = new MysqlDataSource();
         dataSource.setUrl(properties.getProperty("jdbc.url"));
         dataSource.setUser(properties.getProperty("jdbc.username"));
         dataSource.setPassword(properties.getProperty("jdbc.password"));
-        defaultPersistenceExceptionResolver = new SQLErrorCodesResolver(dataSource);
-        testSubject = createEngine(NoOpEventUpcaster.INSTANCE, defaultPersistenceExceptionResolver, new EventSchema(),
-                byte[].class, MySqlEventTableFactory.INSTANCE);
+        try {
+            defaultPersistenceExceptionResolver = new SQLErrorCodesResolver(dataSource);
+            testSubject = createEngine(NoOpEventUpcaster.INSTANCE, defaultPersistenceExceptionResolver, new EventSchema(),
+                                       byte[].class, MySqlEventTableFactory.INSTANCE);
+        } catch (Exception e) {
+            Assume.assumeNoException("Ignoring this test, as no valid MySQL instance is configured", e);
+        }
     }
 
     /**
@@ -63,20 +83,20 @@ public class MysqlJdbcEventStorageEngineTest {
     }
 
     protected JdbcEventStorageEngine createEngine(EventUpcaster upcasterChain,
-            PersistenceExceptionResolver persistenceExceptionResolver, EventSchema eventSchema, Class<?> dataType,
-            EventTableFactory tableFactory) {
+                                                  PersistenceExceptionResolver persistenceExceptionResolver, EventSchema eventSchema, Class<?> dataType,
+                                                  EventTableFactory tableFactory) {
         return createEngine(upcasterChain, persistenceExceptionResolver, null, eventSchema, dataType, tableFactory,
-                100);
+                            100);
     }
 
     protected JdbcEventStorageEngine createEngine(EventUpcaster upcasterChain,
-            PersistenceExceptionResolver persistenceExceptionResolver,
-            Predicate<? super DomainEventData<?>> snapshotFilter, EventSchema eventSchema, Class<?> dataType,
-            EventTableFactory tableFactory, int batchSize) {
+                                                  PersistenceExceptionResolver persistenceExceptionResolver,
+                                                  Predicate<? super DomainEventData<?>> snapshotFilter, EventSchema eventSchema, Class<?> dataType,
+                                                  EventTableFactory tableFactory, int batchSize) {
         XStreamSerializer serializer = new XStreamSerializer();
         JdbcEventStorageEngine result = new JdbcEventStorageEngine(serializer, upcasterChain,
-                persistenceExceptionResolver, serializer, snapshotFilter, batchSize, dataSource::getConnection,
-                NoTransactionManager.INSTANCE, dataType, eventSchema, null, null);
+                                                                   persistenceExceptionResolver, serializer, snapshotFilter, batchSize, dataSource::getConnection,
+                                                                   NoTransactionManager.INSTANCE, dataType, eventSchema, null, null);
 
         try {
             Connection connection = dataSource.getConnection();
