c49fc0b3ef05665ece7e6293fc1bbfb4998f19b2
smcvb <smcvbeelen@gmail.com>
2018-10-05 12:12:45 +0200

    Provide javadoc, lower case logger and some reindent actions
    Provide javadoc, lower case logger and some reindent actions
    
    #756

--- distributed-commandbus-springcloud/src/main/java/org/axonframework/springcloud/commandhandling/SpringHttpCommandBusConnector.java
+++ distributed-commandbus-springcloud/src/main/java/org/axonframework/springcloud/commandhandling/SpringHttpCommandBusConnector.java
@@ -43,11 +43,20 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
+/**
+ * A {@link CommandBusConnector} implementation based on Spring Rest characteristics. Serves as a {@link RestController}
+ * to receive Command Messages for its node, but also contains a {@link RestOperations} component to send Command
+ * Messages to other nodes. Will use a {@code localCommandBus} of type {@link CommandBus} to publish any received
+ * Command Messages to its local instance. Messages are de-/serialized using a {@link Serializer}.
+ *
+ * @author Steven van Beelen
+ * @since 3.0
+ */
 @RestController
 @RequestMapping("/spring-command-bus-connector")
 public class SpringHttpCommandBusConnector implements CommandBusConnector {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SpringHttpCommandBusConnector.class);
+    private static final Logger logger = LoggerFactory.getLogger(SpringHttpCommandBusConnector.class);
 
     private static final boolean EXPECT_REPLY = true;
     private static final boolean DO_NOT_EXPECT_REPLY = false;
@@ -58,12 +67,37 @@ public class SpringHttpCommandBusConnector implements CommandBusConnector {
     private final Serializer serializer;
     private final Executor executor;
 
+    /**
+     * Initialize a {@link SpringHttpCommandBusConnector} using the provided local {@link CommandBus},
+     * {@link RestOperations} and {@link Serializer}. The {@code localCommandBus} is used to publish received commands
+     * which to the local segment. The given RestOperations provides the connectivity between other nodes to send
+     * commands, and the Serializer is used to serialize the command messages when they are sent between nodes.
+     * The {@link Executor} used to make the sending of commands asynchronous is defaulted to a
+     * {@link DirectExecutor#INSTANCE}.
+     *
+     * @param localCommandBus the {@link CommandBus} to publish received commands which to the local segment
+     * @param restOperations  the {@link RestOperations} used to send commands to other nodes
+     * @param serializer      the {@link Serializer} used to serialize command messages when they are sent between nodes
+     */
     public SpringHttpCommandBusConnector(CommandBus localCommandBus,
                                          RestOperations restOperations,
                                          Serializer serializer) {
         this(localCommandBus, restOperations, serializer, DirectExecutor.INSTANCE);
     }
 
+    /**
+     * Initialize a {@link SpringHttpCommandBusConnector} using the provided local {@link CommandBus},
+     * {@link RestOperations}, {@link Serializer} and {@link Executor}. The {@code localCommandBus} is used to publish
+     * received commands which to the local segment. The given RestOperations provides the connectivity between other
+     * nodes to send commands, and the Serializer is used to serialize the command messages when they are sent between
+     * nodes. The provided Executor is used to unblock then sending of a command with the RestOperations.
+     *
+     * @param localCommandBus the {@link CommandBus} to publish received commands which to the local segment
+     * @param restOperations  the {@link RestOperations} used to send commands to other nodes
+     * @param serializer      the {@link Serializer} used to serialize command messages when they are sent between nodes
+     * @param executor        the {@link Executor} used to make the sending of commands using the @link RestOperations}
+     *                        asynchronous
+     */
     public SpringHttpCommandBusConnector(CommandBus localCommandBus,
                                          RestOperations restOperations,
                                          Serializer serializer,
@@ -130,10 +164,8 @@ public class SpringHttpCommandBusConnector implements CommandBusConnector {
         } else {
             String errorMessage = String.format("No Connection Endpoint found in Member [%s] for protocol [%s] " +
                                                         "to send the command message [%s] to",
-                                                destination,
-                                                URI.class,
-                                                commandMessage);
-            LOGGER.error(errorMessage);
+                                                destination, URI.class, commandMessage);
+            logger.error(errorMessage);
             throw new IllegalArgumentException(errorMessage);
         }
     }
@@ -142,7 +174,7 @@ public class SpringHttpCommandBusConnector implements CommandBusConnector {
         try {
             return new URI(scheme, userInfo, host, port, path + COMMAND_BUS_CONNECTOR_PATH, null, null);
         } catch (URISyntaxException e) {
-            LOGGER.error("Failed to build URI for [{}{}{}], with user info [{}] and path [{}]",
+            logger.error("Failed to build URI for [{}{}{}], with user info [{}] and path [{}]",
                          scheme, host, port, userInfo, COMMAND_BUS_CONNECTOR_PATH, e);
             throw new IllegalArgumentException(e);
         }
@@ -163,7 +195,7 @@ public class SpringHttpCommandBusConnector implements CommandBusConnector {
                 localCommandBus.dispatch(commandMessage, replyFutureCallback);
                 return replyFutureCallback;
             } catch (Exception e) {
-                LOGGER.error("Could not dispatch command", e);
+                logger.error("Could not dispatch command", e);
                 return CompletableFuture.completedFuture(createReply(commandMessage, false, e));
             }
         } else {
@@ -171,7 +203,7 @@ public class SpringHttpCommandBusConnector implements CommandBusConnector {
                 localCommandBus.dispatch(commandMessage);
                 return CompletableFuture.completedFuture("");
             } catch (Exception e) {
-                LOGGER.error("Could not dispatch command", e);
+                logger.error("Could not dispatch command", e);
                 return CompletableFuture.completedFuture(createReply(commandMessage, false, e));
             }
         }
@@ -181,7 +213,7 @@ public class SpringHttpCommandBusConnector implements CommandBusConnector {
         try {
             return new SpringHttpReplyMessage<>(commandMessage.getIdentifier(), success, result, serializer);
         } catch (Exception e) {
-            LOGGER.warn("Could not serialize command reply [{}]. Sending back NULL.", result, e);
+            logger.warn("Could not serialize command reply [{}]. Sending back NULL.", result, e);
             return new SpringHttpReplyMessage(commandMessage.getIdentifier(), success, null, serializer);
         }
     }
