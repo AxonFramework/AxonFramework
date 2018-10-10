b61c7dfcef3fac22ea5b8f413531dc288730e40e
smcvb <smcvbeelen@gmail.com>
2018-10-10 11:48:45 +0200

    Move all axon-core/config classes to axon-configuration
    Move all axon-core/config classes to axon-configuration
    
    #755

--- configuration/src/main/java/org/axonframework/config/SagaConfiguration.java
+++ configuration/src/main/java/org/axonframework/config/SagaConfiguration.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,7 +25,6 @@ import org.axonframework.eventhandling.saga.SagaRepository;
 import org.axonframework.eventhandling.saga.repository.AnnotatedSagaRepository;
 import org.axonframework.eventhandling.saga.repository.SagaStore;
 
-import java.util.Optional;
 import java.util.function.Function;
 
 import static java.lang.String.format;
