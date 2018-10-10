16d790fdc44f9e7cdc8421ae0b38ca04026c8284
smcvb <smcvbeelen@gmail.com>
2018-10-10 11:47:41 +0200

    Add assertWithin()
    Add assertWithin()
    
    #755

--- core/src/main/java/org/axonframework/common/Assert.java
+++ core/src/main/java/org/axonframework/common/Assert.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2010-2017. Axon Framework
+ * Copyright (c) 2010-2018. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +17,7 @@
 package org.axonframework.common;
 
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -131,5 +132,30 @@ public abstract class Assert {
     public static <T, X extends Throwable> void assertNonNull(T value,
                                                               Supplier<? extends X> exceptionSupplier) throws X {
         assertThat(value, Objects::nonNull, exceptionSupplier);
+    }
+
+    // TODO Ok to move this guy here?
+    /**
+     * Assert that the given {@code assertion} succeeds with the given {@code time} and {@code unit}.
+     *
+     * @param time      an {@code int} which paired with the {@code unit} specifies the time in which the assertion must
+     *                  pass
+     * @param unit      a {@link TimeUnit} in which {@code time} is expressed
+     * @param assertion a {@link Runnable} containing the assertion to succeed within the deadline
+     */
+    public static void assertWithin(int time, TimeUnit unit, Runnable assertion) {
+        long now = System.currentTimeMillis();
+        long deadline = now + unit.toMillis(time);
+        do {
+            try {
+                assertion.run();
+                break;
+            } catch (AssertionError e) {
+                if (now >= deadline) {
+                    throw e;
+                }
+            }
+            now = System.currentTimeMillis();
+        } while (true);
     }
 }
