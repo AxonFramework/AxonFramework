3801fd1bee831288ebc713a1612ad000504ebb05
smcvb <smcvbeelen@gmail.com>
2018-10-12 09:46:58 +0200

    Change amqp module dependency from core to messaging
    -Change amqp module dependency from core to messaging
    -Small clean up action in pom file
    -Fix all broken import statements
    
    #755

--- amqp/src/main/java/org/axonframework/amqp/eventhandling/legacy/EventMessageType.java
+++ amqp/src/main/java/org/axonframework/amqp/eventhandling/legacy/EventMessageType.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2010-2014. Axon Framework
+ * Copyright (c) 2010-2018. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,8 +16,8 @@
 
 package org.axonframework.amqp.eventhandling.legacy;
 
+import org.axonframework.eventhandling.DomainEventMessage;
 import org.axonframework.eventhandling.EventMessage;
-import org.axonframework.eventsourcing.DomainEventMessage;
 
 /**
  * Enumeration of supported Message Types by the {@link EventMessageWriter} and {@link EventMessageReader}.
--- amqp/src/test/java/org/axonframework/amqp/eventhandling/DefaultAMQPMessageConverterTest.java
+++ amqp/src/test/java/org/axonframework/amqp/eventhandling/DefaultAMQPMessageConverterTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,10 +16,10 @@
 
 package org.axonframework.amqp.eventhandling;
 
+import org.axonframework.eventhandling.DomainEventMessage;
 import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.GenericDomainEventMessage;
 import org.axonframework.eventhandling.GenericEventMessage;
-import org.axonframework.eventsourcing.DomainEventMessage;
-import org.axonframework.eventsourcing.GenericDomainEventMessage;
 import org.axonframework.messaging.Headers;
 import org.axonframework.messaging.MetaData;
 import org.axonframework.serialization.xml.XStreamSerializer;
