55f30485efb6e3081da0dd0636e56110321d9267
smcvb <smcvbeelen@gmail.com>
2018-10-14 14:00:45 +0200

    Ignore data source auto config
    Ignore data source auto config
    
    #755

--- spring-boot-autoconfigure/src/test/java/org/axonframework/boot/AxonAutoConfigurationWithoutObjectMapperTest.java
+++ spring-boot-autoconfigure/src/test/java/org/axonframework/boot/AxonAutoConfigurationWithoutObjectMapperTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2010-2017. Axon Framework
+ * Copyright (c) 2010-2018. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,6 +23,7 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.context.ApplicationContext;
@@ -34,6 +35,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @EnableAutoConfiguration(exclude = {
         JmxAutoConfiguration.class,
         WebClientAutoConfiguration.class,
+        DataSourceAutoConfiguration.class,
         JacksonAutoConfiguration.class
 })
 @RunWith(SpringRunner.class)
--- spring-boot-autoconfigure/src/test/java/org/axonframework/boot/EventProcessorConfigurationTest.java
+++ spring-boot-autoconfigure/src/test/java/org/axonframework/boot/EventProcessorConfigurationTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -32,6 +32,7 @@ import org.junit.*;
 import org.junit.runner.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -50,7 +51,10 @@ import static org.junit.Assert.*;
 @SpringBootTest
 @TestPropertySource("classpath:test-processors.application.properties")
 @EnableAutoConfiguration(exclude = {
-        AMQPAutoConfiguration.class, JmxAutoConfiguration.class, WebClientAutoConfiguration.class
+        JmxAutoConfiguration.class,
+        WebClientAutoConfiguration.class,
+        DataSourceAutoConfiguration.class,
+        AMQPAutoConfiguration.class
 })
 @RunWith(SpringRunner.class)
 public class EventProcessorConfigurationTest {
