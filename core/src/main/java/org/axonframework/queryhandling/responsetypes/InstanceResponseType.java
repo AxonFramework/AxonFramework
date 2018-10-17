ad5d7c2613891f7c8fdcb765e65b73e2bff651a0
smcvb <smcvbeelen@gmail.com>
2018-10-17 09:55:53 +0200

    Provide a toString()
    Provide a toString() function to correctly describe the
    InstanceResponseType and MultipleInstancesResponseType of the
    NoHandlerForQueryException being thrown in the SimpleQueryBus
    
    Resolves #805

--- core/src/main/java/org/axonframework/queryhandling/responsetypes/InstanceResponseType.java
+++ core/src/main/java/org/axonframework/queryhandling/responsetypes/InstanceResponseType.java
@@ -19,7 +19,8 @@ public class InstanceResponseType<R> extends AbstractResponseType<R> {
 
     /**
      * Instantiate a {@link org.axonframework.queryhandling.responsetypes.InstanceResponseType} with the given
-     * {@code expectedResponseType} as the type to be matched against and to which the query response should be converted
+     * {@code expectedResponseType} as the type to be matched against and to which the query response should be
+     * converted
      * to.
      *
      * @param expectedResponseType the response type which is expected to be matched against and returned
@@ -47,5 +48,10 @@ public class InstanceResponseType<R> extends AbstractResponseType<R> {
     @Override
     public Class<R> responseMessagePayloadType() {
         return (Class<R>) expectedResponseType;
+    }
+
+    @Override
+    public String toString() {
+        return "InstanceResponseType{" + expectedResponseType + "}";
     }
 }
