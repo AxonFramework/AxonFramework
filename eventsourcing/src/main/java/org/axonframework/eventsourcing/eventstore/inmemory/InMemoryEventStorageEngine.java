aa7378226cb17f751abe718b35bbed522ad8b819
Jan Galinski <jan.galinski@holisticon.de>
2019-01-08 13:46:45 +0100

    reformat

--- eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/inmemory/InMemoryEventStorageEngine.java
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/inmemory/InMemoryEventStorageEngine.java
@@ -16,7 +16,13 @@
 
 package org.axonframework.eventsourcing.eventstore.inmemory;
 
-import static org.axonframework.eventhandling.EventUtils.asTrackedEventMessage;
+import org.axonframework.eventhandling.DomainEventMessage;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.GlobalSequenceTrackingToken;
+import org.axonframework.eventhandling.TrackedEventMessage;
+import org.axonframework.eventhandling.TrackingToken;
+import org.axonframework.eventsourcing.eventstore.DomainEventStream;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 
 import java.time.Instant;
 import java.util.Collections;
@@ -33,13 +39,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import org.axonframework.eventhandling.DomainEventMessage;
-import org.axonframework.eventhandling.EventMessage;
-import org.axonframework.eventhandling.GlobalSequenceTrackingToken;
-import org.axonframework.eventhandling.TrackedEventMessage;
-import org.axonframework.eventhandling.TrackingToken;
-import org.axonframework.eventsourcing.eventstore.DomainEventStream;
-import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+
+import static org.axonframework.eventhandling.EventUtils.asTrackedEventMessage;
 
 /**
  * Thread-safe event storage engine that stores events and snapshots in memory.
@@ -63,101 +64,98 @@ public class InMemoryEventStorageEngine implements EventStorageEngine {
     this.snapshots = snapshots;
   }
 
-  @Override
-  public void appendEvents(List<? extends EventMessage<?>> events) {
-    synchronized (this.events) {
-      GlobalSequenceTrackingToken trackingToken = nextTrackingToken();
-      this.events.putAll(IntStream.range(0, events.size()).mapToObj(
-          i -> asTrackedEventMessage((EventMessage<?>) events.get(i), trackingToken.offsetBy(i)))
-          .collect(
-              Collectors.toMap(TrackedEventMessage::trackingToken, Function.identity())));
+    @Override
+    public void appendEvents(List<? extends EventMessage<?>> events) {
+        synchronized (this.events) {
+            GlobalSequenceTrackingToken trackingToken = nextTrackingToken();
+            this.events.putAll(IntStream.range(0, events.size()).mapToObj(
+                    i -> asTrackedEventMessage((EventMessage<?>) events.get(i), trackingToken.offsetBy(i))).collect(
+                    Collectors.toMap(TrackedEventMessage::trackingToken, Function.identity())));
+        }
     }
-  }
 
-  @Override
-  public void storeSnapshot(DomainEventMessage<?> snapshot) {
-    snapshots.compute(snapshot.getAggregateIdentifier(), (aggregateId, snapshotsSoFar) -> {
-      if (snapshotsSoFar == null) {
-        CopyOnWriteArrayList<DomainEventMessage<?>> newSnapshots = new CopyOnWriteArrayList<>();
-        newSnapshots.add(snapshot);
-        return newSnapshots;
-      }
-      snapshotsSoFar.add(snapshot);
-      return snapshotsSoFar;
-    });
-  }
-
-  /**
-   * {@inheritDoc}
-   * <p>
-   * This implementation produces non-blocking event streams.
-   */
-  @Override
-  public Stream<? extends TrackedEventMessage<?>> readEvents(TrackingToken trackingToken,
-      boolean mayBlock) {
-    if (trackingToken == null) {
-      return events.values().stream();
+    @Override
+    public void storeSnapshot(DomainEventMessage<?> snapshot) {
+        snapshots.compute(snapshot.getAggregateIdentifier(), (aggregateId, snapshotsSoFar) -> {
+            if (snapshotsSoFar == null) {
+                CopyOnWriteArrayList<DomainEventMessage<?>> newSnapshots = new CopyOnWriteArrayList<>();
+                newSnapshots.add(snapshot);
+                return newSnapshots;
+            }
+            snapshotsSoFar.add(snapshot);
+            return snapshotsSoFar;
+        });
     }
-    return events.tailMap(trackingToken, false).values().stream();
-  }
 
-  @Override
-  public DomainEventStream readEvents(String aggregateIdentifier, long firstSequenceNumber) {
-    AtomicReference<Long> sequenceNumber = new AtomicReference<>();
-    Stream<? extends DomainEventMessage<?>> stream =
-        events.values().stream().filter(event -> event instanceof DomainEventMessage<?>)
-            .map(event -> (DomainEventMessage<?>) event)
-            .filter(event -> aggregateIdentifier.equals(event.getAggregateIdentifier())
-                && event.getSequenceNumber() >= firstSequenceNumber)
-            .peek(event -> sequenceNumber.set(event.getSequenceNumber()));
-    return DomainEventStream.of(stream, sequenceNumber::get);
-  }
-
-  @Override
-  public Optional<DomainEventMessage<?>> readSnapshot(String aggregateIdentifier) {
-    return snapshots.getOrDefault(aggregateIdentifier, Collections.emptyList())
-        .stream()
-        .max(Comparator.comparingLong(DomainEventMessage::getSequenceNumber));
-  }
-
-  @Override
-  public TrackingToken createTailToken() {
-    if (events.size() == 0) {
-      return null;
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This implementation produces non-blocking event streams.
+     */
+    @Override
+    public Stream<? extends TrackedEventMessage<?>> readEvents(TrackingToken trackingToken, boolean mayBlock) {
+        if (trackingToken == null) {
+            return events.values().stream();
+        }
+        return events.tailMap(trackingToken, false).values().stream();
     }
-    GlobalSequenceTrackingToken firstToken = (GlobalSequenceTrackingToken) events.firstKey();
-    return new GlobalSequenceTrackingToken(firstToken.getGlobalIndex() - 1);
-  }
 
-  @Override
-  public TrackingToken createHeadToken() {
-    if (events.size() == 0) {
-      return null;
+    @Override
+    public DomainEventStream readEvents(String aggregateIdentifier, long firstSequenceNumber) {
+        AtomicReference<Long> sequenceNumber = new AtomicReference<>();
+        Stream<? extends DomainEventMessage<?>> stream =
+                events.values().stream().filter(event -> event instanceof DomainEventMessage<?>)
+                      .map(event -> (DomainEventMessage<?>) event)
+                      .filter(event -> aggregateIdentifier.equals(event.getAggregateIdentifier())
+                              && event.getSequenceNumber() >= firstSequenceNumber)
+                      .peek(event -> sequenceNumber.set(event.getSequenceNumber()));
+        return DomainEventStream.of(stream, sequenceNumber::get);
     }
-    return events.lastKey();
-  }
 
-  @Override
-  public TrackingToken createTokenAt(Instant dateTime) {
-    return events.values()
-        .stream()
-        .filter(event -> event.getTimestamp().equals(dateTime) || event.getTimestamp()
-            .isAfter(dateTime))
-        .min(Comparator.comparingLong(e -> ((GlobalSequenceTrackingToken) e.trackingToken())
-            .getGlobalIndex()))
-        .map(TrackedEventMessage::trackingToken)
-        .map(tt -> (GlobalSequenceTrackingToken) tt)
-        .map(tt -> new GlobalSequenceTrackingToken(tt.getGlobalIndex() - 1))
-        .orElse(null);
-  }
+    @Override
+    public Optional<DomainEventMessage<?>> readSnapshot(String aggregateIdentifier) {
+        return snapshots.getOrDefault(aggregateIdentifier, Collections.emptyList())
+                        .stream()
+                        .max(Comparator.comparingLong(DomainEventMessage::getSequenceNumber));
+    }
 
-  /**
-   * Returns the tracking token to use for the next event to be stored.
-   *
-   * @return the tracking token for the next event
-   */
-  protected GlobalSequenceTrackingToken nextTrackingToken() {
-    return events.isEmpty() ? new GlobalSequenceTrackingToken(0) :
-        ((GlobalSequenceTrackingToken) events.lastKey()).next();
-  }
+    @Override
+    public TrackingToken createTailToken() {
+        if (events.size() == 0) {
+            return null;
+        }
+        GlobalSequenceTrackingToken firstToken = (GlobalSequenceTrackingToken) events.firstKey();
+        return new GlobalSequenceTrackingToken(firstToken.getGlobalIndex() - 1);
+    }
+
+    @Override
+    public TrackingToken createHeadToken() {
+        if (events.size() == 0) {
+            return null;
+        }
+        return events.lastKey();
+    }
+
+    @Override
+    public TrackingToken createTokenAt(Instant dateTime) {
+        return events.values()
+                     .stream()
+                     .filter(event -> event.getTimestamp().equals(dateTime) || event.getTimestamp().isAfter(dateTime))
+                     .min(Comparator.comparingLong(e -> ((GlobalSequenceTrackingToken) e.trackingToken())
+                             .getGlobalIndex()))
+                     .map(TrackedEventMessage::trackingToken)
+                     .map(tt -> (GlobalSequenceTrackingToken) tt)
+                     .map(tt -> new GlobalSequenceTrackingToken(tt.getGlobalIndex() - 1))
+                     .orElse(null);
+    }
+
+    /**
+     * Returns the tracking token to use for the next event to be stored.
+     *
+     * @return the tracking token for the next event
+     */
+    protected GlobalSequenceTrackingToken nextTrackingToken() {
+        return events.isEmpty() ? new GlobalSequenceTrackingToken(0) :
+                ((GlobalSequenceTrackingToken) events.lastKey()).next();
+    }
 }
