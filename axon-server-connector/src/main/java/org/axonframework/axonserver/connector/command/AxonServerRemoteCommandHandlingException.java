a2587e0a6e688aa9ee675bf494a1b5b8f1c7c371
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-02-05 15:03:46 +0100

    Update copyright notices
    Update copyright notices

--- axon-server-connector/src/main/java/org/axonframework/axonserver/connector/command/AxonServerRemoteCommandHandlingException.java
+++ axon-server-connector/src/main/java/org/axonframework/axonserver/connector/command/AxonServerRemoteCommandHandlingException.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.axonframework.axonserver.connector.command;
 
 import io.axoniq.axonserver.grpc.ErrorMessage;
