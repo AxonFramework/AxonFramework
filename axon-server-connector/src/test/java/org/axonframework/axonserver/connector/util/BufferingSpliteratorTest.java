cacad3885b1f518ad0833149471d842beb2d7e91
Allard Buijze <allard.buijze@axoniq.io>
2019-07-17 16:05:24 +0200

    Improved BufferingSpliteratorTest
    Added some assertions to validate that given timeouts are respected.

--- axon-server-connector/src/test/java/org/axonframework/axonserver/connector/util/BufferingSpliteratorTest.java
+++ axon-server-connector/src/test/java/org/axonframework/axonserver/connector/util/BufferingSpliteratorTest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2018. AxonIQ
+ * Copyright (c) 2010-2019. Axon Framework
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,51 +20,55 @@ import org.junit.Test;
 
 import java.time.Instant;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import java.util.stream.StreamSupport;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.axonframework.axonserver.connector.utils.AssertUtils.assertWithin;
+import static org.junit.Assert.*;
 
 /**
  * Author: marc
  */
 public class BufferingSpliteratorTest {
 
-    @Test
+    private static final Consumer<Object> IGNORE = s -> {};
+
+    @Test(timeout = 1000)
     public void testTimeout() {
-        BufferingSpliterator<String> bufferingSpliterator = new BufferingSpliterator<>(Instant.now().plusSeconds(1));
-        assertFalse(bufferingSpliterator.tryAdvance(s -> System.out.println(s)));
+        BufferingSpliterator<String> bufferingSpliterator = new BufferingSpliterator<>(Instant.now().plusMillis(250));
+        long t1 = System.currentTimeMillis();
+        assertFalse(bufferingSpliterator.tryAdvance(IGNORE));
+        long t2 = System.currentTimeMillis();
+        assertTrue("Expected at least 150 millis to have elapsed", t2 - t1 > 150);
     }
 
-    @Test
+    @Test(timeout = 10000)
     public void testCompleteWithNull() {
-        BufferingSpliterator<String> bufferingSpliterator = new BufferingSpliterator<>(Instant.now().plusSeconds(1));
-        Thread queueListener = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                bufferingSpliterator.tryAdvance(s -> System.out.println(s));
-
-            }
-        });
+        BufferingSpliterator<String> bufferingSpliterator = new BufferingSpliterator<>(Instant.now().plusSeconds(10));
+        Thread queueListener = new Thread(() -> bufferingSpliterator.tryAdvance(IGNORE));
         queueListener.start();
         bufferingSpliterator.cancel(null);
+        assertWithin(500, TimeUnit.MILLISECONDS, () -> assertFalse(queueListener.isAlive()));
     }
 
     @Test
     public void testCompleteWithValues() {
         BufferingSpliterator<String> bufferingSpliterator = new BufferingSpliterator<>(Instant.now().plusSeconds(1));
+        List<String> items = new CopyOnWriteArrayList<>();
         Thread queueListener = new Thread(new Runnable() {
             @Override
             public void run() {
-                List<String> items = StreamSupport.stream(bufferingSpliterator, false).collect(Collectors.toList());
-                assertEquals(2, items.size());
+                StreamSupport.stream(bufferingSpliterator, false).forEach(items::add);
             }
         });
         queueListener.start();
         bufferingSpliterator.put("One");
         bufferingSpliterator.put("Two");
         bufferingSpliterator.cancel(null);
+
+        assertWithin(1, TimeUnit.SECONDS, () -> assertEquals(2, items.size()));
     }
 
 }
