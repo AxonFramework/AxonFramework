4e3b82c2cd54ca6c64e756b193837ce07eb5663c
Allard Buijze <allard.buijze@axoniq.io>
2019-07-29 10:32:50 +0200

    Improved timing in flaky tests

--- eventsourcing/src/test/java/org/axonframework/eventsourcing/MultiStreamableMessageSourceTest.java
+++ eventsourcing/src/test/java/org/axonframework/eventsourcing/MultiStreamableMessageSourceTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010-2019. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.axonframework.eventsourcing;
 
 import org.axonframework.common.stream.BlockingStream;
@@ -62,9 +78,10 @@ public class MultiStreamableMessageSourceTest {
 
         long beforePollTime = System.currentTimeMillis();
         assertFalse(singleEventStream.hasNextAvailable(100, TimeUnit.MILLISECONDS));
-        long afterPollTime = System.currentTimeMillis();
-        assertTrue(afterPollTime - beforePollTime > 90);
-        assertTrue(afterPollTime - beforePollTime < 105);
+        long pollTime = System.currentTimeMillis() - beforePollTime;
+        // allow for some deviation in polling time
+        assertTrue("Poll time too short: " + pollTime + "ms", pollTime > 80);
+        assertTrue("Poll time too long: " + pollTime + "ms", pollTime < 120);
 
         singleEventStream.close();
     }
