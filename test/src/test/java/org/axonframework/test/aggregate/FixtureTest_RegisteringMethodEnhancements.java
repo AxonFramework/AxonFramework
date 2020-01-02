2ae740d9bd44d427426e83c29f2099ff85179198
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-01-02 13:54:07 +0100

    Add tests for registering handler specifics
    Add tests which use a FixtureConfiguration of the AnnotatedAggregate,
    which add mock version of a HandlerDefinition and
    HandlerEnhancerDefinition which toggle an AtomicBoolean for verification
    
    #1049

---
+++ test/src/test/java/org/axonframework/test/aggregate/FixtureTest_RegisteringMethodEnhancements.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2010-2019. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.test.aggregate;
+
+import org.axonframework.messaging.annotation.HandlerDefinition;
+import org.axonframework.messaging.annotation.HandlerEnhancerDefinition;
+import org.axonframework.messaging.annotation.MessageHandlingMember;
+import org.axonframework.messaging.annotation.ParameterResolverFactory;
+import org.junit.jupiter.api.*;
+
+import java.lang.reflect.Executable;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.axonframework.test.matchers.Matchers.exactSequenceOf;
+import static org.axonframework.test.matchers.Matchers.predicate;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This test class is intended to test whether the registration of a {@link org.axonframework.messaging.annotation.HandlerDefinition}
+ * and a {@link org.axonframework.messaging.annotation.HandlerEnhancerDefinition} go according to plan.
+ *
+ * @author Steven van Beelen
+ */
+public class FixtureTest_RegisteringMethodEnhancements {
+
+    private FixtureConfiguration<AnnotatedAggregate> testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new AggregateTestFixture<>(AnnotatedAggregate.class);
+        testSubject.registerInjectableResource(new HardToCreateResource());
+    }
+
+    @Test
+    void testCreateHandlerMethodIsCalledForRegisteredCustomHandlerDefinition() {
+        AtomicBoolean handlerDefinitionReached = new AtomicBoolean(false);
+
+        testSubject.registerHandlerDefinition(new TestHandlerDefinition(handlerDefinitionReached))
+                   .givenNoPriorActivity()
+                   .when(new CreateAggregateCommand("aggregate-identifier"))
+                   .expectEventsMatching(exactSequenceOf(predicate(
+                           eventMessage -> eventMessage.getPayloadType().isAssignableFrom(MyEvent.class)
+                   )));
+
+        assertTrue(handlerDefinitionReached.get());
+    }
+
+    @Test
+    void testCreateHandlerMethodIsCalledForRegisteredCustomHandlerEnhancerDefinition() {
+        AtomicBoolean handlerEnhancerReached = new AtomicBoolean(false);
+
+        testSubject.registerHandlerEnhancerDefinition(new TestHandlerEnhancerDefinition(handlerEnhancerReached))
+                   .givenNoPriorActivity()
+                   .when(new CreateAggregateCommand("aggregate-identifier"))
+                   .expectEventsMatching(exactSequenceOf(predicate(
+                           eventMessage -> eventMessage.getPayloadType().isAssignableFrom(MyEvent.class)
+                   )));
+
+        assertTrue(handlerEnhancerReached.get());
+    }
+
+    private static class TestHandlerDefinition implements HandlerDefinition {
+
+        private final AtomicBoolean assertion;
+
+        public TestHandlerDefinition(AtomicBoolean assertion) {
+            this.assertion = assertion;
+        }
+
+        @Override
+        public <T> Optional<MessageHandlingMember<T>> createHandler(Class<T> declaringType,
+                                                                    Executable executable,
+                                                                    ParameterResolverFactory parameterResolverFactory) {
+            assertion.set(true);
+            // We do not care about a specific MessageHandlingMember,
+            //  only that this method is called to ensure its part of the FixtureConfiguration.
+            return Optional.empty();
+        }
+    }
+
+    private static class TestHandlerEnhancerDefinition implements HandlerEnhancerDefinition {
+
+        private final AtomicBoolean assertion;
+
+        private TestHandlerEnhancerDefinition(AtomicBoolean assertion) {
+            this.assertion = assertion;
+        }
+
+        @Override
+        public <T> MessageHandlingMember<T> wrapHandler(MessageHandlingMember<T> original) {
+            assertion.set(true);
+            // We do not care about a specific MessageHandlingMember,
+            //  only that this method is called to ensure its part of the FixtureConfiguration.
+            return original;
+        }
+    }
+}
