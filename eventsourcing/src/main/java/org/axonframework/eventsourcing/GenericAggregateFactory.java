4bb185f183780adb74be08a2f1d591c636bf86e8
Milan Savic <milan.savic@axoniq.io>
2020-01-09 09:41:52 +0100

    AggregatePolymorphism - event sourcing

--- eventsourcing/src/main/java/org/axonframework/eventsourcing/GenericAggregateFactory.java
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/GenericAggregateFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2018. Axon Framework
+ * Copyright (c) 2010-2020. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,10 +18,12 @@ package org.axonframework.eventsourcing;
 
 import org.axonframework.common.Assert;
 import org.axonframework.eventhandling.DomainEventMessage;
+import org.axonframework.modelling.command.AggregateRoot;
+import org.axonframework.modelling.command.inspection.AggregateModel;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
+import java.util.Optional;
 
 import static java.lang.String.format;
 import static org.axonframework.common.ReflectionUtils.ensureAccessible;
@@ -39,8 +41,6 @@ import static org.axonframework.common.ReflectionUtils.ensureAccessible;
  */
 public class GenericAggregateFactory<T> extends AbstractAggregateFactory<T> {
 
-    private final Constructor<T> constructor;
-
     /**
      * Initialize the AggregateFactory for creating instances of the given {@code aggregateType}.
      *
@@ -48,16 +48,27 @@ public class GenericAggregateFactory<T> extends AbstractAggregateFactory<T> {
      * @throws IncompatibleAggregateException if the aggregate constructor throws an exception, or if the JVM security
      *                                        settings prevent the GenericAggregateFactory from calling the
      *                                        constructor.
+     * @deprecated use {@link #GenericAggregateFactory(AggregateModel)} instead
      */
+    @Deprecated
     public GenericAggregateFactory(Class<T> aggregateType) {
         super(aggregateType);
         Assert.isFalse(Modifier.isAbstract(aggregateType.getModifiers()), () -> "Given aggregateType may not be abstract");
         try {
-            this.constructor = ensureAccessible(aggregateType.getDeclaredConstructor());
+            ensureAccessible(aggregateType.getDeclaredConstructor());
         } catch (NoSuchMethodException e) {
             throw new IncompatibleAggregateException(format("The aggregate [%s] doesn't provide a no-arg constructor.",
                                                             aggregateType.getSimpleName()), e);
         }
+    }
+
+    /**
+     * Initialize the AggregateFactory for creating instances of the given {@code aggregateModel}.
+     *
+     * @param aggregateModel the model of aggregate this factory creates instances of
+     */
+    public GenericAggregateFactory(AggregateModel<T> aggregateModel) {
+        super(aggregateModel);
     }
 
     /**
@@ -71,21 +82,52 @@ public class GenericAggregateFactory<T> extends AbstractAggregateFactory<T> {
     @SuppressWarnings({"unchecked"})
     @Override
     protected T doCreateAggregate(String aggregateIdentifier, DomainEventMessage firstEvent) {
+        return createByAggregateRootAnnotation(firstEvent.getType())
+                .orElse(createBySimpleType(firstEvent.getType())
+                                .orElseThrow(() -> new IncompatibleAggregateException(format(
+                                        "The [%s] aggregate does not exist.",
+                                        firstEvent.getType()))));
+    }
+
+    private Optional<T> createByAggregateRootAnnotation(String firstEventType) {
+        return aggregateModel.allEventHandlers()
+                             .keySet()
+                             .stream()
+                             .filter(t -> t.isAnnotationPresent(AggregateRoot.class))
+                             .filter(t -> t.getAnnotation(AggregateRoot.class).type().equals(firstEventType))
+                             .map(this::newInstance)
+                             .findFirst();
+    }
+
+    private Optional<T> createBySimpleType(String firstEventType) {
+        return aggregateModel.allEventHandlers()
+                             .keySet()
+                             .stream()
+                             .filter(t -> t.getSimpleName().equals(firstEventType))
+                             .map(this::newInstance)
+                             .findFirst();
+    }
+
+    @SuppressWarnings("unchecked")
+    private T newInstance(Class<?> type) {
         try {
-            return constructor.newInstance();
+            return (T) ensureAccessible(type.getDeclaredConstructor()).newInstance();
         } catch (InstantiationException e) {
             throw new IncompatibleAggregateException(format(
                     "The aggregate [%s] does not have a suitable no-arg constructor.",
-                    getAggregateType().getSimpleName()), e);
+                    type.getSimpleName()), e);
         } catch (IllegalAccessException e) {
             throw new IncompatibleAggregateException(format(
                     "The aggregate no-arg constructor of the aggregate [%s] is not accessible. Please ensure that "
                             + "the constructor is public or that the Security Manager allows access through "
-                            + "reflection.", getAggregateType().getSimpleName()), e);
+                            + "reflection.", type.getSimpleName()), e);
         } catch (InvocationTargetException e) {
             throw new IncompatibleAggregateException(format(
                     "The no-arg constructor of [%s] threw an exception on invocation.",
-                    getAggregateType().getSimpleName()), e);
+                    type.getSimpleName()), e);
+        } catch (NoSuchMethodException e) {
+            throw new IncompatibleAggregateException(format("The aggregate [%s] doesn't provide a no-arg constructor.",
+                                                            type.getSimpleName()), e);
         }
     }
 }
--- eventsourcing/src/test/java/org/axonframework/eventsourcing/GenericAggregateFactoryTest.java
+++ eventsourcing/src/test/java/org/axonframework/eventsourcing/GenericAggregateFactoryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2018. Axon Framework
+ * Copyright (c) 2010-2020. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,7 +51,7 @@ class GenericAggregateFactoryTest {
     @Test
     void testInitializeFromAggregateSnapshot() {
         StubAggregate aggregate = new StubAggregate("stubId");
-        DomainEventMessage<StubAggregate> snapshotMessage = new GenericDomainEventMessage<>("type", aggregate.getIdentifier(),
+        DomainEventMessage<StubAggregate> snapshotMessage = new GenericDomainEventMessage<>("StubAggregate", aggregate.getIdentifier(),
                                                                                             2, aggregate);
         GenericAggregateFactory<StubAggregate> factory = new GenericAggregateFactory<>(StubAggregate.class);
         assertSame(aggregate, factory.createAggregateRoot(aggregate.getIdentifier(), snapshotMessage));
