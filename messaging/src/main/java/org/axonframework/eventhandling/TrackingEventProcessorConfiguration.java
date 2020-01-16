5dacc563aa3e2d7de8f41867c32c506ed0058a34
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-01-16 12:04:31 +0100

    Resolve warnings and indentation
    Resolve warnings and indentation
    
    #boy-scouting

--- messaging/src/main/java/org/axonframework/eventhandling/TrackingEventProcessorConfiguration.java
+++ messaging/src/main/java/org/axonframework/eventhandling/TrackingEventProcessorConfiguration.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2010-2019. Axon Framework
+ * Copyright (c) 2010-2020. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,12 +28,13 @@ import java.util.function.Function;
 import static org.axonframework.common.BuilderUtils.assertThat;
 
 /**
- * Configuration object for the {@link TrackingEventProcessor}. The TrackingEventProcessorConfiguration provides access to the options to tweak
- * various settings. Instances are not thread-safe and should not be altered after they have been used to initialize
- * a TrackingEventProcessor.
+ * Configuration object for the {@link TrackingEventProcessor}. The TrackingEventProcessorConfiguration provides access
+ * to the options to tweak various settings. Instances are not thread-safe and should not be altered after they have
+ * been used to initialize a TrackingEventProcessor.
  *
  * @author Christophe Bouhier
  * @author Allard Buijze
+ * @since 3.1
  */
 public class TrackingEventProcessorConfiguration {
 
@@ -44,7 +45,7 @@ public class TrackingEventProcessorConfiguration {
     private final int maxThreadCount;
     private int batchSize;
     private int initialSegmentCount;
-    private Function<StreamableMessageSource, TrackingToken> initialTrackingTokenBuilder = StreamableMessageSource::createTailToken;
+    private Function<StreamableMessageSource<TrackedEventMessage<?>>, TrackingToken> initialTrackingTokenBuilder = StreamableMessageSource::createTailToken;
     private Function<String, ThreadFactory> threadFactory;
     private long tokenClaimInterval;
     private int eventAvailabilityTimeout = 1000;
@@ -59,9 +60,9 @@ public class TrackingEventProcessorConfiguration {
     }
 
     /**
-     * Initialize a configuration instance with the given {@code threadCount}. This is both the number of threads
-     * that a processor will start for processing, as well as the initial number of segments that will be created when
-     * the processor is first started.
+     * Initialize a configuration instance with the given {@code threadCount}. This is both the number of threads that a
+     * processor will start for processing, as well as the initial number of segments that will be created when the
+     * processor is first started.
      *
      * @param threadCount the number of segments to process in parallel
      * @return a newly created configuration
@@ -113,13 +114,13 @@ public class TrackingEventProcessorConfiguration {
      * Set the duration where a Tracking Processor will wait for the availability of Events, in each cycle, before
      * extending the claim on the tokens it owns.
      * <p>
-     * Note that some storage engines for the EmbeddedEventStore do not support streaming. They may poll for
-     * messages once on an {@link TrackingEventStream#hasNextAvailable(int, TimeUnit)} invocation, and wait for the
-     * timeout to occur.
+     * Note that some storage engines for the EmbeddedEventStore do not support streaming. They may poll for messages
+     * once on an {@link TrackingEventStream#hasNextAvailable(int, TimeUnit)} invocation, and wait for the timeout to
+     * occur.
      * <p>
-     * This value should be significantly shorter than the claim timeout configured on the Token Store. Failure to
-     * do so may cause claims to be stolen while a tread is waiting for events. Also, with very long timeouts, it will
-     * take longer for threads to pick up the instructions they need to process.
+     * This value should be significantly shorter than the claim timeout configured on the Token Store. Failure to do so
+     * may cause claims to be stolen while a tread is waiting for events. Also, with very long timeouts, it will take
+     * longer for threads to pick up the instructions they need to process.
      * <p>
      * Defaults to 1 second.
      * <p>
@@ -131,7 +132,8 @@ public class TrackingEventProcessorConfiguration {
      */
     public TrackingEventProcessorConfiguration andEventAvailabilityTimeout(long interval, TimeUnit unit) {
         long i = unit.toMillis(interval);
-        assertThat(i, it -> it <= Integer.MAX_VALUE, "Interval may not be longer than Integer.MAX_VALUE milliseconds long");
+        assertThat(i, it -> it <= Integer.MAX_VALUE,
+                   "Interval may not be longer than Integer.MAX_VALUE milliseconds long");
         assertThat(i, it -> it > 0, "Interval must be strictly positive");
         this.eventAvailabilityTimeout = (int) i;
         return this;
@@ -145,7 +147,7 @@ public class TrackingEventProcessorConfiguration {
      * @return {@code this} for method chaining
      */
     public TrackingEventProcessorConfiguration andInitialTrackingToken(
-            Function<StreamableMessageSource, TrackingToken> initialTrackingTokenBuilder) {
+            Function<StreamableMessageSource<TrackedEventMessage<?>>, TrackingToken> initialTrackingTokenBuilder) {
         this.initialTrackingTokenBuilder = initialTrackingTokenBuilder;
         return this;
     }
@@ -179,7 +181,7 @@ public class TrackingEventProcessorConfiguration {
     /**
      * @return the Builder of initial tracking token
      */
-    public Function<StreamableMessageSource, TrackingToken> getInitialTrackingToken() {
+    public Function<StreamableMessageSource<TrackedEventMessage<?>>, TrackingToken> getInitialTrackingToken() {
         return initialTrackingTokenBuilder;
     }
 
@@ -191,8 +193,8 @@ public class TrackingEventProcessorConfiguration {
     }
 
     /**
-     * @return the time, in milliseconds, that a processor should wait for available events before going into a cycle
-     * of updating claims and checking for incoming instructions.
+     * @return the time, in milliseconds, that a processor should wait for available events before going into a cycle of
+     * updating claims and checking for incoming instructions.
      */
     public int getEventAvailabilityTimeout() {
         return eventAvailabilityTimeout;
