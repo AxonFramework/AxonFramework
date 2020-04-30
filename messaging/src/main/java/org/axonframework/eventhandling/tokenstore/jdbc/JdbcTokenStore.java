46623e2ab51eb0c0105f9db3f814b14840a09445
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-04-30 10:05:49 +0200

    Fix review comments
    Fix review comments around javadoc
    
    #1303

--- messaging/src/main/java/org/axonframework/eventhandling/tokenstore/jdbc/JdbcTokenStore.java
+++ messaging/src/main/java/org/axonframework/eventhandling/tokenstore/jdbc/JdbcTokenStore.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2010-2019. Axon Framework
+ * Copyright (c) 2010-2020. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -202,9 +202,9 @@ public class JdbcTokenStore implements TokenStore {
     }
 
     /**
-     * Returns the serialized used by the Token Store to serialize tokens.
+     * Returns the serializer used by the Token Store to serialize tokens.
      *
-     * @return the serialized used by the Token Store to serialize tokens
+     * @return the serializer used by the Token Store to serialize tokens
      */
     public Serializer serializer() {
         return serializer;
