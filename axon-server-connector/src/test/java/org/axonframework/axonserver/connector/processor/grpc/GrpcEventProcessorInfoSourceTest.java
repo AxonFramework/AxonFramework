6205aca50cdf54df46f7af0bcb6027423f4cdb5a
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-04-30 11:58:16 +0200

    Fix test case
    Fix NullPointerException upon creations of the
    TrackingEventProcessorInfoMessage due to the new getTokenStoreIdentifier
     method
    
    #1409

--- axon-server-connector/src/test/java/org/axonframework/axonserver/connector/processor/grpc/GrpcEventProcessorInfoSourceTest.java
+++ axon-server-connector/src/test/java/org/axonframework/axonserver/connector/processor/grpc/GrpcEventProcessorInfoSourceTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010-2020. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.axonframework.axonserver.connector.processor.grpc;
 
 import com.google.common.collect.ImmutableMap;
@@ -24,14 +40,15 @@ class GrpcEventProcessorInfoSourceTest {
     private final EventProcessingConfiguration eventProcessingConfiguration = mock(EventProcessingConfiguration.class);
     private final AxonServerConnectionManager axonServerConnectionManager = mock(AxonServerConnectionManager.class);
 
-    private GrpcEventProcessorInfoSource testSubject = new GrpcEventProcessorInfoSource(
+    private final GrpcEventProcessorInfoSource testSubject = new GrpcEventProcessorInfoSource(
             eventProcessingConfiguration, axonServerConnectionManager, BOUNDED_CONTEXT
     );
 
     @Test
     void testNotifyInformation() {
-        EventProcessor mockEventProcessor = mock(TrackingEventProcessor.class);
+        TrackingEventProcessor mockEventProcessor = mock(TrackingEventProcessor.class);
         when(mockEventProcessor.getName()).thenReturn("eventProcessor");
+        when(mockEventProcessor.getTokenStoreIdentifier()).thenReturn("tokenStoreIdentifier");
         Map<String, EventProcessor> testEventProcessors = ImmutableMap.of("eventProcessor", mockEventProcessor);
         when(eventProcessingConfiguration.eventProcessors()).thenReturn(testEventProcessors);
 
