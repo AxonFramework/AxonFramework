7b8a1862f711e2093751fc81a37c16599789dac2
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-05-15 17:26:42 +0200

    Change ResetHandler annotation
    Change the ResetHandler annotation to  specifically cope with message of
     type ResetMessage instead of EventMessages containing the
     ResetTriggeredEvent
    
    #1390

--- messaging/src/main/java/org/axonframework/eventhandling/ResetTriggeredEvent.java
+++ messaging/src/main/java/org/axonframework/eventhandling/ResetTriggeredEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2018. Axon Framework
+ * Copyright (c) 2010-2020. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,9 @@ package org.axonframework.eventhandling;
  * @author Allard Buijze
  * @see ResetHandler @ResetHandler
  * @since 3.2
+ * @deprecated in favor of the {@link org.axonframework.eventhandling.replay.ResetMessage}
  */
+@Deprecated
 public class ResetTriggeredEvent {
 
 }
