e16661e500bbc82671861947282082df6690e16a
Ivan Dugalic <ivan.dugalic@axoniq.io>
2020-06-16 22:03:17 +0200

    Added JavaDoc

--- metrics-micrometer/src/main/java/org/axonframework/micrometer/TagsUtil.java
+++ metrics-micrometer/src/main/java/org/axonframework/micrometer/TagsUtil.java
@@ -1,5 +1,11 @@
 package org.axonframework.micrometer;
 
+/**
+ * Utility class for micrometer tag management.
+ * <p>
+ * Contains 'static final' fields which represent the micrometer tag KEY that should be consistent across different
+ * metrics.
+ */
 public class TagsUtil {
 
     public static final String PAYLOAD_TYPE_TAG = "payloadType";
