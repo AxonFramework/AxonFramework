c4f1fe73d136d894141205d89f8bc83df8ffa825
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-07-21 13:54:10 +0200

    Include method target
    Include this annotation to be allowed on methods
    
    #1089

--- modelling/src/main/java/org/axonframework/modelling/command/AggregateVersion.java
+++ modelling/src/main/java/org/axonframework/modelling/command/AggregateVersion.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2018. Axon Framework
+ * Copyright (c) 2010-2020. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ import java.lang.annotation.Target;
  * @author Allard Buijze
  * @since 2.0
  */
-@Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AggregateVersion {
 
