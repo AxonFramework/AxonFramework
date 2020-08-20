b0f2b7b5f3d0c9171a8cde40c44d5e9ca3aa196d
Kristian Rosenvold <kristian.rosenvold@gmail.com>
2020-08-20 08:34:15 +0200

    Added support for random variation to snapshotting trigger

--- eventsourcing/src/main/java/org/axonframework/eventsourcing/EventCountSnapshotTriggerDefinition.java
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/EventCountSnapshotTriggerDefinition.java
@@ -21,6 +21,7 @@ import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.messaging.unitofwork.CurrentUnitOfWork;
 
 import java.io.Serializable;
+import java.util.Random;
 
 /**
  * Snapshotter trigger mechanism that counts the number of events to decide when to create a snapshot. A snapshot is
@@ -33,6 +34,8 @@ public class EventCountSnapshotTriggerDefinition implements SnapshotTriggerDefin
 
     private final Snapshotter snapshotter;
     private final int threshold;
+    private final int variation;
+
 
     /**
      * Initialized the SnapshotTriggerDefinition to threshold snapshots using the given {@code snapshotter}
@@ -44,11 +47,28 @@ public class EventCountSnapshotTriggerDefinition implements SnapshotTriggerDefin
     public EventCountSnapshotTriggerDefinition(Snapshotter snapshotter, int threshold) {
         this.snapshotter = snapshotter;
         this.threshold = threshold;
+        this.variation = 0;
+    }
+
+    /**
+     * Initialized the SnapshotTriggerDefinition to threshold snapshots using the given {@code snapshotter}
+     * when {@code threshold + rand.nextInt(variation)} events have been applied to an Aggregate instance
+     *
+     * @param snapshotter the snapshotter to notify when a snapshot needs to be taken
+     * @param threshold   the number of events that will threshold the creation of a snapshot event
+     * @param variation   The maximal amount that will randomly be added to threshold per invocation.
+     *                    Used in a cluster to avoid multiple nodes doing snapshotting at the same time for the same aggregates, which
+     *                    in some use cases can happen quite often.
+     */
+    public EventCountSnapshotTriggerDefinition(Snapshotter snapshotter, int threshold, int variation) {
+        this.snapshotter = snapshotter;
+        this.threshold = threshold;
+        this.variation = variation;
     }
 
     @Override
     public SnapshotTrigger prepareTrigger(Class<?> aggregateType) {
-        return new EventCountSnapshotTrigger(snapshotter, aggregateType, threshold);
+        return new EventCountSnapshotTrigger(snapshotter, aggregateType, threshold, variation);
     }
 
     @Override
@@ -57,7 +77,7 @@ public class EventCountSnapshotTriggerDefinition implements SnapshotTriggerDefin
             ((EventCountSnapshotTrigger) trigger).setSnapshotter(snapshotter);
             return trigger;
         }
-        return new EventCountSnapshotTrigger(snapshotter, aggregateType, threshold);
+        return new EventCountSnapshotTrigger(snapshotter, aggregateType, threshold, variation);
     }
 
     private static class EventCountSnapshotTrigger implements SnapshotTrigger, Serializable {
@@ -67,16 +87,29 @@ public class EventCountSnapshotTriggerDefinition implements SnapshotTriggerDefin
 
         private transient Snapshotter snapshotter;
         private int counter = 0;
+        private final int variation;
+        private final Random rand = new Random();
+
 
         public EventCountSnapshotTrigger(Snapshotter snapshotter, Class<?> aggregateType, int threshold) {
             this.snapshotter = snapshotter;
             this.aggregateType = aggregateType;
             this.threshold = threshold;
+            this.variation = 0;
         }
+
+        public EventCountSnapshotTrigger(Snapshotter snapshotter, Class<?> aggregateType, int threshold, int variation) {
+            this.snapshotter = snapshotter;
+            this.aggregateType = aggregateType;
+            this.threshold = threshold;
+            this.variation = variation;
+        }
+
 
         @Override
         public void eventHandled(EventMessage<?> msg) {
-            if (++counter >= threshold && msg instanceof DomainEventMessage) {
+            int actualThreshold = variation > 0 ? threshold + rand.nextInt(variation) : threshold;
+            if (++counter >= actualThreshold && msg instanceof DomainEventMessage) {
                 if (CurrentUnitOfWork.isStarted()) {
                     CurrentUnitOfWork.get().onPrepareCommit(
                             u -> scheduleSnapshot((DomainEventMessage) msg));
