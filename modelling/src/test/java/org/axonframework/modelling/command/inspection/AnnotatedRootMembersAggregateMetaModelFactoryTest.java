b26d01543403371f2685cbe56ebcab93925249a0
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-11-03 17:07:13 +0100

    Add a test case validating correct amount of handler invocations
    Add a test case which creates a polymorphic aggregate with aggregate
    members on the root. Validate in this test case that the AggregateEvent
    handler will only be invoked twice (once for the root and once for the
    member) and that the MemberEvent is only invoked once (for the member).

---
+++ modelling/src/test/java/org/axonframework/modelling/command/inspection/AnnotatedRootMembersAggregateMetaModelFactoryTest.java
@@ -0,0 +1,116 @@
+package org.axonframework.modelling.command.inspection;
+
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.GenericEventMessage;
+import org.axonframework.modelling.command.AggregateMember;
+import org.junit.jupiter.api.*;
+
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class validating an annotated aggregate model with {@link AggregateMember}s on the root level of a polymorphic
+ * aggregate behaves as desired when a meta model is created with the {@link AnnotatedAggregateMetaModelFactory}.
+ *
+ * @author Steven van Beelen
+ */
+class AnnotatedRootMembersAggregateMetaModelFactoryTest {
+
+    private static final AtomicInteger aggregateEventCounter = new AtomicInteger(0);
+    private static final AtomicInteger memberEventCounter = new AtomicInteger(0);
+
+    /**
+     * The hierarchy of the Aggregate, is as follows:
+     * <p>
+     * +--------------+
+     * |Root Aggregate|
+     * |   +------+   |
+     * |   |Member|   |
+     * |   +------+   |
+     * +--------------+
+     *        v
+     * +------+-------+
+     * |Node Aggregate|
+     * +------+-------+
+     *        v
+     * +------+-------+
+     * |Leaf Aggregate|
+     * +--------------+
+     * <p>
+     * On all levels an AggregateEvent handler is present. Only the Member has the MemberEvent handler. In such a set up
+     * we would assume the AggregateEvent handler to be invoked once in the root (which encompasses the root, node and
+     * leaf aggregate) and once in the member. Furthermore we would anticipate the MemberEvent handler to be invoked
+     * once too, since there only is a single occurrence of the member in the entire set up.
+     */
+    @Test
+    void testCreateAggregateModelDoesNotDuplicateRootLevelAggregateMembers() {
+        int expectedNumberOfAggregateEventHandlerInvocations = 2;
+        int expectedNumberOfMemberEventHandlerInvocations = 1;
+
+        EventMessage<AggregateEvent> testAggregateEvent = GenericEventMessage.asEventMessage(new AggregateEvent());
+        EventMessage<MemberEvent> testMemberEvent = GenericEventMessage.asEventMessage(new MemberEvent());
+        LeafAggregate testModel = new LeafAggregate();
+
+        AggregateModel<RootAggregate> testSubject = AnnotatedAggregateMetaModelFactory.inspectAggregate(
+                RootAggregate.class, Collections.singleton(LeafAggregate.class)
+        );
+
+        testSubject.publish(testAggregateEvent, testModel);
+        assertEquals(expectedNumberOfAggregateEventHandlerInvocations, aggregateEventCounter.get());
+        testSubject.publish(testMemberEvent, testModel);
+        assertEquals(expectedNumberOfMemberEventHandlerInvocations, memberEventCounter.get());
+    }
+
+    @SuppressWarnings("unused")
+    private abstract static class RootAggregate {
+
+        @AggregateMember
+        private final Member member = new Member();
+
+        @EventHandler
+        public void on(AggregateEvent event) {
+            aggregateEventCounter.incrementAndGet();
+        }
+    }
+
+    private abstract static class NodeAggregate extends RootAggregate {
+
+        @EventHandler
+        public void on(AggregateEvent event) {
+            aggregateEventCounter.incrementAndGet();
+        }
+    }
+
+    private static class LeafAggregate extends NodeAggregate {
+
+        @EventHandler
+        public void on(AggregateEvent event) {
+            aggregateEventCounter.incrementAndGet();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private static class Member {
+
+        @EventHandler
+        public void on(AggregateEvent event) {
+            aggregateEventCounter.incrementAndGet();
+        }
+
+        @EventHandler
+        public void on(MemberEvent event) {
+            memberEventCounter.incrementAndGet();
+        }
+    }
+
+    private static class AggregateEvent {
+
+    }
+
+    private static class MemberEvent {
+
+    }
+}
