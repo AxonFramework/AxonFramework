94e328f18312957f534c3c82c7f92bd590e2ca44
Steven van Beelen <steven.vanbeelen@axoniq.io>
2020-11-13 10:36:12 +0100

    Remove JUnit4 based test
    With the switch to a new spring boot version, there's no transitive
    dependency on junit4 anymore. Hence this test broke and had to be
    reverted to JUnit5 as a consequence.
    
    #1603

--- spring-boot-autoconfigure/src/test/java/org/axonframework/springboot/autoconfig/AxonServerAutoConfigurationLoadFactorTest.java
+++ spring-boot-autoconfigure/src/test/java/org/axonframework/springboot/autoconfig/AxonServerAutoConfigurationLoadFactorTest.java
@@ -1,28 +1,29 @@
 package org.axonframework.springboot.autoconfig;
 
 import org.axonframework.axonserver.connector.command.CommandLoadFactorProvider;
-import org.junit.*;
-import org.junit.runner.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.jmx.support.RegistrationPolicy;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
+ * Test class validating the {@link CommandLoadFactorProvider} is correctly auto configured.
+ *
  * @author Sara Pellegrini
  */
 @ContextConfiguration
 @EnableAutoConfiguration
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @TestPropertySource("classpath:application.commandLoadFactor-autoConfiguration.properties")
 public class AxonServerAutoConfigurationLoadFactorTest {
-
 
     @Autowired
     private CommandLoadFactorProvider commandLoadFactorProvider;
