fe95dd91626a5a656532e842ef83e86efafaa62d
Arnaud J <jabin@cikaba.com>
2020-12-14 16:59:17 +0100

    :white_check_mark: Add test

---
+++ extensions/kotlin/kotlin-test/src/test/kotlin/org/axonframework/extension/kotlin/test/testObjects.kt
@@ -0,0 +1,20 @@
+package org.axonframework.extension.kotlin.test
+
+import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.commandhandling.RoutingKey
+import org.axonframework.modelling.command.AggregateIdentifier
+
+internal data class ExampleCommand(@RoutingKey val aggregateId: String)
+
+internal class ExampleAggregate {
+
+    @AggregateIdentifier
+    lateinit var aggregateId: String
+
+    constructor()
+
+    @CommandHandler
+    constructor(command: ExampleCommand) {
+        throw Exception()
+    }
+}
