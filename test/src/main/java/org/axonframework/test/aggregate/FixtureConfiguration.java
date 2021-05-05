cfe59694e4302a819b8812a8455e2b1e15c23428
Sascha Eisenmann <sascha@eisenmann.it>
2021-05-05 13:58:28 +0200

    Improved documentation for useStateStorage()

--- test/src/main/java/org/axonframework/test/aggregate/FixtureConfiguration.java
+++ test/src/main/java/org/axonframework/test/aggregate/FixtureConfiguration.java
@@ -108,6 +108,7 @@ public interface FixtureConfiguration<T> {
     /**
      * Configures the fixture for state stored aggregates.
      * This will register an {@link org.axonframework.test.aggregate.AggregateTestFixture.InMemoryRepository} with the fixture
+     * Should be used before calling {@link FixtureConfiguration#givenState(Supplier)} or {@link FixtureConfiguration#givenCommands(List)} (Supplier)}
      *
      * @return the current FixtureConfiguration, for fluent interfacing
      */
