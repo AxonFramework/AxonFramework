eb3b66e0abcb654fb075fa10bcb9f9be4e78f83c
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-09-28 18:03:05 +0200

    Update the used XStreamSerializer
    Update the used XStreamSerializer to ascertain that it uses a dedicated
    XStream instance.
    
    #1917

--- spring-boot-autoconfigure/src/test/java/org/axonframework/springboot/AxonAutoConfigurationWithEventSerializerTest.java
+++ spring-boot-autoconfigure/src/test/java/org/axonframework/springboot/AxonAutoConfigurationWithEventSerializerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2018. Axon Framework
+ * Copyright (c) 2010-2021. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,24 +14,9 @@
  * limitations under the License.
  */
 
-/*
- * Copyright (c) 2010-2017. Axon Framework
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.axonframework.springboot;
 
+import com.thoughtworks.xstream.XStream;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.common.jdbc.ConnectionProvider;
@@ -118,8 +103,10 @@ public class AxonAutoConfigurationWithEventSerializerTest {
 
         @Bean
         @Primary
-        public Serializer mySerializer() {
-            return XStreamSerializer.builder().build();
+        public Serializer mySerializer(XStream xStream) {
+            return XStreamSerializer.builder()
+                                    .xStream(xStream)
+                                    .build();
         }
 
         @Bean
