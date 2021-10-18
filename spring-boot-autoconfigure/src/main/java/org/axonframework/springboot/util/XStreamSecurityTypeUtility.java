79d84375e150b34f0f18f18f2bb8209c3e0ee402
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-10-18 16:16:27 +0200

    Adjust secured type search approach
    Using the ComponentScan annotation is, sadly, not recommended by the
    Spring team. Instead, basing ourselves around the EntityScan annotation
    is more reasonable. Added, in absence of the EntityScan annotation,
    using the AutoConfigurationPackages#get(BeanFactory) method is regarded
    as the default in absence of that annotation. A short description on
    why, together with the new approach, can be found here ->
    https://stackoverflow.com/questions/50808941/how-to-get-basepackages-of
    -componentscan-programatically-at-runtime
    
    #1963
    
    (cherry picked from commit 9fdd348f8fc8a3be3f6e19ed00760999092cbe7d)

--- spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/util/XStreamSecurityTypeUtility.java
+++ spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/util/XStreamSecurityTypeUtility.java
@@ -17,17 +17,24 @@
 package org.axonframework.springboot.util;
 
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
+import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
 
 /**
- * Utility class exposing an operation that searches the {@link ApplicationContext} for {@link ComponentScan} annotated
- * beans and adds the contained types and packages to an {@link XStream} instance.
+ * Utility class exposing an operation that searches the {@link ApplicationContext} for entity types and packages and
+ * adds them to an {@link XStream} instance. It prefers {@link EntityScan} annotated beans. In absence of these it
+ * defaults to the output from the {@link AutoConfigurationPackages#get(BeanFactory)} method.
  * <p>
- * The {@link ComponentScan} annotation on these beans defines the base classes and base packages of types used within a
+ * The {@link EntityScan} annotation on these beans defines the base classes and base packages of types used within a
  * Spring application. These thus reflect a reasonable default of types that {@link XStream} should allow.
  *
  * @author Steven van Beelen
@@ -35,51 +42,77 @@ import java.util.Objects;
  */
 public abstract class XStreamSecurityTypeUtility {
 
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     private static final String PACKAGES_AND_SUBPACKAGES_WILDCARD = ".**";
 
     /**
-     * Searches the {@link ApplicationContext} for {@link ComponentScan} annotated beans to allow the contained types
-     * for the given {@code xStream} instance.
+     * Searches the {@link ApplicationContext} for entity types or package names to allow for the given {@code xStream}
+     * instance. This method prefers {@link EntityScan} annotated beans. In absence of the {@code EntityScan} we use the
+     * output from {@link AutoConfigurationPackages#get(BeanFactory)}.
      * <p>
-     * Will add the {@link ComponentScan#basePackageClasses()} and {@link ComponentScan#basePackages()} ({@link
-     * ComponentScan#value() is used instead if not specified} to the given {@code xStream}, through {@link
+     * Will add the {@link EntityScan#basePackageClasses()} and {@link EntityScan#basePackages()} ({@link
+     * EntityScan#value() is used instead if not specified} to the given {@code xStream}, through {@link
      * XStream#allowTypes(Class[])} and {@link XStream#allowTypesByWildcard(String[])} respectively. If both the base
-     * package classes and base packages of the {@link ComponentScan} are not set, the package of the {@link
-     * ComponentScan} annotated bean is included.
+     * package classes and base packages of the {@code EntityScan} are not set, the package of the {@code EntityScan}
+     * annotated bean is included.
+     * <p>
+     * We use the {@code AutoConfigurationPackages#get(BeanFactory)} outcome as the default, since Spring Boot uses this
+     * too. This default will include the package names of {@link javax.persistence.Entity} and {@link
+     * org.springframework.boot.autoconfigure.EnableAutoConfiguration} annotated beans.
      *
-     * @param applicationContext the {@link ApplicationContext} to retrieve {@link ComponentScan} beans from
+     * @param applicationContext the {@link ApplicationContext} to retrieve {@link EntityScan} beans from
      * @param xStream            the {@link XStream} to set allow types and type wildcards for
      */
-    public static void allowTypesFromComponentScanAnnotatedBeans(ApplicationContext applicationContext,
-                                                                 XStream xStream) {
-        applicationContext.getBeansWithAnnotation(ComponentScan.class)
-                          .forEach((beanName, bean) -> {
-                              ComponentScan ann =
-                                      applicationContext.findAnnotationOnBean(beanName, ComponentScan.class);
-                              if (!Objects.nonNull(ann)) {
-                                  throw new IllegalArgumentException(
-                                          "The ApplicationContext retrieved a bean of name [" + beanName + "] and type"
-                                                  + " [" + bean.getClass() + "] for the ComponentScan annotation "
-                                                  + "which does not contain the ComponentScan annotation."
-                                  );
-                              }
-
-                              if (isEmptyAnnotation(ann)) {
-                                  xStream.allowTypesByWildcard(new String[]{
-                                          bean.getClass().getPackage().getName() + PACKAGES_AND_SUBPACKAGES_WILDCARD
-                                  });
-                              } else {
-                                  xStream.allowTypes(ann.basePackageClasses());
-                                  if (ann.basePackages().length != 0) {
-                                      allowPackages(xStream, ann.basePackages());
-                                  } else {
-                                      allowPackages(xStream, ann.value());
-                                  }
-                              }
-                          });
+    public static void allowEntityTypesFrom(ApplicationContext applicationContext, XStream xStream) {
+        Map<String, Object> entityScanAnnotatedBeans = applicationContext.getBeansWithAnnotation(EntityScan.class);
+        if (!entityScanAnnotatedBeans.isEmpty()) {
+            entityScanAnnotatedBeans.forEach(
+                    (beanName, bean) -> extractAllowedTypesFrom(beanName, bean, applicationContext, xStream)
+            );
+        } else {
+            extractAllowedTypesFromAutoConfigPackages(applicationContext, xStream);
+        }
     }
 
-    private static boolean isEmptyAnnotation(ComponentScan ann) {
+    private static void extractAllowedTypesFromAutoConfigPackages(ApplicationContext applicationContext,
+                                                                  XStream xStream) {
+        if (AutoConfigurationPackages.has(applicationContext)) {
+            allowPackages(xStream, AutoConfigurationPackages.get(applicationContext).toArray(new String[]{}));
+        } else {
+            logger.warn("Cannot extract allowed types for XStream, because the provided ApplicationContext "
+                                + "does not contain any @EnableAutoConfiguration annotated beans.");
+        }
+    }
+
+    private static void extractAllowedTypesFrom(String beanName,
+                                                Object bean,
+                                                ApplicationContext applicationContext,
+                                                XStream xStream) {
+        EntityScan ann = applicationContext.findAnnotationOnBean(beanName, EntityScan.class);
+        if (!Objects.nonNull(ann)) {
+            throw new IllegalArgumentException(
+                    "The ApplicationContext retrieved a bean of name [" + beanName + "] and type"
+                            + " [" + bean.getClass() + "] for the EntityScan annotation "
+                            + "which does not contain the EntityScan annotation."
+            );
+        }
+
+        if (isEmptyAnnotation(ann)) {
+            xStream.allowTypesByWildcard(new String[]{
+                    bean.getClass().getPackage().getName() + PACKAGES_AND_SUBPACKAGES_WILDCARD
+            });
+        } else {
+            xStream.allowTypes(ann.basePackageClasses());
+            if (ann.basePackages().length != 0) {
+                allowPackages(xStream, ann.basePackages());
+            } else {
+                allowPackages(xStream, ann.value());
+            }
+        }
+    }
+
+    private static boolean isEmptyAnnotation(EntityScan ann) {
         return ann.basePackageClasses().length == 0 && ann.basePackages().length == 0 && ann.value().length == 0;
     }
 
