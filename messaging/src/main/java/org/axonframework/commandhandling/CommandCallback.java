dc93479982ea2b3f9008c363a23053ff01d575cf
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2022-04-21 12:36:13 +0200

    Update messaging/src/main/java/org/axonframework/commandhandling/CommandCallback.java
    Co-authored-by: Steven van Beelen <steven.vanbeelen@axoniq.io>

--- messaging/src/main/java/org/axonframework/commandhandling/CommandCallback.java
+++ messaging/src/main/java/org/axonframework/commandhandling/CommandCallback.java
@@ -39,8 +39,8 @@ public interface CommandCallback<C, R> {
                   @Nonnull CommandResultMessage<? extends R> commandResultMessage);
 
     /**
-     * Wraps the command callback with another using an {@link WrappedCommandCallback}. If provided with a null
-     * callback, will not wrap it and keep the original callback.
+     * Wraps the command callback with another using a {@link WrappedCommandCallback}. If provided with a null
+     * callback this method will not wrap it, keeping the original callback instead.
      * <p>
      * In effect, the given callback will be executed first, and then the original will be executed second. You can wrap
      * callback as many times as you'd like.
