22a5993bc84eb17a16cf0622137811dbc782b6fd
Stefan <91stefan@gmail.com>
2022-06-29 12:12:15 +0200

    auto configuration tests

--- axon-server-connector/src/main/java/org/axonframework/axonserver/connector/event/axon/EventProcessorInfoConfiguration.java
+++ axon-server-connector/src/main/java/org/axonframework/axonserver/connector/event/axon/EventProcessorInfoConfiguration.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -97,5 +97,9 @@ public class EventProcessorInfoConfiguration implements ModuleConfiguration {
         if (config.eventProcessingConfiguration() != null) {
             this.config.onStart(Phase.INBOUND_EVENT_CONNECTORS, eventProcessorControlService::get);
         }
+    }
+
+    public Component<EventProcessorControlService> getEventProcessorControlService() {
+        return eventProcessorControlService;
     }
 }
