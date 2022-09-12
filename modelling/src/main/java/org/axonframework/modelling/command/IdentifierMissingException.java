294e2b6ea745d760d3cc6644edeaf8c93597701e
Steven van Beelen <steven.vanbeelen@axoniq.io>
2022-09-12 13:46:45 +0200

    Adjust javadoc for clarity
    Adjust javadoc for clarity
    
    #2209

--- modelling/src/main/java/org/axonframework/modelling/command/IdentifierMissingException.java
+++ modelling/src/main/java/org/axonframework/modelling/command/IdentifierMissingException.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010-2022. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.axonframework.modelling.command;
 
 import org.axonframework.common.AxonException;
@@ -11,9 +27,9 @@ import org.axonframework.common.AxonException;
 public class IdentifierMissingException extends AxonException {
 
     /**
-     * Construct a {@code IdentifierMissingException} instance from the given {@code message}.
+     * Constructs an exception based on the given {@code message}.
      *
-     * @param message The message to pass on to parent exception.
+     * @param message The description of this {@link IdentifierMissingException}.
      */
     public IdentifierMissingException(String message) {
         super(message);
