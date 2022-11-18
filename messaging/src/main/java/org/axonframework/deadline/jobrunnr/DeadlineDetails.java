eec009819a8bf181927f4a68020732c4796d9505
Gerard Klijs <gerard.klijs@axoniq.io>
2022-11-18 17:25:32 +0100

    Makes sure errors handling deadlines are not ignored, but retried instead. Keeps the ScopeDiscriptor instead of putting it as String, such that it stays a SagaScopeDescriptor.

--- messaging/src/main/java/org/axonframework/deadline/jobrunnr/DeadlineDetails.java
+++ messaging/src/main/java/org/axonframework/deadline/jobrunnr/DeadlineDetails.java
@@ -1,4 +1,22 @@
+/*
+ * Copyright (c) 2010-2022. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.axonframework.deadline.jobrunnr;
+
+import org.axonframework.messaging.ScopeDescriptor;
 
 import java.util.UUID;
 
@@ -6,15 +24,16 @@ public class DeadlineDetails {
     
     private String deadlineName;
     private UUID deadlineId;
-    private String scopeDescription;
+    private ScopeDescriptor scopeDescription;
     private Object payload;
     private String[] keys;
     private Object[] values;
     
     private DeadlineDetails() {
     }
-    
-    public DeadlineDetails(String deadlineName, UUID deadlineId, String scopeDescription, Object payload, String[] keys, Object[] values) {
+
+    public DeadlineDetails(String deadlineName, UUID deadlineId, ScopeDescriptor scopeDescription, Object payload,
+                           String[] keys, Object[] values) {
         this.deadlineName = deadlineName;
         this.deadlineId = deadlineId;
         this.scopeDescription = scopeDescription;
@@ -30,8 +49,8 @@ public class DeadlineDetails {
     public UUID getDeadlineId() {
         return deadlineId;
     }
-    
-    public String getScopeDescription() {
+
+    public ScopeDescriptor getScopeDescription() {
         return scopeDescription;
     }
     
--- messaging/src/main/java/org/axonframework/deadline/jobrunnr/JobRunrDeadlineManager.java
+++ messaging/src/main/java/org/axonframework/deadline/jobrunnr/JobRunrDeadlineManager.java
@@ -1,17 +1,25 @@
+/*
+ * Copyright (c) 2010-2022. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.axonframework.deadline.jobrunnr;
 
-import static java.lang.String.format;
-import static org.axonframework.common.BuilderUtils.assertNonNull;
-import static org.axonframework.deadline.GenericDeadlineMessage.asDeadlineMessage;
-import static org.slf4j.LoggerFactory.getLogger;
-
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 import org.axonframework.common.AxonConfigurationException;
 import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.deadline.AbstractDeadlineManager;
+import org.axonframework.deadline.DeadlineException;
 import org.axonframework.deadline.DeadlineMessage;
 import org.axonframework.deadline.GenericDeadlineMessage;
 import org.axonframework.eventhandling.GenericEventMessage;
@@ -26,131 +34,144 @@ import org.axonframework.messaging.unitofwork.UnitOfWork;
 import org.jobrunr.scheduling.JobScheduler;
 import org.slf4j.Logger;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static java.lang.String.format;
+import static org.axonframework.common.BuilderUtils.assertNonNull;
+import static org.axonframework.deadline.GenericDeadlineMessage.asDeadlineMessage;
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class JobRunrDeadlineManager extends AbstractDeadlineManager {
-    
+
     private static final Logger LOGGER = getLogger(JobRunrDeadlineManager.class);
-    
+
     private final ScopeAwareProvider scopeAwareProvider;
     private final JobScheduler jobScheduler;
     private final TransactionManager transactionManager;
-    
+
     public static Builder builder() {
         return new Builder();
     }
-    
+
     protected JobRunrDeadlineManager(Builder builder) {
         builder.validate();
         this.scopeAwareProvider = builder.scopeAwareProvider;
         this.jobScheduler = builder.jobScheduler;
         this.transactionManager = builder.transactionManager;
     }
-    
+
     @Override
-    public String schedule(Instant triggerDateTime, String deadlineName, Object messageOrPayload, ScopeDescriptor deadlineScope) {
+    public String schedule(Instant triggerDateTime, String deadlineName, Object messageOrPayload,
+                           ScopeDescriptor deadlineScope) {
         DeadlineMessage<Object> deadlineMessage = asDeadlineMessage(deadlineName, messageOrPayload, triggerDateTime);
         UUID deadlineId = UUID.fromString(deadlineMessage.getIdentifier());
         runOnPrepareCommitOrNow(() -> {
             DeadlineMessage<Object> interceptedDeadlineMessage = processDispatchInterceptors(deadlineMessage);
             DeadlineDetails deadlineDetails =
-                    new DeadlineDetails(deadlineName, deadlineId, deadlineScope.scopeDescription(), interceptedDeadlineMessage.getPayload(), interceptedDeadlineMessage.getMetaData().keySet().toArray(new String[0]),
-                            interceptedDeadlineMessage.getMetaData().values().toArray());
+                    new DeadlineDetails(deadlineName,
+                                        deadlineId,
+                                        deadlineScope,
+                                        interceptedDeadlineMessage.getPayload(),
+                                        interceptedDeadlineMessage.getMetaData().keySet().toArray(new String[0]),
+                                        interceptedDeadlineMessage.getMetaData().values().toArray());
             jobScheduler.<JobRunrDeadlineManager>schedule(deadlineId, triggerDateTime, x -> x.execute(deadlineDetails));
         });
         return deadlineId.toString();
     }
-    
+
     @Override
     public void cancelSchedule(String deadlineName, String scheduleId) {
         runOnPrepareCommitOrNow(() -> jobScheduler.delete(scheduleId));
     }
-    
+
     @Override
     public void cancelAll(String deadlineName) {
         throw new UnsupportedOperationException();
     }
-    
+
     @Override
     public void cancelAllWithinScope(String deadlineName, ScopeDescriptor scope) {
         throw new UnsupportedOperationException();
     }
-    
+
     public void execute(DeadlineDetails deadlineDetails) {
-        try {
-            Instant triggerInstant = GenericEventMessage.clock.instant();
-            Map<String, Object> metaData = new HashMap<>();
-            for (int i = 0; i < deadlineDetails.getKeys().length; i++) {
-                metaData.put(deadlineDetails.getKeys()[i], deadlineDetails.getValues()[i]);
-            }
-            GenericDeadlineMessage rebuiltDeadlineMessage = new GenericDeadlineMessage<>(deadlineDetails.getDeadlineName(),
-                    deadlineDetails.getDeadlineId().toString(),
-                    deadlineDetails.getPayload(),
-                    metaData,
-                    triggerInstant);
-            UnitOfWork<DeadlineMessage<?>> unitOfWork = new DefaultUnitOfWork<>(rebuiltDeadlineMessage);
-            unitOfWork.attachTransaction(transactionManager);
-            InterceptorChain chain =
-                    new DefaultInterceptorChain<>(unitOfWork,
-                            handlerInterceptors(),
-                            deadlineMessage -> {
-                                executeScheduledDeadline(deadlineMessage, (ScopeDescriptor) deadlineDetails::getScopeDescription);
-                                return null;
-                            });
-            ResultMessage<?> resultMessage = unitOfWork.executeWithResult(chain::proceed);
-            if (resultMessage.isExceptional()) {
-                Throwable e = resultMessage.exceptionResult();
-                LOGGER.error("An error occurred while triggering the deadline [{}] with identifier [{}]",
-                        deadlineDetails.getDeadlineName(), deadlineDetails.getDeadlineId(), e);
-            }
-        } catch (Exception e) {
+        Instant triggerInstant = GenericEventMessage.clock.instant();
+        Map<String, Object> metaData = new HashMap<>();
+        for (int i = 0; i < deadlineDetails.getKeys().length; i++) {
+            metaData.put(deadlineDetails.getKeys()[i], deadlineDetails.getValues()[i]);
+        }
+        GenericDeadlineMessage rebuiltDeadlineMessage = new GenericDeadlineMessage<>(deadlineDetails.getDeadlineName(),
+                                                                                     deadlineDetails.getDeadlineId()
+                                                                                                    .toString(),
+                                                                                     deadlineDetails.getPayload(),
+                                                                                     metaData,
+                                                                                     triggerInstant);
+        UnitOfWork<DeadlineMessage<?>> unitOfWork = new DefaultUnitOfWork<>(rebuiltDeadlineMessage);
+        unitOfWork.attachTransaction(transactionManager);
+        InterceptorChain chain =
+                new DefaultInterceptorChain<>(unitOfWork,
+                                              handlerInterceptors(),
+                                              deadlineMessage -> {
+                                                  executeScheduledDeadline(deadlineMessage,
+                                                                           deadlineDetails.getScopeDescription());
+                                                  return null;
+                                              });
+        ResultMessage<?> resultMessage = unitOfWork.executeWithResult(chain::proceed);
+        if (resultMessage.isExceptional()) {
+            Throwable e = resultMessage.exceptionResult();
             LOGGER.error("An error occurred while triggering the deadline [{}] with identifier [{}]",
-                    deadlineDetails.getDeadlineName(), deadlineDetails.getDeadlineId(), e);
+                         deadlineDetails.getDeadlineName(), deadlineDetails.getDeadlineId(), e);
+            throw new DeadlineException("Failed to process", e.getCause());
         }
     }
-    
+
     private void executeScheduledDeadline(DeadlineMessage<?> deadlineMessage, ScopeDescriptor deadlineScope) {
         scopeAwareProvider.provideScopeAwareStream(deadlineScope)
-                .filter(scopeAwareComponent -> scopeAwareComponent.canResolve(deadlineScope))
-                .forEach(scopeAwareComponent -> {
-                    try {
-                        scopeAwareComponent.send(deadlineMessage, deadlineScope);
-                    } catch (Exception e) {
-                        String exceptionMessage = format(
-                                "Failed to send a DeadlineMessage for scope [%s]",
-                                deadlineScope.scopeDescription()
-                        );
-                        throw new ExecutionException(exceptionMessage, e);
-                    }
-                });
+                          .filter(scopeAwareComponent -> scopeAwareComponent.canResolve(deadlineScope))
+                          .forEach(scopeAwareComponent -> {
+                              try {
+                                  scopeAwareComponent.send(deadlineMessage, deadlineScope);
+                              } catch (Exception e) {
+                                  String exceptionMessage = format(
+                                          "Failed to send a DeadlineMessage for scope [%s]",
+                                          deadlineScope.scopeDescription()
+                                  );
+                                  throw new ExecutionException(exceptionMessage, e);
+                              }
+                          });
     }
-    
+
     public static class Builder {
-        
+
         private JobScheduler jobScheduler;
         private ScopeAwareProvider scopeAwareProvider;
         private TransactionManager transactionManager;
-        
+
         public Builder scopeAwareProvider(ScopeAwareProvider scopeAwareProvider) {
             assertNonNull(scopeAwareProvider, "ScopeAwareProvider may not be null");
             this.scopeAwareProvider = scopeAwareProvider;
             return this;
         }
-        
+
         public Builder jobScheduler(JobScheduler jobScheduler) {
             assertNonNull(jobScheduler, "JobScheduler may not be null");
             this.jobScheduler = jobScheduler;
             return this;
         }
-        
+
         public Builder transactionManager(TransactionManager transactionManager) {
             assertNonNull(transactionManager, "TransactionManager may not be null");
             this.transactionManager = transactionManager;
             return this;
         }
-        
+
         public JobRunrDeadlineManager build() {
             return new JobRunrDeadlineManager(this);
         }
-        
+
         protected void validate() throws AxonConfigurationException {
             assertNonNull(scopeAwareProvider, "The ScopeAwareProvider is a hard requirement and should be provided");
             assertNonNull(jobScheduler, "The JobScheduler is a hard requirement and should be provided");
