036c60c657b17fe53e20238b8687e9c5dade9df5
Steven van Beelen <steven.vanbeelen@axoniq.io>
2023-01-02 13:13:46 +0100

    Replace PriorityQueue for LinkedList
    Replace PriorityQueue for LinkedList
    
    #2544

--- spring-boot-autoconfigure/src/test/java/org/axonframework/springboot/autoconfig/InfraConfigurationTest.java
+++ spring-boot-autoconfigure/src/test/java/org/axonframework/springboot/autoconfig/InfraConfigurationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2022. Axon Framework
+ * Copyright (c) 2010-2023. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.jmx.support.RegistrationPolicy;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.util.PriorityQueue;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -219,7 +219,7 @@ class InfraConfigurationTest {
 
         @Bean
         public Queue<String> handlingOutcome() {
-            return new PriorityQueue<>();
+            return new LinkedList<>();
         }
 
         @Bean
