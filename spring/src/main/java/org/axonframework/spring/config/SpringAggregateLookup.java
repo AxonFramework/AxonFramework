83c36f4325f8eca614be59c3425b5e48a609fb9e
Steven van Beelen <steven.vanbeelen@axoniq.io>
2023-01-25 13:03:40 +0100

    Clean-up
    - Align indentation
    - Resolve warnings
    - Resolve typos
    
    #2578

--- spring/src/main/java/org/axonframework/spring/config/SpringAggregateLookup.java
+++ spring/src/main/java/org/axonframework/spring/config/SpringAggregateLookup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2022. Axon Framework
+ * Copyright (c) 2010-2023. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -201,8 +201,9 @@ public class SpringAggregateLookup implements BeanDefinitionRegistryPostProcesso
                         repositoryName,
                         BeanDefinitionBuilder.rootBeanDefinition(SpringRepositoryFactoryBean.class)
                                              .addConstructorArgValue(aggregateType)
-                                             .addPropertyValue("configuration",
-                                                               new RuntimeBeanReference(Configuration.class))
+                                             .addPropertyValue(
+                                                     "configuration", new RuntimeBeanReference(Configuration.class)
+                                             )
                                              .getBeanDefinition()
                 );
             }
