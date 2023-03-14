c06c77d86f23806845627ea13d78bf28512fb0a6
Gerard Klijs <gerard.klijs@axoniq.io>
2023-03-14 10:10:09 +0100

    Don't log warn when the aggregate is handling a deadline message.

--- modelling/src/main/java/org/axonframework/modelling/command/AbstractRepository.java
+++ modelling/src/main/java/org/axonframework/modelling/command/AbstractRepository.java
@@ -19,6 +19,7 @@ package org.axonframework.modelling.command;
 import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.common.Assert;
 import org.axonframework.common.AxonConfigurationException;
+import org.axonframework.deadline.DeadlineMessage;
 import org.axonframework.messaging.Message;
 import org.axonframework.messaging.ScopeDescriptor;
 import org.axonframework.messaging.annotation.ClasspathHandlerDefinition;
@@ -33,6 +34,7 @@ import org.axonframework.tracing.SpanFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -122,7 +124,6 @@ public abstract class AbstractRepository<T, A extends Aggregate<T>> implements R
      *
      * @param factoryMethod The method to create the aggregate's root instance
      * @return an Aggregate instance describing the aggregate's state
-     *
      * @throws Exception when the factoryMethod throws an exception
      */
     protected abstract A doCreateNew(Callable<T> factoryMethod) throws Exception;
@@ -174,11 +175,21 @@ public abstract class AbstractRepository<T, A extends Aggregate<T>> implements R
     private UnitOfWork<?> currentUnitOfWork() {
         UnitOfWork<?> uow = CurrentUnitOfWork.get();
         Class<?> messageType = uow.getMessage() != null ? uow.getMessage().getClass() : null;
-        if (messageType != null && !CommandMessage.class.isAssignableFrom(messageType)) {
-            logger.warn("The active Unit of Work is expected to contain a CommandMessage, but instead contains a [{}]",
+        if (invalidMessageType(messageType)) {
+            logger.warn("The active Unit of Work is expected to contain a CommandMessage or a DeadlineMessage, but instead contains a [{}]",
                         messageType);
         }
         return uow;
+    }
+
+    private boolean invalidMessageType(Class<?> messageType) {
+        if (messageType == null) {
+            return false;
+        }
+        if (CommandMessage.class.isAssignableFrom(messageType)) {
+            return false;
+        }
+        return !DeadlineMessage.class.isAssignableFrom(messageType);
     }
 
     /**
@@ -370,6 +381,7 @@ public abstract class AbstractRepository<T, A extends Aggregate<T>> implements R
             }
         }
     }
+
     @Override
     public boolean canResolve(@Nonnull ScopeDescriptor scopeDescription) {
         return scopeDescription instanceof AggregateScopeDescriptor && aggregateModel.types().anyMatch(
