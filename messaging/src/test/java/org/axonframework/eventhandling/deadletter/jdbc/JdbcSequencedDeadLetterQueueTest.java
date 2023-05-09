aa0a201d5fba3892f188c3835125a7109abbe287
Steven van Beelen <steven.vanbeelen@axoniq.io>
2023-05-09 16:53:23 +0200

    Construct JDBC DLQ according to builder adjustments
    Ensure the test case constructs a JDBC DLQ using a DataSource, upon
    which the ConnectionProvider and TransactionManager can be based.
    
    #feature/2350-jdbc-dlq

--- messaging/src/test/java/org/axonframework/eventhandling/deadletter/jdbc/JdbcSequencedDeadLetterQueueTest.java
+++ messaging/src/test/java/org/axonframework/eventhandling/deadletter/jdbc/JdbcSequencedDeadLetterQueueTest.java
@@ -16,14 +16,22 @@
 
 package org.axonframework.eventhandling.deadletter.jdbc;
 
+import org.axonframework.common.transaction.Transaction;
+import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.messaging.deadletter.DeadLetter;
 import org.axonframework.messaging.deadletter.GenericDeadLetter;
 import org.axonframework.messaging.deadletter.SequencedDeadLetterQueue;
 import org.axonframework.messaging.deadletter.SequencedDeadLetterQueueTest;
 import org.axonframework.serialization.TestSerializer;
+import org.hsqldb.jdbc.JDBCDataSource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.time.Clock;
+import javax.sql.DataSource;
 
 /**
  * Implementation of the {@link SequencedDeadLetterQueueTest}, validating the {@link JdbcSequencedDeadLetterQueue}.
@@ -37,12 +45,49 @@ class JdbcSequencedDeadLetterQueueTest extends SequencedDeadLetterQueueTest<Even
 
     @Override
     protected SequencedDeadLetterQueue<EventMessage<?>> buildTestSubject() {
-        return JdbcSequencedDeadLetterQueue.builder()
-                                           .maxSequences(MAX_SEQUENCES_AND_SEQUENCE_SIZE)
-                                           .maxSequenceSize(MAX_SEQUENCES_AND_SEQUENCE_SIZE)
-                                           .processingGroup(TEST_PROCESSING_GROUP)
-                                           .serializer(TestSerializer.JACKSON.getSerializer())
-                                           .build();
+        DataSource dataSource = dataSource();
+        JdbcSequencedDeadLetterQueue<EventMessage<?>> deadLetterQueue =
+                JdbcSequencedDeadLetterQueue.builder()
+                                            .processingGroup(TEST_PROCESSING_GROUP)
+                                            .maxSequences(MAX_SEQUENCES_AND_SEQUENCE_SIZE)
+                                            .maxSequenceSize(MAX_SEQUENCES_AND_SEQUENCE_SIZE)
+                                            .connectionProvider(dataSource::getConnection)
+                                            .transactionManager(transactionManager(dataSource))
+                                            .serializer(TestSerializer.JACKSON.getSerializer())
+                                            .build();
+        deadLetterQueue.createSchema(new GenericDeadLetterTableFactory());
+        return deadLetterQueue;
+    }
+
+    private DataSource dataSource() {
+        JDBCDataSource dataSource = new JDBCDataSource();
+        dataSource.setUrl("jdbc:hsqldb:mem:axontest");
+        dataSource.setUser("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    private TransactionManager transactionManager(DataSource dataSource) {
+        PlatformTransactionManager platformTransactionManager = platformTransactionManager(dataSource);
+        return () -> {
+            TransactionStatus transaction =
+                    platformTransactionManager.getTransaction(new DefaultTransactionDefinition());
+            return new Transaction() {
+                @Override
+                public void commit() {
+                    platformTransactionManager.commit(transaction);
+                }
+
+                @Override
+                public void rollback() {
+                    platformTransactionManager.rollback(transaction);
+                }
+            };
+        };
+    }
+
+    private PlatformTransactionManager platformTransactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 
     @Override
