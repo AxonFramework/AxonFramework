35eb206cbcf02338f4cb15219077e43af2c711c2
Gerard Klijs <gerard.klijs@axoniq.io>
2023-05-30 13:29:32 +0200

    Update messaging/src/main/java/org/axonframework/deadline/dbscheduler/DbSchedulerHumanReadableDeadlineDetails.java
    Co-authored-by: Steven van Beelen <steven.vanbeelen@axoniq.io>

--- messaging/src/main/java/org/axonframework/deadline/dbscheduler/DbSchedulerHumanReadableDeadlineDetails.java
+++ messaging/src/main/java/org/axonframework/deadline/dbscheduler/DbSchedulerHumanReadableDeadlineDetails.java
@@ -32,10 +32,10 @@ import javax.annotation.Nullable;
 import static java.lang.String.format;
 
 /**
- * Pojo that contains the needed for a {@link com.github.kagkarlsson.scheduler.task.Task} handling a deadline, will be
+ * Pojo that contains the needed information for a {@link com.github.kagkarlsson.scheduler.task.Task} handling a deadline. Will be
  * serialized and deserialized using the configured {@link Serializer} on the
- * {@link com.github.kagkarlsson.scheduler.Scheduler}. This one is used with the
- * {@link DbSchedulerDeadlineManager#humanReadableTask()}
+ * {@link com.github.kagkarlsson.scheduler.Scheduler}. This object is used with the
+ * {@link DbSchedulerDeadlineManager#humanReadableTask()}.
  *
  * @author Gerard Klijs
  * @since 4.8.0
