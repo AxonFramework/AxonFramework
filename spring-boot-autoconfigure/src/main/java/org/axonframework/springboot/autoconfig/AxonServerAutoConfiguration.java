b1e6506d0dd2f9013c3cbb80e9ef54bc210f8e16
Gerard Klijs <gerard.klijs@axoniq.io>
2023-08-17 14:06:24 +0200

    Add property to easily disable using Axon Server as event store, still using it as query and command router.

--- spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/autoconfig/AxonServerAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/autoconfig/AxonServerAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2022. Axon Framework
+ * Copyright (c) 2010-2023. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -153,6 +153,7 @@ public class AxonServerAutoConfiguration implements ApplicationContextAware {
 
     @Bean
     @ConditionalOnMissingBean
+    @ConditionalOnProperty(name = "axon.axonserver.event-store.enabled", matchIfMissing = true)
     public EventScheduler eventScheduler(@Qualifier("eventSerializer") Serializer eventSerializer,
                                          AxonServerConnectionManager connectionManager) {
         return AxonServerEventScheduler.builder()
