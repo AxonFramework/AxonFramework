667f8c8f3462a8d5c0daa513d99657d706d362ff
Steven van Beelen <steven.vanbeelen@axoniq.io>
2023-08-24 10:20:46 +0200

    Actually set command and query permits
    Actually set command and query permits. This should be regarded as a bug
    
    #enhancement/clean-up-as-config-and-config-usage

--- axon-server-connector/src/main/java/org/axonframework/axonserver/connector/AxonServerConnectionManager.java
+++ axon-server-connector/src/main/java/org/axonframework/axonserver/connector/AxonServerConnectionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2022. Axon Framework
+ * Copyright (c) 2010-2023. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -315,7 +315,10 @@ public class AxonServerConnectionManager implements Lifecycle, ConnectionManager
                 }
             }
 
-            builder.connectTimeout(axonServerConfiguration.getConnectTimeout(), TimeUnit.MILLISECONDS);
+            builder.connectTimeout(axonServerConfiguration.getConnectTimeout(), TimeUnit.MILLISECONDS)
+                   .commandPermits(axonServerConfiguration.getCommandFlowControl().getPermits())
+                   .queryPermits(axonServerConfiguration.getQueryFlowControl().getPermits());
+
             if (axonServerConfiguration.getToken() != null) {
                 builder.token(axonServerConfiguration.getToken());
             }
