bac4904b5e8f7509011be4c27cddeae7d4dda13a
Allard Buijze <allard.buijze@axoniq.io>
2023-09-05 12:36:11 +0200

    Documentation and formatting changes - part 2
    As requested by reviewers.

--- spring-boot-3-integrationtests/src/test/java/org/axonframework/springboot/service/connection/SpringBootDockerComposeIntegrationTest.java
+++ spring-boot-3-integrationtests/src/test/java/org/axonframework/springboot/service/connection/SpringBootDockerComposeIntegrationTest.java
@@ -1,21 +1,32 @@
-package org.axonframework.springboot.connection;
+/*
+ * Copyright (c) 2010-2023. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.springboot.service.connection;
 
 import io.axoniq.axonserver.connector.AxonServerConnection;
 import org.axonframework.axonserver.connector.AxonServerConfiguration;
 import org.axonframework.axonserver.connector.AxonServerConnectionManager;
-import org.axonframework.springboot.service.connection.AxonServerConnectionDetails;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.time.Duration;
 
 import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SpringBootDockerComposeIntegrationTest {
 
