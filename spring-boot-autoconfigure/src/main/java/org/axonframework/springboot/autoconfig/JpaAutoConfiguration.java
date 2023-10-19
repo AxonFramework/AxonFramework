1ebaf60b4bef2d32df7f4caaab3e07237c8c20b4
Gerard Klijs <gerard.klijs@axoniq.io>
2023-10-19 12:03:11 +0200

    Update spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/autoconfig/JpaAutoConfiguration.java
    Co-authored-by: Steven van Beelen <steven.vanbeelen@axoniq.io>

--- spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/autoconfig/JpaAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/autoconfig/JpaAutoConfiguration.java
@@ -119,6 +119,5 @@ public class JpaAutoConfiguration {
                                                                         .build()
         );
     }
-
     // end::JpaDeadLetterQueueProviderConfigurerModule[]
 }
