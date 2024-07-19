a69d2f5edfe991b83dd3380c18e2141cec39b0c8
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-07-19 13:52:43 +0200

    Fine tune indentation
    Fine tune indentation
    
    #3028

--- messaging/src/test/java/org/axonframework/eventhandling/pooled/PooledStreamingEventProcessorTest.java
+++ messaging/src/test/java/org/axonframework/eventhandling/pooled/PooledStreamingEventProcessorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2023. Axon Framework
+ * Copyright (c) 2010-2024. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -961,7 +961,7 @@ class PooledStreamingEventProcessorTest {
     @Test
     void maxCapacityReturnsConfiguredCapacity() {
         int expectedMaxCapacity = 500;
-        setTestSubject(createTestSubject(builder -> builder.maxSegmentProvider( p -> expectedMaxCapacity)));
+        setTestSubject(createTestSubject(builder -> builder.maxSegmentProvider(p -> expectedMaxCapacity)));
 
         assertEquals(expectedMaxCapacity, testSubject.maxCapacity());
     }
