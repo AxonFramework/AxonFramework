670d568c4b044a23c69d7dec9e2a44cb6e3342cb
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-08-27 14:42:10 +0200

    Add missing MessageStreamTest implementations
    Add test implementations for the:
    - CompletionCallbackMessageStream
    - MappedMessageStream
    - OnErrorContinueMessageStream
    
    #3063

---
+++ messaging/src/test/java/org/axonframework/messaging/CompletionCallbackMessageStreamTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2010-2024. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.messaging;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Test class validating the {@link CompletionCallbackMessageStream} through the {@link MessageStreamTest} suite.
+ *
+ * @author Allard Buijze
+ * @author Steven van Beelen
+ */
+class CompletionCallbackMessageStreamTest extends MessageStreamTest<String> {
+
+    private static final Runnable NO_OP_COMPLETION_CALLBACK = () -> {
+    };
+
+    @Override
+    MessageStream<Message<String>> testSubject(List<Message<String>> messages) {
+        return new CompletionCallbackMessageStream<>(MessageStream.fromIterable(messages), NO_OP_COMPLETION_CALLBACK);
+    }
+
+    @Override
+    MessageStream<Message<String>> failingTestSubject(List<Message<String>> messages, Exception failure) {
+        return new CompletionCallbackMessageStream<>(MessageStream.fromIterable(messages)
+                                                                  .concatWith(MessageStream.failed(failure)),
+                                                     NO_OP_COMPLETION_CALLBACK);
+    }
+
+    @Override
+    String createRandomValidEntry() {
+        return "test-" + ThreadLocalRandom.current().nextInt(10000);
+    }
+}
---
+++ messaging/src/test/java/org/axonframework/messaging/OnErrorContinueMessageStreamTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2010-2024. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.messaging;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Test class validating the {@link OnErrorContinueMessageStream} through the {@link MessageStreamTest} suite.
+ *
+ * @author Allard Buijze
+ * @author Steven van Beelen
+ */
+class OnErrorContinueMessageStreamTest extends MessageStreamTest<String> {
+
+    @Override
+    MessageStream<Message<String>> testSubject(List<Message<String>> messages) {
+        return new OnErrorContinueMessageStream<>(MessageStream.fromIterable(messages), error -> MessageStream.empty());
+    }
+
+    @Override
+    MessageStream<Message<String>> failingTestSubject(List<Message<String>> messages, Exception failure) {
+        return new OnErrorContinueMessageStream<>(MessageStream.fromIterable(messages)
+                                                               .concatWith(MessageStream.failed(failure)),
+                                                  error -> MessageStream.failed(failure));
+    }
+
+    @Override
+    String createRandomValidEntry() {
+        return "test-" + ThreadLocalRandom.current().nextInt(10000);
+    }
+}
