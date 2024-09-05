59cfbcc1f0af3c82941f10d22c1cccc433f4b44b
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-09-05 16:53:09 +0200

    Fine tune AsyncEventStore interface
    - Drop all streaming information in favor of the StreamableEventSource
    - Make parameters notNull and notEmpty.
    - Fine tune JavaDoc
    - Drop snapshotting TODO, as that will be taken care of in a separate
    PR and thus branch.
    
    #3101

--- eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/AsyncEventStore.java
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/AsyncEventStore.java
@@ -1,17 +1,32 @@
+/*
+ * Copyright (c) 2010-2024. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.axonframework.eventsourcing.eventstore;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.axonframework.common.infra.DescribableComponent;
 import org.axonframework.eventhandling.EventMessage;
-import org.axonframework.eventhandling.TrackingToken;
-import org.axonframework.messaging.MessageStream;
 import org.axonframework.messaging.unitofwork.ProcessingContext;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.concurrent.CompletableFuture;
-
 /**
- * TODO fill in
+ * Infrastructure component providing the means to start an {@link EventStoreTransaction} to
+ * {@link EventStoreTransaction#appendEvent(EventMessage) append events} and
+ * {@link EventStoreTransaction#source(SourcingCondition, ProcessingContext) event source} models from the underlying
+ * storage solution.
  *
  * @author Allard Buijze
  * @author Rene de Waele
@@ -23,26 +38,16 @@ public interface AsyncEventStore extends DescribableComponent {
     /**
      * Retrieves the {@link EventStoreTransaction transaction for appending events} for the given
      * {@code processingContext}. If no transaction is available, a new, empty transaction is created.
+     * <p>
+     * The given {@code context} ensure the returned {@code EventStoreTransaction}
+     * {@link EventStoreTransaction#source(SourcingCondition, ProcessingContext) sources} and
+     * {@link EventStoreTransaction#appendEvent(EventMessage) appends} from the desired location.
      *
      * @param processingContext The context for which to retrieve the {@link EventStoreTransaction}.
-     * @return The {@link EventStoreTransaction}, existing or newly created, for the given {@code processingContext}.
+     * @param context           The (bounded) context for which to retrieve the {@link EventStoreTransaction}.
+     * @return The {@link EventStoreTransaction}, existing or newly created, for the given {@code processingContext} and
+     * {@code context}.
      */
-    EventStoreTransaction transaction(ProcessingContext processingContext, String context);
-
-    /**
-     * @param condition
-     * @return
-     */
-    MessageStream<? extends EventMessage<?>> stream(StreamingCondition condition);
-
-    // Token operations?
-    CompletableFuture<TrackingToken> headToken();
-
-    CompletableFuture<TrackingToken> tailToken();
-
-    CompletableFuture<TrackingToken> tokenAt(Instant at);
-
-    CompletableFuture<TrackingToken> tokenSince(Duration since);
-
-    // TODO snapshots? Or separate SnapshotStore? Or layered on impl i.o. on the interface? Or a SnapshotStorageEngine that lives next to the EventStorageEngine, and are both used by this interface?
+    EventStoreTransaction transaction(@NotNull ProcessingContext processingContext,
+                                      @NotEmpty String context);
 }
