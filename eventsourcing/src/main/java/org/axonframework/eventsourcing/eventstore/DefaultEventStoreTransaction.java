e07e4a00e589e15af4a4748155c10e1f7a6520cd
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-09-05 16:49:55 +0200

    Fine tune EventStoreTransaction
    - Add NotNull annotations for API clarity
    - Remove TODO on the EventStoreTransaction#appendEvent, as the
    AppendCondition will be constructed as a result of all the
    EventStoreTransaction#source operations
    - Clarify mapping of the SourcingConditions to an AppendCondition in the
     JavaDoc.
    - Adjust the EventStoreTransaction#onAppend to add a callback for each
    invocation of the method instead of overriding it.
    - Rename QueueingEventStoreTransaction to DefaultEventStoreTransaction,
    as it would be the default EventStoreTransaction to use
    - Add JavaDoc to the DefaultEventStoreTransaction
    
    #3101

---
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/DefaultEventStoreTransaction.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2010-2024. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.eventsourcing.eventstore;
+
+import jakarta.validation.constraints.NotNull;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.messaging.MessageStream;
+import org.axonframework.messaging.unitofwork.ProcessingContext;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Consumer;
+
+/**
+ * The default {@link EventStoreTransaction}.
+ * <p>
+ * While {@link #source(SourcingCondition, ProcessingContext) sourcing} it will map the {@link SourcingCondition} into
+ * an {@link AppendCondition} for {@link #appendEvent(EventMessage) appending}, taking into account several sourcing
+ * invocation might have occurred in the same {@link ProcessingContext}. During
+ * {@link #appendEvent(EventMessage) appending} it will pass along a collection of {@link EventMessage events} to an
+ * {@link AsyncEventStorageEngine} is part of the prepare commit phase of the {@link ProcessingContext}.
+ *
+ * @author Steven van Beelen
+ * @since 5.0.0
+ */
+public class DefaultEventStoreTransaction implements EventStoreTransaction {
+
+    private static final ProcessingContext.ResourceKey<AppendCondition> appendConditionKey =
+            ProcessingContext.ResourceKey.create("appendCondition");
+    private static final ProcessingContext.ResourceKey<List<EventMessage<?>>> eventQueueKey =
+            ProcessingContext.ResourceKey.create("eventQueue");
+
+    private final AsyncEventStorageEngine eventStorageEngine;
+    private final ProcessingContext context; // the context this transaction belongs too
+    private final List<Consumer<EventMessage<?>>> callbacks = new CopyOnWriteArrayList<>();
+
+    /**
+     * Constructs a {@link DefaultEventStoreTransaction} using the given {@code eventStorageEngine} to
+     * {@link #appendEvent(EventMessage) append events} originating from the given {@code context}.
+     *
+     * @param eventStorageEngine The {@link AsyncEventStorageEngine} used to
+     *                           {@link #appendEvent(EventMessage) append events} with.
+     * @param context            The {@link ProcessingContext} from which to
+     *                           {@link #appendEvent(EventMessage) append events} and attach resources like the
+     *                           {@link EventStoreTransaction#APPEND_POSITION_KEY} to.
+     */
+    public DefaultEventStoreTransaction(@NotNull AsyncEventStorageEngine eventStorageEngine,
+                                        @NotNull ProcessingContext context) {
+        this.eventStorageEngine = eventStorageEngine;
+        this.context = context;
+    }
+
+    @Override
+    public MessageStream<EventMessage<?>> source(@NotNull SourcingCondition condition,
+                                                 @NotNull ProcessingContext context) {
+        context.updateResource(
+                appendConditionKey,
+                appendCondition -> appendCondition == null
+                        ? AppendCondition.from(condition)
+                        : appendCondition.with(condition)
+        );
+        return eventStorageEngine.source(condition);
+    }
+
+    @Override
+    public void appendEvent(@NotNull EventMessage<?> eventMessage) {
+        List<EventMessage<?>> eventQueue = context.computeResourceIfAbsent(
+                eventQueueKey,
+                () -> {
+                    attachAppendEventsStep();
+                    return new CopyOnWriteArrayList<>();
+                }
+        );
+        eventQueue.add(eventMessage);
+
+        callbacks.forEach(callback -> callback.accept(eventMessage));
+    }
+
+    private void attachAppendEventsStep() {
+        context.onPrepareCommit(
+                commitContext -> {
+                    AppendCondition appendCondition =
+                            commitContext.computeResourceIfAbsent(appendConditionKey, AppendCondition::none);
+                    List<EventMessage<?>> eventQueue = commitContext.getResource(eventQueueKey);
+                    return eventStorageEngine.appendEvents(appendCondition, eventQueue)
+                                             .whenComplete((position, exception) -> commitContext.putResource(
+                                                     APPEND_POSITION_KEY, position
+                                             ));
+                }
+        );
+    }
+
+    @Override
+    public void onAppend(@NotNull Consumer<EventMessage<?>> callback) {
+        callbacks.add(callback);
+    }
+}
--- eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/EventStoreTransaction.java
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/EventStoreTransaction.java
@@ -16,6 +16,7 @@
 
 package org.axonframework.eventsourcing.eventstore;
 
+import jakarta.validation.constraints.NotNull;
 import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.messaging.MessageStream;
 import org.axonframework.messaging.unitofwork.ProcessingContext;
@@ -24,9 +25,10 @@ import java.util.function.Consumer;
 
 /**
  * Interface describing the actions that can be taken on a transaction to source a model from the
- * {@link AsyncEventStore} based on the resulting {@link MessageStream}. Note that this transaction includes operations
- * for {@link #source(SourcingCondition, ProcessingContext) sourcing} the model as well as
- * {@link #appendEvent(EventMessage) appending events}.
+ * {@link AsyncEventStore} based on the resulting {@link MessageStream}.
+ * <p>
+ * Note that this transaction includes operations for {@link #source(SourcingCondition, ProcessingContext) sourcing} the
+ * model as well as {@link #appendEvent(EventMessage) appending events}.
  *
  * @author Allard Buijze
  * @author Steven van Beelen
@@ -42,9 +44,13 @@ public interface EventStoreTransaction {
 
     /**
      * Sources a {@link MessageStream} of type {@link EventMessage} based on the given {@code condition} that can be
-     * used to rehydrate a model. The given {@code context} is used to perform tasks at the right
+     * used to rehydrate a model.
+     * <p>
+     * The given {@code context} is used to perform tasks at the right
      * {@link org.axonframework.messaging.unitofwork.ProcessingLifecycle.Phase phases} and register resources throughout
-     * its lifecycle.
+     * its lifecycle. All {@code condition} instances provided to source as part of the given {@code context} should be
+     * used to derive the {@link AppendCondition} typically used by the {@link #appendEvent(EventMessage)} method's
+     * implementation.
      *
      * @param condition The {@link SourcingCondition} used to retrieve the {@link MessageStream} containing the sequence
      *                  of events that can rehydrate a model.
@@ -54,7 +60,8 @@ public interface EventStoreTransaction {
      * @return The {@link MessageStream} of type {@link EventMessage} containing to the event sequence complying to the
      * given {@code condition}.
      */
-    MessageStream<? extends EventMessage<?>> source(SourcingCondition condition, ProcessingContext context);
+    MessageStream<EventMessage<?>> source(@NotNull SourcingCondition condition,
+                                          @NotNull ProcessingContext context);
 
     /**
      * Appends an {@code eventMessage} to be appended to an {@link AsyncEventStore} in this transaction with the given
@@ -62,17 +69,16 @@ public interface EventStoreTransaction {
      *
      * @param eventMessage The {@link EventMessage} to append.
      */
-    //TODO does a condition per event make sense? Wouldn't this apply for the entire?
-    // TODO we should construct an AppendCondition based on the call to source (or multiple)
-    void appendEvent(EventMessage<?> eventMessage);
+    void appendEvent(@NotNull EventMessage<?> eventMessage);
 
     /**
-     * Registers a {@code callback} to invoke when an event is
-     * {@link #appendEvent(EventMessage) appended} to this transaction. Each {@code callback}
-     * registration adds a new callback that is invoked on the {@code appendEvent(EventMessage, AppendCondition)}
-     * operation.
+     * Registers a {@code callback} to invoke when an event is {@link #appendEvent(EventMessage) appended} to this
+     * transaction.
+     * <p>
+     * Each {@code callback} registration adds a new callback that is invoked on the
+     * {@code appendEvent(EventMessage, AppendCondition)} operation.
      *
      * @param callback A {@link Consumer} to invoke when an event is appended in this transaction.
      */
-    void onAppend(Consumer<EventMessage<?>> callback);
+    void onAppend(@NotNull Consumer<EventMessage<?>> callback);
 }
