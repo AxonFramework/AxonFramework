1cadd7c0a2a515f0d0c3867789015c0b4c547899
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-09-11 16:00:18 +0200

    Add todo clarifying it will be deprecated entirely
    Add todo clarifying it will be deprecated entirely
    
    #3101

--- messaging/src/main/java/org/axonframework/eventhandling/TrackedEventMessage.java
+++ messaging/src/main/java/org/axonframework/eventhandling/TrackedEventMessage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2023. Axon Framework
+ * Copyright (c) 2010-2024. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ package org.axonframework.eventhandling;
  * @param <T> The type of payload contained in this Message
  * @author Rene de Waele
  */
-// TODO Shouldn't we make the tracked-info part of **every** EventMessage?
+// TODO replace this for a entry/pair of TrackingToken-to-MessageImpl
 public interface TrackedEventMessage<T> extends EventMessage<T> {
 
     /**
