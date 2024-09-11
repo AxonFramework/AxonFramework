6eb8495706b76cff02acd25e9076df18017bdfb0
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-09-11 15:53:16 +0200

    Rename AsyncEmbeddedEventStore to SimpleEventStore
    Rename the AsyncEmbeddedEventStore to the SimpleEventStore. The biggest
    difference, is that the SimpleEventStore only works for a single context
    . Hence, the given context on all implemented methods is validated
    against the provided context during construction. Furthermore, remove
    the Clock, as that allows us to delegate all timed operations to the
    EventStorageEngine, as well as solidify the storage engine's task to set
     the time of the new events (once that's in place).
    
    #3101

---
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/SimpleEventStore.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2010-2024. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.eventsourcing.eventstore;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.axonframework.common.infra.ComponentDescriptor;
+import org.axonframework.eventhandling.TrackedEventMessage;
+import org.axonframework.eventhandling.TrackingToken;
+import org.axonframework.messaging.MessageStream;
+import org.axonframework.messaging.unitofwork.ProcessingContext;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
+import javax.annotation.Nonnull;
+
+/**
+ * Simple implementation of the {@link AsyncEventStore} and {@link StreamableEventSource} fixed for a <b>single</b>
+ * {@code context}.
+ * <p>
+ * Invocations of this event store that provide a different {@code context} than is used during construction of this
+ * event store will fail with an {@link IllegalArgumentException}.
+ *
+ * @author Allard Buijze
+ * @author Rene de Waele
+ * @author Steven van Beelen
+ * @since 3.0
+ */
+public class SimpleEventStore implements AsyncEventStore, StreamableEventSource<TrackedEventMessage<?>> {
+
+    private final ProcessingContext.ResourceKey<EventStoreTransaction> eventStoreTransactionKey =
+            ProcessingContext.ResourceKey.create("eventStoreTransaction");
+
+    private final AsyncEventStorageEngine eventStorageEngine;
+    private final String context;
+
+    /**
+     * Constructs a {@link SimpleEventStore} using the given {@code eventStorageEngine} to start
+     * {@link #transaction(ProcessingContext, String) transactions} and
+     * {@link #open(String, StreamingCondition) open event streams} with.
+     * <p>
+     * Invocations of this event store are validated against the given {@code context}.
+     *
+     * @param eventStorageEngine The {@link AsyncEventStorageEngine} used to start
+     *                           {@link #transaction(ProcessingContext, String) transactions} and
+     *                           {@link #open(String, StreamingCondition) open event streams} with.
+     * @param context            The (bounded) {@code context} this event store operates in.
+     */
+    public SimpleEventStore(@NotNull AsyncEventStorageEngine eventStorageEngine,
+                            @NotEmpty String context) {
+        this.eventStorageEngine = eventStorageEngine;
+        this.context = context;
+    }
+
+    @Override
+    public EventStoreTransaction transaction(@NotNull ProcessingContext processingContext,
+                                             @NotEmpty String context) {
+        validate(context);
+        return processingContext.computeResourceIfAbsent(
+                eventStoreTransactionKey,
+                () -> new DefaultEventStoreTransaction(eventStorageEngine, processingContext)
+        );
+    }
+
+    @Override
+    public MessageStream<TrackedEventMessage<?>> open(String context, StreamingCondition condition) {
+        validate(context);
+        return eventStorageEngine.stream(condition);
+    }
+
+    @Override
+    public CompletableFuture<TrackingToken> headToken(String context) {
+        validate(context);
+        return eventStorageEngine.headToken();
+    }
+
+    @Override
+    public CompletableFuture<TrackingToken> tailToken(String context) {
+        validate(context);
+        return eventStorageEngine.tailToken();
+    }
+
+    @Override
+    public CompletableFuture<TrackingToken> tokenAt(String context, Instant at) {
+        validate(context);
+        return eventStorageEngine.tokenAt(at);
+    }
+
+    @Override
+    public CompletableFuture<TrackingToken> tokenSince(String context, Duration since) {
+        validate(context);
+        return eventStorageEngine.tokenSince(since);
+    }
+
+    @Override
+    public void describeTo(@Nonnull ComponentDescriptor descriptor) {
+        descriptor.describeProperty("eventStorageEngine", eventStorageEngine);
+        descriptor.describeProperty("context", context);
+    }
+
+    private void validate(String context) {
+        if (this.context.equals(context)) {
+            return;
+        }
+        throw new IllegalArgumentException("Context '" + context + "' does not match context '" + this.context + "'");
+    }
+}
