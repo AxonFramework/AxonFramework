cafb52f265021e35c72ed7d0e5481f26621169e7
Weronika Trzaska <wtrzas2@illinois.edu>
2024-10-02 22:37:36 +0200

    Fixed style check
    (cherry picked from commit f933856a584e94192c34f49c9389fd8baad5e057)

--- messaging/src/test/java/org/axonframework/messaging/annotation/AnnotatedMessageHandlingMemberTest.java
+++ messaging/src/test/java/org/axonframework/messaging/annotation/AnnotatedMessageHandlingMemberTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2023. Axon Framework
+ * Copyright (c) 2010-2024. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,12 +38,16 @@ class AnnotatedMessageHandlingMemberTest {
 
     @BeforeEach
     void setUp() {
-        testSubject = new AnnotatedMessageHandlingMember<>(
-                AnnotatedHandler.class.getMethods()[0],
-                EventMessage.class,
-                String.class,
-                ClasspathParameterResolverFactory.forClass(AnnotatedHandler.class)
-        );
+        try {
+            testSubject = new AnnotatedMessageHandlingMember<>(
+                    AnnotatedHandler.class.getMethod("handlingMethod", String.class),
+                    EventMessage.class,
+                    String.class,
+                    ClasspathParameterResolverFactory.forClass(AnnotatedHandler.class)
+            );
+        } catch (NoSuchMethodException e) {
+            fail(e.getMessage());
+        }
     }
 
     @Test
