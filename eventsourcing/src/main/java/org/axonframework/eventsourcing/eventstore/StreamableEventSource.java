6b184d19232f26d359adf6673b45c8c740426da1
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-10-04 16:02:32 +0200

    Drop default tokenSince impl to ensure a Clock is taken into account
    Drop default tokenSince impl to ensure a Clock is taken into account
    
    #3101

--- eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/StreamableEventSource.java
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/StreamableEventSource.java
@@ -110,8 +110,6 @@ public interface StreamableEventSource<E extends EventMessage<?>> {
      * @return A {@link CompletableFuture} of {@link TrackingToken} pointing at a position before the given
      * {@code duration} of the {@link MessageStream event stream} for the given {@code context}.
      */
-    default CompletableFuture<TrackingToken> tokenSince(@Nonnull String context,
-                                                        @Nonnull Duration since) {
-        return tokenAt(context, Instant.now().minus(since));
-    }
+    CompletableFuture<TrackingToken> tokenSince(@Nonnull String context,
+                                                @Nonnull Duration since);
 }
