7932522b167de805462962ad3cb91e51250c5405
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-12-24 10:41:32 +0100

    Add Context operations and data validation
    Add Context operations and data validation. The Context operations are
    required to remove the ubiquitous use of the IndexedEventMessage in
    favor of a TaggedEventMessage, with the Set<Tag> moved to the Context
    when events are given to the event stream
    
    #enhancement/index-to-tag-rename

--- eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/Tag.java
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/Tag.java
@@ -17,8 +17,14 @@
 package org.axonframework.eventsourcing.eventstore;
 
 import jakarta.annotation.Nonnull;
+import org.axonframework.common.Assert;
+import org.axonframework.common.Context;
+import org.axonframework.common.Context.ResourceKey;
 import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.messaging.unitofwork.ProcessingContext;
+
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * An {@code Tag} refers to fields and their values within which an {@link org.axonframework.eventhandling.EventMessage}
@@ -42,4 +48,39 @@ import org.axonframework.messaging.unitofwork.ProcessingContext;
 public record Tag(@Nonnull String key,
                   @Nonnull String value) {
 
+    /**
+     * Compact constructor validating that the given {@code key} and {@code value} are not {@code null}.
+     */
+    public Tag {
+        Assert.notNull(key, () -> "A Tag's key cannot be null");
+        Assert.notNull(value, () -> "A Tag's value cannot be null");
+    }
+
+    /**
+     * The {@link ResourceKey} used whenever a {@link Context} would contain a {@link Set} of {@link Tag Tags}.
+     */
+    public static final ResourceKey<Set<Tag>> RESOURCE_KEY = ResourceKey.create("tags");
+
+    /**
+     * Adds the given {@code token} to the given {@code context} using the {@link #RESOURCE_KEY}.
+     *
+     * @param context The {@link Context} to add the given {@code token} to.
+     * @param tags    The {@link Set} of {@link Tag Tags} to add to the given {@code context} using the
+     *                {@link #RESOURCE_KEY}.
+     */
+    public static Context addToContext(Context context, Set<Tag> tags) {
+        return context.withResource(RESOURCE_KEY, tags);
+    }
+
+    /**
+     * Returns an {@link Optional} of {@link Tag Tags}, returning the resource keyed under the {@link #RESOURCE_KEY} in
+     * the given {@code context}.
+     *
+     * @param context The {@link Context} to retrieve the {@link Tag Tags} from, if present.
+     * @return An {@link Optional} of {@link Tag Tags}, returning the resource keyed under the {@link #RESOURCE_KEY} in
+     * the given {@code context}.
+     */
+    public static Optional<Set<Tag>> fromContext(Context context) {
+        return Optional.ofNullable(context.getResource(RESOURCE_KEY));
+    }
 }
