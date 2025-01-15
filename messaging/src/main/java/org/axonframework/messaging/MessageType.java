91ecd1e7b8903e51d2044af06e8e70bc4b42760b
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-01-15 12:57:59 +0100

    Introduce the MessageType
    Introduce the MessageType, forming a combination of a QualifiedName and
    a version. The version will thus be extracted from the QualifiedName, as
     versioning isn't typically a concern to qualified names. However,
     messages require a combination of a name and version; that's the intent
      of the MessageType record.

---
+++ messaging/src/main/java/org/axonframework/messaging/MessageType.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.messaging;
+
+import jakarta.annotation.Nonnull;
+import org.axonframework.common.Assert;
+import org.axonframework.common.StringUtils;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Interface describing a {@link QualifiedName qualified @code qualifiedName} and {@code version}.
+ * <p>
+ * The {@code QualifiedName} is useful to provide clear names to {@link Message Messages},
+ * {@link MessageHandler MessageHandlers}, and other components that require naming.
+ * <p>
+ * When you do not require a version for typing, consider using the {@code QualifiedName} directly instead.
+ *
+ * @param qualifiedName The {@code QualifiedName} of this {@code MessageType}.
+ * @param version       the version of this {@code MessageType}.
+ * @author Allard Buijze
+ * @author Mateusz Nowak
+ * @author Steven van Beelen
+ * @since 5.0.0
+ */
+public record MessageType(@Nonnull QualifiedName qualifiedName,
+                          @Nonnull String version) implements Serializable {
+
+    /**
+     * The default version of a {@code MessageType} when none is given. Set to {@code 0.0.1}.
+     */
+    public static final String DEFAULT_VERSION = "0.0.1";
+
+    private static final String VERSION_DELIMITER = "#";
+    private static final String DEBUG_STRING_REGEX = "^([^#]+)#([^#]+)$";
+    private static final Pattern DEBUG_STRING_PATTERN = Pattern.compile(DEBUG_STRING_REGEX);
+    private static final int QUALIFIED_NAME_GROUP = 1;
+    private static final int VERSION_GROUP = 2;
+
+    /**
+     * Compact constructor validating that the given {@code qualifiedName} is non-null.
+     */
+    public MessageType {
+        requireNonNull(qualifiedName, "The qualified qualifiedName cannot be null.");
+        Assert.assertThat(version, StringUtils::nonEmptyOrNull,
+                          () -> new IllegalArgumentException(
+                                  "The given version [" + version + "] is unsupported because it is null or empty."
+                          ));
+    }
+
+    /**
+     * A {@code MessageType} constructor setting the {@code version} to {@code null}.
+     *
+     * @param name The {@code QualifiedName} of this {@code MessageType}.
+     */
+    public MessageType(@Nonnull QualifiedName name) {
+        this(requireNonNull(name, "The given QualifiedName cannot be null"), DEFAULT_VERSION);
+    }
+
+    /**
+     * A {@code MessageType} constructor using the given {@code qualifiedName} invoking the
+     * {@link QualifiedName#QualifiedName(String)} constructor. The {@code version} is fixed to {@code null}.
+     *
+     * @param qualifiedName The qualifiedName for the {@link QualifiedName#QualifiedName(String) QualifiedName} for the
+     *                      {@code MessageType} under construction.
+     */
+    public MessageType(@Nonnull String qualifiedName) {
+        this(new QualifiedName(requireNonNull(qualifiedName, "The given qualified qualifiedName cannot be null.")),
+             DEFAULT_VERSION);
+    }
+
+    /**
+     * A {@code MessageType} constructor using the given {@code qualifiedName} to invoke the
+     * {@link QualifiedName#QualifiedName(String)} constructor to derive the {@code qualifiedName} for this
+     * {@code MessageType}.
+     *
+     * @param qualifiedName The qualified qualifiedName used to invoke {@link QualifiedName#QualifiedName(String)}
+     *                      constructor for the {@code MessageType} under construction.
+     * @param version       The version for the {@code MessageType} under construction.
+     */
+    public MessageType(@Nonnull String qualifiedName,
+                       @Nonnull String version) {
+        this(new QualifiedName(requireNonNull(qualifiedName, "The given qualified qualifiedName cannot be null.")),
+             requireNonNull(version, "The given version cannot be null."));
+    }
+
+    /**
+     * A {@code MessageType} constructor using the given {@code namespace} and {@code localName} to invoke the
+     * {@link QualifiedName#QualifiedName(String, String)} constructor to derive the {@code qualifiedName} for this
+     * {@code MessageType}.
+     *
+     * @param namespace The namespace used to invoke {@link QualifiedName#QualifiedName(String, String)} constructor for
+     *                  the {@code MessageType} under construction.
+     * @param localName The local qualifiedName used to invoke {@link QualifiedName#QualifiedName(String, String)}
+     *                  constructor for the {@code MessageType} under construction.
+     * @param version   The version for the {@code MessageType} under construction.
+     */
+    public MessageType(String namespace,
+                       @Nonnull String localName,
+                       @Nonnull String version) {
+        this(new QualifiedName(namespace, requireNonNull(localName, "The given localName cannot be null.")),
+             requireNonNull(version, "The given version cannot be null."));
+    }
+
+    /**
+     * A {@code MessageType} constructor using the given {@code clazz} to invoke the
+     * {@link QualifiedName#QualifiedName(Class)} constructor  to derive the {@code qualifiedName} for this
+     * {@code MessageType}.
+     *
+     * @param clazz   The {@code Class} used to invoke {@link QualifiedName#QualifiedName(Class)} constructor for the
+     *                {@code MessageType} under construction.
+     * @param version The version for the {@code MessageType} under construction.
+     */
+    public MessageType(@Nonnull Class<?> clazz,
+                       @Nonnull String version) {
+        this(new QualifiedName(requireNonNull(clazz, "The given Class cannot be null.")),
+             requireNonNull(version, "The given version cannot be null."));
+    }
+
+    /**
+     * A {@code MessageType} constructor using the given {@code clazz} to invoke the
+     * {@link QualifiedName#QualifiedName(Class)} constructor  to derive the {@code qualifiedName} for this
+     * {@code MessageType}. The {@code version} is fixed to {@code null}.
+     *
+     * @param clazz The {@code Class} used to invoke {@link QualifiedName#QualifiedName(Class)} constructor for the
+     *              {@code MessageType} under construction.
+     */
+    public MessageType(@Nonnull Class<?> clazz) {
+        this(clazz, DEFAULT_VERSION);
+    }
+
+    public String name() {
+        return qualifiedName.toString();
+    }
+
+    /**
+     * Reconstruct a {@code MessageType} based on the output of {@link MessageType#toString()}.
+     * <p>
+     * The output of {@code MessageType#toString()} is a concatenation of the {@link #qualifiedName()} and
+     * {@link #version()}, split by means of a hashtag ({@code #}).
+     * <p>
+     * Thus, if the given {@code String} equals {@code "my.context:BusinessName:1.0.5"}, the {@code #qualifiedName()} is
+     * set to a {@link QualifiedName} of {@code "my.context.BusinessName"} and the {@code #version()} is set to
+     * {@code "1.0.5"}.
+     *
+     * @param s The output of {@link MessageType#toString()}, given to reconstruct it into a {@code MessageType}.
+     * @return A reconstructed {@code MessageType} based on the expected output of {@link MessageType#toString()}.
+     */
+    public static MessageType fromString(@Nonnull String s) {
+        Matcher matcher = DEBUG_STRING_PATTERN.matcher(
+                Objects.requireNonNull(s, "Cannot construct a MessageType based on a null or empty String.")
+        );
+        Assert.isTrue(matcher.matches(),
+                      () -> "The given simple String [" + s + "] does not match the expected pattern.");
+        return new MessageType(new QualifiedName(matcher.group(QUALIFIED_NAME_GROUP)), matcher.group(VERSION_GROUP));
+    }
+
+    /**
+     * The output of {@code MessageType#toString()} is a concatenation of the {@link #qualifiedName()} and
+     * {@link #version()}, split by means of a hashtag ({@code #}).
+     * <p>
+     * Thus, if {@code #qualifiedName()} returns {@code "my.context.BusinessName"} and the {@code #version()} returns
+     * {@code "1.0.5"}, the result of <b>this</b> operation would be {@code "my.context:BusinessName:1.0.5"}.
+     *
+     * @return A combination of the {@link #qualifiedName()} and {@link #version()}, separated by a hashtag.
+     */
+    @Override
+    public String toString() {
+        return qualifiedName + VERSION_DELIMITER + version;
+    }
+}
