3a940b36706883de078af4467566c406217cca6b
Mateusz Nowak <mateusz@nakodach.pl>
2025-01-24 14:19:29 +0100

    domain/tracked wording

--- integrationtests/src/test/java/org/axonframework/integrationtests/eventsourcing/eventstore/jpa/LegacyJpaEventStorageEngineTest.java
+++ integrationtests/src/test/java/org/axonframework/integrationtests/eventsourcing/eventstore/jpa/LegacyJpaEventStorageEngineTest.java
@@ -23,12 +23,17 @@ import org.axonframework.common.jpa.EntityManagerProvider;
 import org.axonframework.common.jpa.SimpleEntityManagerProvider;
 import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.eventhandling.GapAwareTrackingToken;
+import org.axonframework.eventhandling.GenericTrackedDomainEventMessage;
+import org.axonframework.eventhandling.GenericTrackedEventMessage;
 import org.axonframework.eventhandling.GlobalSequenceTrackingToken;
 import org.axonframework.eventhandling.TrackingToken;
 import org.axonframework.eventsourcing.eventstore.AggregateBasedStorageEngineTestSuite;
 import org.axonframework.eventsourcing.eventstore.StreamingCondition;
 import org.axonframework.eventsourcing.eventstore.jdbc.JdbcSQLErrorCodesResolver;
 import org.axonframework.eventsourcing.eventstore.jpa.LegacyJpaEventStorageEngine;
+import org.axonframework.messaging.QualifiedNameUtils;
+import org.axonframework.serialization.LazyDeserializingObject;
+import org.axonframework.serialization.SerializedMessage;
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.TestSerializer;
 import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager;
@@ -47,6 +52,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Collections;
 
@@ -85,7 +91,9 @@ class LegacyJpaEventStorageEngineTest extends AggregateBasedStorageEngineTestSui
 
     @Override
     protected EventMessage<String> convertPayload(EventMessage<?> original) {
-        return original.withConvertedPayload(p -> TEST_SERIALIZER.convert(p, String.class));
+//        return original.withConvertedPayload(p -> TEST_SERIALIZER.convert(p, String.class));
+        return original.withConvertedPayload(p -> new String((byte[]) p, StandardCharsets.UTF_8).replaceAll("\"", ""));
+//        return original.withConvertedPayload(p -> TEST_SERIALIZER.convert(p, String.class).replaceAll("\"", ""));
     }
 
     @Test
