54b68dc9c1d63a9b7775a4d58a48947612672461
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-02-06 10:51:14 +0100

    Fine tune JavaDoc
    Fine tune JavaDoc
    
    #3067

--- messaging/src/main/java/org/axonframework/commandhandling/CommandBus.java
+++ messaging/src/main/java/org/axonframework/commandhandling/CommandBus.java
@@ -18,6 +18,8 @@ package org.axonframework.commandhandling;
 
 import org.axonframework.common.infra.DescribableComponent;
 import org.axonframework.messaging.Message;
+import org.axonframework.messaging.MessageType;
+import org.axonframework.messaging.QualifiedName;
 import org.axonframework.messaging.unitofwork.ProcessingContext;
 
 import java.util.concurrent.CompletableFuture;
@@ -25,9 +27,14 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 /**
- * The mechanism that dispatches Command objects to their appropriate CommandHandler. CommandHandlers can subscribe and
- * unsubscribe to specific commands (identified by their {@link CommandMessage#getCommandName() name}) on the command
- * bus. Only a single handler may be subscribed for a single command name at any time.
+ * The mechanism that dispatches {@link CommandMessage commands} to their appropriate
+ * {@link CommandHandler command handler}.
+ * <p>
+ * Command handlers can {@link #subscribe(QualifiedName, CommandHandler) subscribe} and unsubscribe to specific commands
+ * on the command bus. A command being {@link #dispatch(CommandMessage, ProcessingContext) dispatched} matches a command
+ * handler based on the {@link QualifiedName} present in the {@link CommandMessage#type() command's type}.
+ * <p>
+ * Only a <em>single</em> handler may be subscribed for a single command name at any time.
  *
  * @author Allard Buijze
  * @since 0.5
@@ -35,12 +42,16 @@ import javax.annotation.Nullable;
 public interface CommandBus extends CommandHandlerRegistry, DescribableComponent {
 
     /**
-     * Dispatch the given {@code command} to the CommandHandler subscribed to the given {@code command}'s name.
+     * Dispatch the given {@code command} to the {@link CommandHandler command handler}
+     * {@link #subscribe(QualifiedName, CommandHandler) subscribed} to the given {@code command}'s name. The name is
+     * typically deferred from the {@link Message#type()}, which contains a {@link MessageType#qualifiedName()}.
      *
-     * @param command           The Command to dispatch
-     * @param processingContext The processing context under which the command is being published (can be {@code null})
-     * @return The CompletableFuture providing the result of the command, once finished
-     * @throws NoHandlerForCommandException when no command handler is registered for the given {@code command}'s name.
+     * @param command           The command to dispatch.
+     * @param processingContext The processing context under which the command is being published (can be
+     *                          {@code null}).
+     * @return The {@code CompletableFuture} providing the result of the command, once finished.
+     * @throws NoHandlerForCommandException when no {@link CommandHandler command handler} is registered for the given
+     *                                      {@code command}'s name.
      */
     CompletableFuture<? extends Message<?>> dispatch(@Nonnull CommandMessage<?> command,
                                                      @Nullable ProcessingContext processingContext);
