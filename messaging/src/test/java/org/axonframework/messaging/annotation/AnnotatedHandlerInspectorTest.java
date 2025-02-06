553e9d38dce06307729084de605d8506ac8914fe
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-02-06 13:03:10 +0100

    Take into account that the return type might already be a MessageStream
    Take into account that the return type might already be a MessageStream
    
    #3067

--- messaging/src/test/java/org/axonframework/messaging/annotation/AnnotatedHandlerInspectorTest.java
+++ messaging/src/test/java/org/axonframework/messaging/annotation/AnnotatedHandlerInspectorTest.java
@@ -76,6 +76,9 @@ class AnnotatedHandlerInspectorTest {
                     r -> new GenericMessage<>(new MessageType(r.getClass()), r)
             ));
         }
+        if (result instanceof MessageStream<?> stream) {
+            return stream;
+        }
         return MessageStream.just(new GenericMessage<>(new MessageType(ObjectUtils.nullSafeTypeOf(result)), result));
     }
 
