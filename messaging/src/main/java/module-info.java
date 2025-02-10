e59bd717f8d9279c4d9d08403556e2357b3445a1
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-02-10 16:37:32 +0100

    Expand on the module-info.java file
    Expand on the module-info.java file by:
     - exporting all main folders
     - providing all service loaded classes
     - using all service loaded classes itself
     - opening the deadline fold to XStream for reflection
    
    #3067

--- messaging/src/main/java/module-info.java
+++ messaging/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module axon.messaging {
     requires cache.api;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
-    requires com.fasterxml.jackson.module.paramnames;
     requires db.scheduler;
     requires ehcache;
     requires jakarta.annotation;
@@ -21,4 +20,61 @@ module axon.messaging {
     requires quartz;
     requires reactor.core;
     requires xstream;
+
+    exports org.axonframework.common;
+    exports org.axonframework.commandhandling;
+    exports org.axonframework.deadline;
+    exports org.axonframework.eventhandling;
+    exports org.axonframework.eventhandling.scheduling.jobrunr to com.fasterxml.jackson.databind;
+    exports org.axonframework.lifecycle;
+    exports org.axonframework.messaging;
+    exports org.axonframework.monitoring;
+    exports org.axonframework.queryhandling;
+    exports org.axonframework.serialization;
+    exports org.axonframework.tracing;
+
+    provides org.axonframework.common.property.PropertyAccessStrategy with
+            org.axonframework.common.property.BeanPropertyAccessStrategy,
+            org.axonframework.common.property.DirectPropertyAccessStrategy,
+            org.axonframework.common.property.UniformPropertyAccessStrategy;
+    provides org.axonframework.serialization.ContentTypeConverter with
+            org.axonframework.serialization.converters.ByteArrayToInputStreamConverter,
+            org.axonframework.serialization.converters.BlobToInputStreamConverter,
+            org.axonframework.serialization.converters.InputStreamToByteArrayConverter,
+            org.axonframework.serialization.converters.ByteArrayToStringConverter,
+            org.axonframework.serialization.converters.StringToByteArrayConverter;
+    provides org.axonframework.messaging.annotation.HandlerDefinition with
+            org.axonframework.messaging.annotation.AnnotatedMessageHandlingMemberDefinition;
+    provides org.axonframework.messaging.annotation.HandlerEnhancerDefinition with
+            org.axonframework.commandhandling.annotation.MethodCommandHandlerDefinition,
+            org.axonframework.deadline.annotation.DeadlineMethodMessageHandlerDefinition,
+            org.axonframework.eventhandling.replay.ReplayAwareMessageHandlerWrapper,
+            org.axonframework.messaging.annotation.MessageHandlerInterceptorDefinition,
+            org.axonframework.queryhandling.annotation.MethodQueryMessageHandlerDefinition;
+    provides org.axonframework.messaging.annotation.ParameterResolverFactory with
+            org.axonframework.commandhandling.annotation.CurrentUnitOfWorkParameterResolverFactory,
+            org.axonframework.messaging.annotation.InterceptorChainParameterResolverFactory,
+            org.axonframework.eventhandling.ConcludesBatchParameterResolverFactory,
+            org.axonframework.eventhandling.SequenceNumberParameterResolverFactory,
+            org.axonframework.eventhandling.TrackingTokenParameterResolverFactory,
+            org.axonframework.eventhandling.TimestampParameterResolverFactory,
+            org.axonframework.eventhandling.replay.ReplayParameterResolverFactory,
+            org.axonframework.eventhandling.replay.ReplayContextParameterResolverFactory,
+            org.axonframework.messaging.annotation.DefaultParameterResolverFactory,
+            org.axonframework.messaging.annotation.MessageIdentifierParameterResolverFactory,
+            org.axonframework.messaging.annotation.SourceIdParameterResolverFactory,
+            org.axonframework.messaging.annotation.ResultParameterResolverFactory,
+            org.axonframework.messaging.annotation.ScopeDescriptorParameterResolverFactory,
+            org.axonframework.messaging.annotation.AggregateTypeParameterResolverFactory,
+            org.axonframework.messaging.deadletter.DeadLetterParameterResolverFactory;
+
+    uses org.axonframework.common.IdentifierFactory;
+    uses org.axonframework.common.property.PropertyAccessStrategy;
+    uses org.axonframework.messaging.annotation.ParameterResolverFactory;
+    uses org.axonframework.messaging.annotation.HandlerDefinition;
+    uses org.axonframework.messaging.annotation.HandlerEnhancerDefinition;
+    uses org.axonframework.serialization.ContentTypeConverter;
+
+    opens org.axonframework.deadline to xstream;
+
 }
