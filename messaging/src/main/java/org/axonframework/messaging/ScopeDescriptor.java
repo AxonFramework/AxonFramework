473d6bd58d4d6bd20386cf615240920dd80df884
Mateusz Nowak <mateusz@nakodach.pl>
2025-03-11 12:56:40 +0100

    [#3305] refactor: `messaging` module - remove Java Serialization - part 7

--- messaging/src/main/java/org/axonframework/messaging/ScopeDescriptor.java
+++ messaging/src/main/java/org/axonframework/messaging/ScopeDescriptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2023. Axon Framework
+ * Copyright (c) 2010-2025. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 
 package org.axonframework.messaging;
 
+import java.io.Serializable;
+
 /**
  * Denotes the description of a {@link Scope}. This can be used to figure out in what scope a given message should be
  * handled.
@@ -23,7 +25,7 @@ package org.axonframework.messaging;
  * @author Steven van Beelen
  * @since 3.3
  */
-public interface ScopeDescriptor {
+public interface ScopeDescriptor extends Serializable {
 
     /**
      * Retrieve a {@link String} description of a {@link Scope} object.
