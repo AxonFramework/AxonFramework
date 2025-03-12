a7c940555941669179c851d07fdac0b5d9a23f94
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-03-12 10:24:43 +0100

    Add JavaDoc
    Add JavaDoc

--- messaging/src/main/java/org/axonframework/common/infra/DescribableComponent.java
+++ messaging/src/main/java/org/axonframework/common/infra/DescribableComponent.java
@@ -18,7 +18,22 @@ package org.axonframework.common.infra;
 
 import jakarta.annotation.Nonnull;
 
+/**
+ * Contract allowing components {@link #describeTo(ComponentDescriptor) to describe} themselves with a given
+ * {@link ComponentDescriptor}.
+ *
+ * @author Allard Buijze
+ * @author Mitchel Herrijgers
+ * @author Steven van Beelen
+ * @since 5.0.0
+ */
+@FunctionalInterface
 public interface DescribableComponent {
 
+    /**
+     * Describe the properties of {@code this DescribableComponent} with the given {@code descriptor}.
+     *
+     * @param descriptor The component descriptor to describe {@code this DescribableComponent}n its properties in.
+     */
     void describeTo(@Nonnull ComponentDescriptor descriptor);
 }
