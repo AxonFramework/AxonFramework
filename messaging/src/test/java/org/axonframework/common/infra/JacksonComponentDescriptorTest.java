e1fc44701eace7ad498d98947b2fef1e98dc7417
Mateusz Nowak <mateusz@nakodach.pl>
2025-03-13 14:35:53 +0100

    [#3337] feat: add `_id` and `_type` properties in `JacksonComponentDescriptor`

--- messaging/src/test/java/org/axonframework/common/infra/JacksonComponentDescriptorTest.java
+++ messaging/src/test/java/org/axonframework/common/infra/JacksonComponentDescriptorTest.java
@@ -85,195 +85,182 @@ class JacksonComponentDescriptorTest {
     }
 
     @Test
-    void describeDescribableComponent() {
+    void describeDescribableComponentShouldIncludeTypeAndId() {
         // given
         var component = new SimpleTestComponent("componentValue", 100);
         testSubject.describeProperty("component", component);
 
         // when
         var result = testSubject.describe();
-        var expected = normalizeJson("""
-                                             {
-                                               "component" : {
-                                                 "name" : "componentValue",
-                                                 "value" : 100
-                                               }
-                                             }
-                                             """);
 
         // then
-        assertEquals(expected, normalizeJson(result));
+        assertJsonMatchesPattern(result, """
+                {
+                  "component" : {
+                    "name" : "componentValue",
+                    "value" : 100,
+                    "_type" : "SimpleTestComponent",
+                    "_id" : "*"
+                  }
+                }
+                """);
     }
 
-    @Test
-    void describeCollection() {
-        // given
-        var collection = List.of("value1", "value2", "value3");
-        testSubject.describeProperty("collection", collection);
+    @Nested
+    class CollectionTests {
 
-        // when
-        var result = testSubject.describe();
-        var expected = normalizeJson("""
-                                             {
-                                               "collection" : [ "value1", "value2", "value3" ]
-                                             }
-                                             """);
+        @Test
+        void describeCollection() {
+            // given
+            var collection = List.of("value1", "value2", "value3");
+            testSubject.describeProperty("collection", collection);
 
-        // then
-        assertEquals(expected, normalizeJson(result));
+            // when
+            var result = testSubject.describe();
+            var expected = normalizeJson("""
+                                                 {
+                                                   "collection" : [ "value1", "value2", "value3" ]
+                                                 }
+                                                 """);
+
+            // then
+            assertEquals(expected, normalizeJson(result));
+        }
+
+        @Test
+        void describeEmptyCollection() {
+            // given
+            var emptyCollection = List.<String>of();
+            testSubject.describeProperty("emptyCollection", emptyCollection);
+
+            // when
+            var result = testSubject.describe();
+            var expected = normalizeJson("""
+                                                 {
+                                                   "emptyCollection" : [ ]
+                                                 }
+                                                 """);
+
+            // then
+            assertEquals(expected, normalizeJson(result));
+        }
+
+        @Test
+        void describeCollectionOfDescribableComponentsShouldIncludeTypeAndId() {
+            // given
+            var components = List.of(
+                    new SimpleTestComponent("component1", 101),
+                    new SimpleTestComponent("component2", 102)
+            );
+            testSubject.describeProperty("components", components);
+
+            // when
+            var result = testSubject.describe();
+
+            // then
+            assertJsonMatchesPattern(result, """
+                    {
+                      "components" : [ {
+                        "name" : "component1",
+                        "value" : 101,
+                        "_type" : "SimpleTestComponent",
+                        "_id" : "*"
+                      }, {
+                        "name" : "component2",
+                        "value" : 102,
+                        "_type" : "SimpleTestComponent",
+                        "_id" : "*"
+                      } ]
+                    }
+                    """);
+        }
     }
 
-    @Test
-    void describeEmptyCollection() {
-        // given
-        var emptyCollection = List.<String>of();
-        testSubject.describeProperty("emptyCollection", emptyCollection);
+    @Nested
+    class MapTests {
 
-        // when
-        var result = testSubject.describe();
-        var expected = normalizeJson("""
-                                             {
-                                               "emptyCollection" : [ ]
-                                             }
-                                             """);
+        @Test
+        void describeMap() {
+            // given
+            var map = Map.of(
+                    "key1", "value1",
+                    "key2", "value2"
+            );
+            testSubject.describeProperty("map", map);
 
-        // then
-        assertEquals(expected, normalizeJson(result));
-    }
+            // when
+            var result = testSubject.describe();
 
-    @Test
-    void describeCollectionOfDescribableComponents() {
-        // given
-        var components = List.of(
-                new SimpleTestComponent("component1", 101),
-                new SimpleTestComponent("component2", 102)
-        );
-        testSubject.describeProperty("components", components);
-
-        // when
-        var result = testSubject.describe();
-        var expected = normalizeJson("""
-                                             {
-                                               "components" : [ {
-                                                 "name" : "component1",
-                                                 "value" : 101
-                                               }, {
-                                                 "name" : "component2",
-                                                 "value" : 102
-                                               } ]
-                                             }
-                                             """);
-
-        // then
-        assertEquals(expected, normalizeJson(result));
-    }
-
-    @Test
-    void describeMap() {
-        // given
-        var map = Map.of(
-                "key1", "value1",
-                "key2", "value2"
-        );
-        testSubject.describeProperty("map", map);
-
-        // when
-        var result = testSubject.describe();
-
-        // then
-        // since maps don't guarantee order in JSON representation, we need to check both possibilities
-        var expected1 = normalizeJson("""
-                                              {
-                                                "map" : {
-                                                  "key1" : "value1",
-                                                  "key2" : "value2"
-                                                }
-                                              }
-                                              """);
-        var expected2 = normalizeJson("""
-                                              {
-                                                "map" : {
-                                                  "key2" : "value2",
-                                                  "key1" : "value1"
-                                                }
-                                              }
-                                              """);
-        var normalizedResult = normalizeJson(result);
-        assertTrue(
-                normalizedResult.equals(expected1) || normalizedResult.equals(expected2),
-                "Expected one of two possible formats but got: " + result
-        );
-    }
-
-    @Test
-    void describeEmptyMap() {
-        // given
-        var emptyMap = Map.<String, String>of();
-        testSubject.describeProperty("emptyMap", emptyMap);
-
-        // when
-        var result = testSubject.describe();
-        var expected = normalizeJson("""
-                                             {
-                                               "emptyMap" : { }
-                                             }
-                                             """);
-
-        // then
-        assertEquals(expected, normalizeJson(result));
-    }
-
-    @Test
-    void describeMapWithDescribableComponents() {
-        // given
-        var map = Map.of(
-                "component1", new SimpleTestComponent("value1", 201),
-                "component2", new SimpleTestComponent("value2", 202)
-        );
-        testSubject.describeProperty("componentMap", map);
-
-        // when
-        var result = testSubject.describe();
-
-        // then
-        // since maps don't guarantee order in JSON representation, we need to check both possibilities
-        var expected1 = normalizeJson("""
-                                              {
-                                                "componentMap" : {
-                                                  "component1" : {
-                                                    "name" : "value1",
-                                                    "value" : 201
-                                                  },
-                                                  "component2" : {
-                                                    "name" : "value2",
-                                                    "value" : 202
+            // then
+            // since maps don't guarantee order in JSON representation, we need to check both possibilities
+            var expected1 = normalizeJson("""
+                                                  {
+                                                    "map" : {
+                                                      "key1" : "value1",
+                                                      "key2" : "value2"
+                                                    }
                                                   }
-                                                }
-                                              }
-                                              """);
-        var expected2 = normalizeJson("""
-                                              {
-                                                "componentMap" : {
-                                                  "component2" : {
-                                                    "name" : "value2",
-                                                    "value" : 202
-                                                  },
-                                                  "component1" : {
-                                                    "name" : "value1",
-                                                    "value" : 201
+                                                  """);
+            var expected2 = normalizeJson("""
+                                                  {
+                                                    "map" : {
+                                                      "key2" : "value2",
+                                                      "key1" : "value1"
+                                                    }
                                                   }
-                                                }
-                                              }
-                                              """);
-        var normalizedResult = normalizeJson(result);
-        assertTrue(
-                normalizedResult.equals(expected1) || normalizedResult.equals(expected2),
-                "Expected one of two possible formats but got: " + result
-        );
+                                                  """);
+            var normalizedResult = normalizeJson(result);
+            assertTrue(
+                    normalizedResult.equals(expected1) || normalizedResult.equals(expected2),
+                    "Expected one of two possible formats but got: " + result
+            );
+        }
+
+        @Test
+        void describeEmptyMap() {
+            // given
+            var emptyMap = Map.<String, String>of();
+            testSubject.describeProperty("emptyMap", emptyMap);
+
+            // when
+            var result = testSubject.describe();
+            var expected = normalizeJson("""
+                                                 {
+                                                   "emptyMap" : { }
+                                                 }
+                                                 """);
+
+            // then
+            assertEquals(expected, normalizeJson(result));
+        }
+
+        @Test
+        void describeMapWithDescribableComponentsShouldIncludeTypeAndId() {
+            // given
+            var map = Map.of(
+                    "component1", new SimpleTestComponent("value1", 201)
+            );
+            testSubject.describeProperty("componentMap", map);
+
+            // when
+            var result = testSubject.describe();
+
+            // then
+            assertJsonMatchesPattern(
+                    result,
+                    """
+                                {
+                                  "componentMap": {
+                                    "component1":{"name":"value1","value":201,"_type":"SimpleTestComponent","_id":"*"}
+                                  }
+                                }
+                            """
+            );
+        }
     }
 
     @Test
-    void describeComplexStructure() {
+    void describeComplexStructureShouldIncludeTypeAndId() {
         // given
         record ComplexTestComponent(
                 String stringValue,
@@ -316,36 +303,50 @@ class JacksonComponentDescriptorTest {
         var result = testSubject.describe();
 
         // then
-        var expected = normalizeJson("""
-                                             {
-                                               "complexStructure" : {
-                                                 "stringValue" : "test",
-                                                 "numberValue" : 42,
-                                                 "booleanValue" : true,
-                                                 "component" : {
-                                                   "name" : "nestedComponent",
-                                                   "value" : 300
-                                                 },
-                                                 "simpleList" : [ "listItem1", "listItem2" ],
-                                                 "componentList" : [ {
-                                                   "name" : "listComponent1",
-                                                   "value" : 301
-                                                 }, {
-                                                   "name" : "listComponent2",
-                                                   "value" : 302
-                                                 } ],
-                                                 "simpleMap" : {
-                                                   "mapKey1" : "mapValue1"
-                                                 }
-                                               }
-                                             }
-                                             """);
-
-        assertEquals(expected, normalizeJson(result));
+        assertJsonMatchesPattern(
+                result,
+                """
+                        {
+                            "complexStructure":{
+                                "stringValue":"test",
+                                "numberValue":42,
+                                "booleanValue":true,
+                                "component":{
+                                    "name":"nestedComponent",
+                                    "value":300,
+                                    "_type":"SimpleTestComponent",
+                                    "_id":"*"
+                                },
+                                "simpleList":["listItem1","listItem2"],
+                                "componentList":[
+                                    {"name":"listComponent1","value":301,"_type":"SimpleTestComponent","_id":"*"},
+                                    {"name":"listComponent2","value":302,"_type":"SimpleTestComponent","_id":"*"}
+                                ],
+                                "simpleMap":{"mapKey1":"mapValue1"},
+                                "_type":"ComplexTestComponent",
+                                "_id":"*"
+                            }
+                        }
+                        """
+        );
     }
 
+    /**
+     * Utility method to normalize JSON strings for comparison.
+     */
     private String normalizeJson(String json) {
         return json.replaceAll("\\s+", "");
+    }
+
+    /**
+     * Utility method to assert that a JSON string matches a pattern, with dynamic IDs replaced for comparison.
+     */
+    private void assertJsonMatchesPattern(String actual, String expectedPattern) {
+        String normalizedActual = normalizeJson(actual).replaceAll("\"_id\":\"\\d+\"", "\"_id\":\"*\"");
+        String normalizedExpected = normalizeJson(expectedPattern);
+
+        assertEquals(normalizedExpected, normalizedActual,
+                     "JSON does not match expected pattern");
     }
 
     private record SimpleTestComponent(String name, int value) implements DescribableComponent {
