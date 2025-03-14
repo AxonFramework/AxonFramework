57f0168e185224ad0ffe8b784236122fd295e982
Mateusz Nowak <mateusz@nakodach.pl>
2025-03-14 11:25:37 +0100

    [#3337] feat: JacksonComponentDescriptor - refactor the implementation

--- messaging/src/main/java/org/axonframework/common/infra/JacksonComponentDescriptor.java
+++ messaging/src/main/java/org/axonframework/common/infra/JacksonComponentDescriptor.java
@@ -20,7 +20,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.annotation.Nonnull;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.Map;
