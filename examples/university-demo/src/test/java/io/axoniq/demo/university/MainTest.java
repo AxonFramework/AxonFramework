63b20c92e57110e843f513fb156451c52d879457
Mateusz Nowak <mateusz@nakodach.pl>
2025-03-20 14:49:20 +0100

    📦 chore(deps): configure `JUnit5` with Maven surefire plugin

---
+++ examples/university-demo/src/test/java/io/axoniq/demo/university/MainTest.java
@@ -0,0 +1,13 @@
+package io.axoniq.demo.university;
+
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void test() {
+        assertThat(true).isTrue();
+    }
+}
