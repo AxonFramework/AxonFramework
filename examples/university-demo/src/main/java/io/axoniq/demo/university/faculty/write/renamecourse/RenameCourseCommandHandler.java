9007e1fac2155b726bb648722c1604fc95ba07c1
Mateusz Nowak <mateusz@nakodach.pl>
2025-03-20 16:31:40 +0100

    ✨ feat: Faculty | add `CourseIdResolver` for `RenameCourse` command handler

--- examples/university-demo/src/main/java/io/axoniq/demo/university/faculty/write/renamecourse/RenameCourseCommandHandler.java
+++ examples/university-demo/src/main/java/io/axoniq/demo/university/faculty/write/renamecourse/RenameCourseCommandHandler.java
@@ -3,13 +3,16 @@ package io.axoniq.demo.university.faculty.write.renamecourse;
 import io.axoniq.demo.university.faculty.BoundedContext;
 import io.axoniq.demo.university.faculty.events.CourseCreated;
 import io.axoniq.demo.university.faculty.events.CourseRenamed;
+import jakarta.annotation.Nonnull;
 import org.axonframework.commandhandling.annotation.CommandHandler;
 import org.axonframework.eventhandling.EventMessage;
 import org.axonframework.eventhandling.EventSink;
 import org.axonframework.eventhandling.GenericEventMessage;
 import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.messaging.Message;
 import org.axonframework.messaging.MessageType;
 import org.axonframework.messaging.unitofwork.ProcessingContext;
+import org.axonframework.modelling.command.EntityIdResolver;
 import org.axonframework.modelling.command.annotation.InjectEntity;
 
 import java.util.List;
@@ -65,6 +68,26 @@ class RenameCourseCommandHandler {
         @EventSourcingHandler
         public void evolve(CourseRenamed event) {
             this.name = event.newName();
+        }
+    }
+
+    public static class CourseIdResolver implements EntityIdResolver<String> {
+
+        @Nonnull
+        @Override
+        public String resolve(@Nonnull Message<?> message, @Nonnull ProcessingContext context) {
+            var id = resolveOrNull(message);
+            if (id == null) {
+                throw new IllegalArgumentException("Cannot resolve course id from the command");
+            }
+            return id;
+        }
+
+        private static String resolveOrNull(Message<?> message) {
+            var payload = message.getPayload();
+            return payload instanceof RenameCourse renameCourse
+                    ? renameCourse.courseId()
+                    : null;
         }
     }
 }
