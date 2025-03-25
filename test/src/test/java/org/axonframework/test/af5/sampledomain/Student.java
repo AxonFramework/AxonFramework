d93c23ed0ed014245720779cf587d1d7062abfa1
Mateusz Nowak <mateusz@nakodach.pl>
2025-03-25 11:55:12 +0100

    [#3073] feat: Command Model Test Fixture - add StatefulCommandHandler test

---
+++ test/src/test/java/org/axonframework/test/af5/sampledomain/Student.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.test.af5.sampledomain;
+
+import org.axonframework.eventsourcing.EventSourcingHandler;
+
+/**
+ * Event-sourced Student model
+ */
+public class Student {
+
+    private String id;
+    private String name;
+
+    public Student(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @EventSourcingHandler
+    public void on(StudentNameChangedEvent event) {
+        this.name = event.name();
+    }
+}
