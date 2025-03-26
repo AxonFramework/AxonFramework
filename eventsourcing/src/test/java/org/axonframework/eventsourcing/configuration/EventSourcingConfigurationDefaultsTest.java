69aa8306cc9be1509fdd22d03518901a8bd11c60
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-03-26 11:44:05 +0100

    Add the EventSourcingConfigurer for the event sourcing module
    Add the EventSourcingConfigurer for the event sourcing module. This
    configurer sets a default TagResolver, EventStorageEngine, EventStore,
    and Snapshotter, to support event sourcing in an Axon application.
    
    #3345

---
+++ eventsourcing/src/test/java/org/axonframework/eventsourcing/configuration/EventSourcingConfigurationDefaultsTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.eventsourcing.configuration;
+
+import org.axonframework.configuration.ApplicationConfigurer;
+import org.axonframework.configuration.AxonApplication;
+import org.axonframework.configuration.NewConfiguration;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventsourcing.Snapshotter;
+import org.axonframework.eventsourcing.eventstore.AnnotationBasedTagResolver;
+import org.axonframework.eventsourcing.eventstore.AsyncEventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.AsyncEventStore;
+import org.axonframework.eventsourcing.eventstore.SimpleEventStore;
+import org.axonframework.eventsourcing.eventstore.Tag;
+import org.axonframework.eventsourcing.eventstore.TagResolver;
+import org.axonframework.eventsourcing.eventstore.inmemory.AsyncInMemoryEventStorageEngine;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.*;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class validating the {@link EventSourcingConfigurationDefaults}.
+ *
+ * @author Steven van Beelen
+ */
+class EventSourcingConfigurationDefaultsTest {
+
+    private EventSourcingConfigurationDefaults testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testSubject = new EventSourcingConfigurationDefaults();
+    }
+
+    @Test
+    void orderEqualsMaxInteger() {
+        assertEquals(Integer.MAX_VALUE, testSubject.order());
+    }
+
+    @Test
+    void enhanceSetsExpectedDefaultsInAbsenceOfTheseComponents() {
+        ApplicationConfigurer<?> configurer = AxonApplication.create();
+        testSubject.enhance(configurer);
+        NewConfiguration resultConfig = configurer.build();
+
+        assertInstanceOf(AnnotationBasedTagResolver.class, resultConfig.getComponent(TagResolver.class));
+        assertInstanceOf(AsyncInMemoryEventStorageEngine.class,
+                         resultConfig.getComponent(AsyncEventStorageEngine.class));
+        assertInstanceOf(SimpleEventStore.class, resultConfig.getComponent(AsyncEventStore.class));
+        assertInstanceOf(Snapshotter.class, resultConfig.getComponent(Snapshotter.class));
+    }
+
+    @Test
+    void enhanceOnlySetsDefaultsThatAreNotPresentYet() {
+        TestTagResolver testTagResolver = new TestTagResolver();
+
+        ApplicationConfigurer<?> configurer =
+                AxonApplication.create()
+                               .registerComponent(TagResolver.class, c -> testTagResolver);
+
+        testSubject.enhance(configurer);
+
+        TagResolver configuredTagResolver = configurer.build()
+                                                      .getComponent(TagResolver.class);
+
+        assertEquals(testTagResolver, configuredTagResolver);
+    }
+
+    private static class TestTagResolver implements TagResolver {
+
+        @Override
+        public Set<Tag> resolve(@NotNull EventMessage<?> event) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
---
+++ eventsourcing/src/test/java/org/axonframework/eventsourcing/configuration/EventSourcingConfigurerTest.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.eventsourcing.configuration;
+
+import org.axonframework.configuration.ConfigurerTestSuite;
+import org.axonframework.configuration.NewConfiguration;
+import org.axonframework.eventsourcing.Snapshotter;
+import org.axonframework.eventsourcing.eventstore.AnnotationBasedTagResolver;
+import org.axonframework.eventsourcing.eventstore.AsyncEventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.AsyncEventStore;
+import org.axonframework.eventsourcing.eventstore.PayloadBasedTagResolver;
+import org.axonframework.eventsourcing.eventstore.SimpleEventStore;
+import org.axonframework.eventsourcing.eventstore.TagResolver;
+import org.axonframework.eventsourcing.eventstore.inmemory.AsyncInMemoryEventStorageEngine;
+import org.axonframework.modelling.configuration.ModellingConfigurer;
+import org.jetbrains.annotations.Nullable;
+import org.junit.jupiter.api.*;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class validating the {@link EventSourcingConfigurer}.
+ *
+ * @author Steven van Beelen
+ */
+class EventSourcingConfigurerTest extends ConfigurerTestSuite<EventSourcingConfigurer> {
+
+    @Override
+    public EventSourcingConfigurer testSubject() {
+        return testSubject == null ? EventSourcingConfigurer.create() : testSubject;
+    }
+
+    @Override
+    public NewConfiguration build() {
+        return testSubject().build();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public @Nullable Class<ModellingConfigurer> delegateType() {
+        return ModellingConfigurer.class;
+    }
+
+    @Test
+    void defaultComponents() {
+        NewConfiguration result = testSubject.build();
+
+        Optional<TagResolver> tagResolver = result.getOptionalComponent(TagResolver.class);
+        assertTrue(tagResolver.isPresent());
+        assertInstanceOf(AnnotationBasedTagResolver.class, tagResolver.get());
+
+        Optional<AsyncEventStorageEngine> eventStorageEngine =
+                result.getOptionalComponent(AsyncEventStorageEngine.class);
+        assertTrue(eventStorageEngine.isPresent());
+        assertInstanceOf(AsyncInMemoryEventStorageEngine.class, eventStorageEngine.get());
+
+        Optional<AsyncEventStore> eventStore = result.getOptionalComponent(AsyncEventStore.class);
+        assertTrue(eventStore.isPresent());
+        assertInstanceOf(SimpleEventStore.class, eventStore.get());
+
+        Optional<Snapshotter> snapshotter = result.getOptionalComponent(Snapshotter.class);
+        assertTrue(snapshotter.isPresent());
+    }
+
+    @Test
+    void registerTagResolverOverridesDefault() {
+        TagResolver expected = PayloadBasedTagResolver.forPayloadType(String.class);
+
+        NewConfiguration result = testSubject.registerTagResolver(c -> expected)
+                                             .build();
+
+        assertEquals(expected, result.getComponent(TagResolver.class));
+    }
+
+    @Test
+    void registerEventStorageEngineOverridesDefault() {
+        AsyncEventStorageEngine expected = new AsyncInMemoryEventStorageEngine();
+
+        NewConfiguration result = testSubject.registerEventStorageEngine(c -> expected)
+                                             .build();
+
+        assertEquals(expected, result.getComponent(AsyncEventStorageEngine.class));
+    }
+
+    @Test
+    void registerEventStoreOverridesDefault() {
+        AsyncEventStore expected = new SimpleEventStore(null, null);
+
+        NewConfiguration result = testSubject.registerEventStore(c -> expected)
+                                             .build();
+
+        assertEquals(expected, result.getComponent(AsyncEventStore.class));
+    }
+
+    @Test
+    void registerSnapshotterOverridesDefault() {
+        Snapshotter expected = (aggregateType, aggregateIdentifier) -> {
+
+        };
+
+        NewConfiguration result = testSubject.registerSnapshotter(c -> expected)
+                                             .build();
+
+        assertEquals(expected, result.getComponent(Snapshotter.class));
+    }
+
+    @Test
+    void modellingDelegatesTasks() {
+        TestComponent result =
+                testSubject.modelling(
+                                   modelling -> modelling.registerComponent(TestComponent.class, c -> TEST_COMPONENT)
+                           )
+                           .build()
+                           .getComponent(TestComponent.class);
+
+        assertEquals(TEST_COMPONENT, result);
+    }
+
+    @Test
+    void messagingDelegatesTasks() {
+        TestComponent result =
+                testSubject.messaging(
+                                   messaging -> messaging.registerComponent(TestComponent.class, c -> TEST_COMPONENT)
+                           )
+                           .build()
+                           .getComponent(TestComponent.class);
+
+        assertEquals(TEST_COMPONENT, result);
+    }
+
+    @Test
+    void applicationDelegatesTasks() {
+        TestComponent result =
+                testSubject.application(axon -> axon.registerComponent(TestComponent.class, c -> TEST_COMPONENT))
+                           .build()
+                           .getComponent(TestComponent.class);
+
+        assertEquals(TEST_COMPONENT, result);
+    }
+}
