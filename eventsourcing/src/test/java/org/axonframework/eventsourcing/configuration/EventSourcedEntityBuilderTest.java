d632b9e8ad73fdfcf6e975ae4e158d0bbd597bb6
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-03-28 13:05:59 +0100

    Add unit tests for the EventSourcedEntityBuilder
    Add unit tests for the EventSourcedEntityBuilder
    
    #3345

---
+++ eventsourcing/src/test/java/org/axonframework/eventsourcing/configuration/EventSourcedEntityBuilderTest.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.eventsourcing.configuration;
+
+import org.axonframework.eventsourcing.AsyncEventSourcingRepository;
+import org.axonframework.eventsourcing.CriteriaResolver;
+import org.axonframework.eventsourcing.EventStateApplier;
+import org.axonframework.eventsourcing.eventstore.EventCriteria;
+import org.axonframework.modelling.repository.AsyncRepository;
+import org.junit.jupiter.api.*;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class validating the {@link EventSourcedEntityBuilder}.
+ *
+ * @author Steven van Beelen
+ */
+class EventSourcedEntityBuilderTest {
+
+    private Function<CourseId, Course> testEntityFactory;
+    private CriteriaResolver<CourseId> testCriteriaResolver;
+    private EventStateApplier<Course> testEventStateApplier;
+    private AtomicBoolean constructedEntityFactory;
+    private AtomicBoolean constructedCriteriaResolver;
+    private AtomicBoolean constructedEventStateApplier;
+
+    private EventSourcedEntityBuilder<CourseId, Course> testSubject;
+
+    @BeforeEach
+    void setUp() {
+        testEntityFactory = Course::new;
+        testCriteriaResolver = event -> EventCriteria.anyEvent();
+        testEventStateApplier = (model, event, processingContext) -> model;
+        constructedEntityFactory = new AtomicBoolean(false);
+        constructedCriteriaResolver = new AtomicBoolean(false);
+        constructedEventStateApplier = new AtomicBoolean(false);
+
+        testSubject = EventSourcedEntityBuilder.entity(CourseId.class, Course.class)
+                                               .entityFactory(c -> {
+                                                   constructedEntityFactory.set(true);
+                                                   return testEntityFactory;
+                                               })
+                                               .criteriaResolver(c -> {
+                                                   constructedCriteriaResolver.set(true);
+                                                   return testCriteriaResolver;
+                                               })
+                                               .eventStateApplier(c -> {
+                                                   constructedEventStateApplier.set(true);
+                                                   return testEventStateApplier;
+                                               });
+    }
+
+    @Test
+    void entityThrowsNullPointerExceptionForNullIdentifierType() {
+        //noinspection DataFlowIssue
+        assertThrows(NullPointerException.class, () -> EventSourcedEntityBuilder.entity(null, Course.class));
+    }
+
+    @Test
+    void entityThrowsNullPointerExceptionForNullEntityType() {
+        //noinspection DataFlowIssue
+        assertThrows(NullPointerException.class, () -> EventSourcedEntityBuilder.entity(CourseId.class, null));
+    }
+
+    @Test
+    void entityFactoryThrowsNullPointerExceptionForNullEntityFactory() {
+        //noinspection DataFlowIssue
+        assertThrows(NullPointerException.class,
+                     () -> EventSourcedEntityBuilder.entity(CourseId.class, Course.class)
+                                                    .entityFactory(null));
+    }
+
+    @Test
+    void criteriaResolverThrowsNullPointerExceptionForNullCriteriaResolver() {
+        //noinspection DataFlowIssue
+        assertThrows(NullPointerException.class,
+                     () -> EventSourcedEntityBuilder.entity(CourseId.class, Course.class)
+                                                    .entityFactory(c -> testEntityFactory)
+                                                    .criteriaResolver(null));
+    }
+
+    @Test
+    void eventStateApplierThrowsNullPointerExceptionForNullEventStateApplier() {
+        //noinspection DataFlowIssue
+        assertThrows(NullPointerException.class,
+                     () -> EventSourcedEntityBuilder.entity(CourseId.class, Course.class)
+                                                    .entityFactory(c -> testEntityFactory)
+                                                    .criteriaResolver(c -> testCriteriaResolver)
+                                                    .eventStateApplier(null));
+    }
+
+    @Test
+    void entityNameCombinesIdentifierAndEntityTypeNames() {
+        String expectedEntityName = "Course#CourseId";
+
+        assertEquals(expectedEntityName, testSubject.entityName());
+    }
+
+    @Test
+    void repositoryConstructsEventSourcingRepositoryForEntityFactoryCriteriaResolverAndEventStateApplier() {
+        AsyncRepository<CourseId, Course> result = testSubject.repository()
+                                                              .build(EventSourcingConfigurer.create().build());
+
+        assertInstanceOf(AsyncEventSourcingRepository.class, result);
+        assertTrue(constructedEntityFactory.get());
+        assertTrue(constructedCriteriaResolver.get());
+        assertTrue(constructedEventStateApplier.get());
+    }
+
+    record CourseId() {
+
+    }
+
+    record Course(CourseId id) {
+
+    }
+}
