44c5dcbc2e354c0fa5aabc7936f97b58c4e8a4f1
Mateusz Nowak <mateusz@nakodach.pl>
2025-04-02 11:32:04 +0200

    ✨ feat: Faculty | `CreateCourseCommandHandler` - implements StatefulCommandHandler

--- examples/university-demo-OLD/src/main/java/io/axoniq/demo/university/faculty/FacultyModuleConfiguration.java
+++ examples/university-demo-OLD/src/main/java/io/axoniq/demo/university/faculty/FacultyModuleConfiguration.java
@@ -1,11 +1,13 @@
 package io.axoniq.demo.university.faculty;
 
+import io.axoniq.demo.university.faculty.write.createcourse.CreateCourseConfiguration;
 import io.axoniq.demo.university.faculty.write.renamecourse.RenameCourseConfiguration;
 import org.axonframework.eventsourcing.configuration.EventSourcingConfigurer;
 
 public class FacultyModuleConfiguration {
 
     public static EventSourcingConfigurer configure(EventSourcingConfigurer configurer) {
+        configurer = CreateCourseConfiguration.configure(configurer);
         configurer = RenameCourseConfiguration.configure(configurer);
         return configurer;
     }
