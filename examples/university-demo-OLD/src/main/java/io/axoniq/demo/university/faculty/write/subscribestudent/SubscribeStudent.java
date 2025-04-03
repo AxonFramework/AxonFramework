700a9c7e6950dc9743228ee724a3c458982a7d72
Mateusz Nowak <mateusz@nakodach.pl>
2025-04-03 13:41:14 +0200

    ✨ feat: Faculty | add `SubscribeStudent` write slice

--- examples/university-demo-OLD/src/main/java/io/axoniq/demo/university/faculty/write/subscribestudent/SubscribeStudent.java
+++ examples/university-demo-OLD/src/main/java/io/axoniq/demo/university/faculty/write/subscribestudent/SubscribeStudent.java
@@ -2,7 +2,12 @@ package io.axoniq.demo.university.faculty.write.subscribestudent;
 
 import io.axoniq.demo.university.faculty.write.CourseId;
 import io.axoniq.demo.university.faculty.write.StudentId;
+import org.axonframework.modelling.command.annotation.TargetEntityId;
 
 public record SubscribeStudent(StudentId studentId, CourseId courseId) {
 
+    @TargetEntityId
+    public SubscriptionId subscriptionId() {
+        return new SubscriptionId(courseId, studentId);
+    }
 }
