27c261c50d986e7fb544d735ff8dbb2f931064bf
Mateusz Nowak <mateusz@nakodach.pl>
2025-04-16 00:46:41 +0200

    ♻️ refactor: `Faculty` | define private constructors to prevent instantiation of classes with just static methods

--- examples/university-demo/src/main/java/io/axoniq/demo/university/faculty/write/createcourseplain/CreateCoursePlainConfiguration.java
+++ examples/university-demo/src/main/java/io/axoniq/demo/university/faculty/write/createcourseplain/CreateCoursePlainConfiguration.java
@@ -34,4 +34,8 @@ public class CreateCoursePlainConfiguration {
         return configurer.registerStatefulCommandHandlingModule(commandHandlingModule);
     }
 
+    private CreateCoursePlainConfiguration() {
+        // Prevent instantiation
+    }
+
 }
