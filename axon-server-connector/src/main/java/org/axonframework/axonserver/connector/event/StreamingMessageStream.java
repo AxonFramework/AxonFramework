729ab865b2d447cc4eb3c156aed47bc689d17782
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-05-01 11:08:19 +0200

    Introduce a StreamingMessageStream. This will be used when streaming..
    Introduce a StreamingMessageStream. This will be used when streaming
    events from the AxonServerEventStorageEngine.
    
    #3239

---
+++ axon-server-connector/src/main/java/org/axonframework/axonserver/connector/event/StreamingMessageStream.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.axonserver.connector.event;
+
+import io.axoniq.axonserver.connector.ResultStream;
+import io.axoniq.axonserver.grpc.event.dcb.SequencedEvent;
+import io.axoniq.axonserver.grpc.event.dcb.StreamEventsResponse;
+import jakarta.annotation.Nonnull;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.GlobalSequenceTrackingToken;
+import org.axonframework.eventhandling.TrackingToken;
+import org.axonframework.messaging.Context;
+import org.axonframework.messaging.MessageStream;
+import org.axonframework.messaging.SimpleEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * A {@link MessageStream} implementation backed by a {@link ResultStream} of
+ * {@link StreamEventsResponse StreamEventsResponses} from Axon Server, translating the {@code StreamEventsResponses}
+ * into {@link EventMessage EventMessages} as it moves along.
+ * <p>
+ * Note that Axon Server regards the {@code ResultStream} as infinite. As such, even if {@link #next()} returns an empty
+ * {@link Optional}, the stream will remain open.
+ *
+ * @author Steven van Beelen
+ * @since 5.0.0
+ */
+class StreamingMessageStream implements MessageStream<EventMessage<?>> {
+
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final ResultStream<StreamEventsResponse> stream;
+    private final EventConverter converter;
+
+    /**
+     * Constructs a {@code StreamingMessageStream} with the given {@code stream} and {@code converter}.
+     *
+     * @param stream    The {@code ResultStream} of {@code StreamEventsResponses} to convert into
+     *                  {@link EventMessage EventMessages} for this {@link MessageStream} implementation.
+     * @param converter The {@code EventConverter} used to convert {@code StreamEventsResponses} into
+     *                  {@link EventMessage EventMessages} for this {@link MessageStream} implementation.
+     */
+    StreamingMessageStream(@Nonnull ResultStream<StreamEventsResponse> stream,
+                           @Nonnull EventConverter converter) {
+        this.stream = Objects.requireNonNull(stream, "The result stream cannot be null.");
+        this.converter = Objects.requireNonNull(converter, "The converter cannot be null.");
+    }
+
+    @Override
+    public Optional<Entry<EventMessage<?>>> next() {
+        StreamEventsResponse response = stream.nextIfAvailable();
+        if (response == null) {
+            logger.debug("There are no more events to stream at this moment in time.");
+            return Optional.empty();
+        }
+        return Optional.of(convertToEntry(response.getEvent()));
+    }
+
+    private SimpleEntry<EventMessage<?>> convertToEntry(SequencedEvent event) {
+        EventMessage<byte[]> eventMessage = converter.convertEvent(event.getEvent());
+        TrackingToken token = new GlobalSequenceTrackingToken(event.getSequence());
+        Context context = Context.with(TrackingToken.RESOURCE_KEY, token);
+        return new SimpleEntry<>(eventMessage, context);
+    }
+
+    @Override
+    public void onAvailable(@Nonnull Runnable callback) {
+        stream.onAvailable(callback);
+    }
+
+    @Override
+    public Optional<Throwable> error() {
+        return stream.getError();
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return stream.isClosed();
+    }
+
+    @Override
+    public boolean hasNextAvailable() {
+        return stream.peek() != null;
+    }
+
+    @Override
+    public void close() {
+        stream.close();
+    }
+}
