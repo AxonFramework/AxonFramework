3144cd4a0119ae6b164f19d54a30707e4a871084
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-05-09 12:20:00 +0200

    Ensure tests can deal with async responses
    Ensure tests can deal with async responses. As we do a quick commit one
    after the other, there's a window of opportunity that Axon Server
    switches around the order. This would also apply to other storage
    engines. Hence, the validation should take this into account.
    
    #3239

--- eventsourcing/src/test/java/org/axonframework/eventsourcing/eventstore/StorageEngineTestSuite.java
+++ eventsourcing/src/test/java/org/axonframework/eventsourcing/eventstore/StorageEngineTestSuite.java
@@ -227,6 +227,9 @@ public abstract class StorageEngineTestSuite<ESE extends EventStorageEngine> {
 
     @Test
     void concurrentTransactionsForNonOverlappingTagsBothCommitWithExpectedConsistencyMarkerResponse() throws Exception {
+        // given...
+        Set<ConsistencyMarker> expected =
+                Set.of(new GlobalIndexConsistencyMarker(1), new GlobalIndexConsistencyMarker(2));
         AppendCondition firstCondition = new DefaultAppendCondition(ConsistencyMarker.ORIGIN, TEST_CRITERIA);
         AppendCondition secondCondition = new DefaultAppendCondition(ConsistencyMarker.ORIGIN, OTHER_CRITERIA);
 
@@ -236,43 +239,49 @@ public abstract class StorageEngineTestSuite<ESE extends EventStorageEngine> {
         AppendTransaction secondTx =
                 testSubject.appendEvents(secondCondition, taggedEventMessage("event-0", OTHER_CRITERIA_TAGS))
                            .get(1, TimeUnit.SECONDS);
-
+        // when...
         CompletableFuture<ConsistencyMarker> firstCommit = firstTx.commit();
         CompletableFuture<ConsistencyMarker> secondCommit = secondTx.commit();
-
-        assertDoesNotThrow(() -> firstCommit.get(1, TimeUnit.SECONDS));
-        assertDoesNotThrow(() -> secondCommit.get(1, TimeUnit.SECONDS));
-
-        ConsistencyMarker firstMarker = firstCommit.get(5, TimeUnit.SECONDS);
-        ConsistencyMarker secondMarker = secondCommit.get(5, TimeUnit.SECONDS);
+        // then...expecting an unordered set of markers, as the commit order is not consistent for an async system.
+        assertDoesNotThrow(() -> firstCommit.get(5, TimeUnit.SECONDS));
+        assertDoesNotThrow(() -> secondCommit.get(5, TimeUnit.SECONDS));
+        ConsistencyMarker firstMarker = firstCommit.get(50, TimeUnit.MILLISECONDS);
+        ConsistencyMarker secondMarker = secondCommit.get(50, TimeUnit.MILLISECONDS);
         assertNotNull(firstMarker);
         assertNotNull(secondMarker);
-        assertEquals(GlobalIndexConsistencyMarker.position(firstMarker) + 1,
-                     GlobalIndexConsistencyMarker.position(secondMarker));
+        Set<ConsistencyMarker> result = Set.of(firstMarker, secondMarker);
+        assertEquals(expected, result);
     }
 
     @Test
     void concurrentTransactionsForOverlappingTagsThrowAnAppendEventsTransactionRejectedException() throws Exception {
         // given...
+        int exceptionCounter = 0;
         AppendCondition firstCondition = new DefaultAppendCondition(ConsistencyMarker.ORIGIN, TEST_CRITERIA);
-        AppendCondition secondCondition = new DefaultAppendCondition(ConsistencyMarker.ORIGIN, OTHER_CRITERIA);
+        AppendCondition secondCondition = new DefaultAppendCondition(ConsistencyMarker.ORIGIN, TEST_CRITERIA);
         AppendTransaction firstTx =
                 testSubject.appendEvents(firstCondition, taggedEventMessage("event-0", TEST_CRITERIA_TAGS))
                            .get(1, TimeUnit.SECONDS);
         AppendTransaction secondTx =
-                testSubject.appendEvents(secondCondition, taggedEventMessage("event-0", TEST_CRITERIA_TAGS))
+                testSubject.appendEvents(secondCondition, taggedEventMessage("event-1", TEST_CRITERIA_TAGS))
                            .get(1, TimeUnit.SECONDS);
         // when...
         CompletableFuture<ConsistencyMarker> firstCommit = firstTx.commit();
         CompletableFuture<ConsistencyMarker> secondCommit = secondTx.commit();
-        // then...
-        // First commit is slightly faster, so should succeed
-        assertDoesNotThrow(() -> firstCommit.get(1, TimeUnit.SECONDS));
-        await("Await exceptional completion of second commit")
-                .atMost(Duration.ofMillis(500))
-                .pollDelay(Duration.ofMillis(50))
-                .until(secondCommit::isCompletedExceptionally);
-        assertInstanceOf(AppendEventsTransactionRejectedException.class, secondCommit.exceptionNow().getClass());
+        // then... try-catch for both. One should fail, but which differs, because it's an async process.
+        try {
+            firstCommit.get(5, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            exceptionCounter++;
+            assertInstanceOf(AppendEventsTransactionRejectedException.class, e.getCause());
+        }
+        try {
+            secondCommit.get(5, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            exceptionCounter++;
+            assertInstanceOf(AppendEventsTransactionRejectedException.class, e.getCause());
+        }
+        assertEquals(1, exceptionCounter);
     }
 
     @Test
