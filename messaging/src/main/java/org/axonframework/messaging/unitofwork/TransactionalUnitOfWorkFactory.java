e75899b8d1ef807ba3732e168aaf0b5b95da5831
Mateusz Nowak <mateusz@nakodach.pl>
2025-05-13 19:09:36 +0200

    [#3098] feat: revise EventProcessors - add TransactionalUnitOfWorkFactory documentation

--- messaging/src/main/java/org/axonframework/messaging/unitofwork/TransactionalUnitOfWorkFactory.java
+++ messaging/src/main/java/org/axonframework/messaging/unitofwork/TransactionalUnitOfWorkFactory.java
@@ -20,15 +20,43 @@ import org.axonframework.common.transaction.Transaction;
 import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.messaging.Context;
 
-// todo: DOCS
+/**
+ * Factory for creating {@link UnitOfWork} instances that are bound to a transaction.
+ * <p>
+ * This factory creates units of work that automatically start a transaction before invocation,
+ * commit the transaction on successful completion, and roll back the transaction when an error occurs.
+ * <p>
+ * The transaction is managed by the configured {@link TransactionManager} and is stored as a resource
+ * in the unit of work's {@link Context}.
+ *
+ * @author Mateusz Nowak
+ * @since 5.0.0
+ */
 public class TransactionalUnitOfWorkFactory {
-
     private final TransactionManager transactionManager;
 
+    /**
+     * Initializes a factory with the given {@code transactionManager}.
+     *
+     * @param transactionManager The transaction manager used to create and manage transactions for the units of work
+     */
     public TransactionalUnitOfWorkFactory(TransactionManager transactionManager) {
         this.transactionManager = transactionManager;
     }
 
+    /**
+     * Creates a new {@link UnitOfWork} that is bound to a transaction.
+     * <p>
+     * The created unit of work will:
+     * <ul>
+     *     <li>Start a new transaction before invocation using the configured {@link TransactionManager}</li>
+     *     <li>Commit the transaction when the unit of work is committed</li>
+     *     <li>Roll back the transaction when an error occurs during any phase of the unit of work</li>
+     * </ul>
+     * The transaction is stored as a resource in the unit of work's context using a resource key with label "transaction".
+     *
+     * @return A new transactional unit of work
+     */
     public UnitOfWork create() {
         var unitOfWork = new UnitOfWork();
         var transactionKey = Context.ResourceKey.<Transaction>withLabel("transaction");
@@ -36,17 +64,14 @@ public class TransactionalUnitOfWorkFactory {
             var transaction = transactionManager.startTransaction();
             ctx.putResource(transactionKey, transaction);
         });
-
         unitOfWork.runOnCommit(ctx -> {
             var transaction = ctx.getResource(transactionKey);
             transaction.commit();
         });
-
         unitOfWork.onError((ctx, phase, error) -> {
             var transaction = ctx.getResource(transactionKey);
             transaction.rollback();
         });
-
         return unitOfWork;
     }
 }
