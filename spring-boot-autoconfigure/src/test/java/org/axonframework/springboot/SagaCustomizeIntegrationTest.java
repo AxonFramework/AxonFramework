082e989d7934664e0424d417ab2aecf553dc09b1
Mateusz Nowak <mateusz@nakodach.pl>
2025-05-30 13:28:12 +0200

    [#3098] feat: revise EventProcessors - disable SagaCustomizeIntegrationTest#publishSomeEvents

--- spring-boot-autoconfigure/src/test/java/org/axonframework/springboot/SagaCustomizeIntegrationTest.java
+++ spring-boot-autoconfigure/src/test/java/org/axonframework/springboot/SagaCustomizeIntegrationTest.java
@@ -86,6 +86,7 @@ class SagaCustomizeIntegrationTest {
     @Autowired
     private EventProcessingModule eventProcessingModule;
 
+    @Disabled("TODO #3443 - Adjust SagaRepository API to be async-native")
     @Test
     void publishSomeEvents() {
         publishEvent(new EchoEvent(UUID.randomUUID().toString()));
