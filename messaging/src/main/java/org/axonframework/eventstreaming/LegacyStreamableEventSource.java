12a09189bf275d7949d60075d043636a05c6e26d
Mateusz Nowak <mateusz@dddheroes.com>
2025-06-12 10:52:02 +0200

    [#3098] refactor: `PooledStreamingEventProcessor` - replace `StreamableMessageSource` with `StreamableEventSource` (#3456)
    This PR is focused on `PooledStreamingEventProcessor`, so
    `TrackingEventProcessor` remains almost unchanged for now.
    
    Next steps:
    - replace `EventHandlerInvoker` with `EventHandlingComponent`
    - probably change from inheritance `AbstractEventProcessor` to some
    composition in the processors implementations
    - allow to define `EventCriteria` per EventProcessor
    - use `StreamableEventSource` also for `TrackingEventProcessor`
    - implement async `TokenStore`

---
+++ messaging/src/main/java/org/axonframework/eventstreaming/LegacyStreamableEventSource.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.eventstreaming;
+
+import jakarta.annotation.Nonnull;
+import org.axonframework.common.stream.BlockingStream;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.TrackedEventMessage;
+import org.axonframework.eventhandling.TrackingToken;
+import org.axonframework.messaging.Context;
+import org.axonframework.messaging.Message;
+import org.axonframework.messaging.MessageStream;
+import org.axonframework.messaging.SimpleEntry;
+import org.axonframework.messaging.StreamableMessageSource;
+
+import java.time.Instant;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Adapter implementation that translates from the deprecated {@link StreamableMessageSource} interface to the new
+ * {@link StreamableEventSource} interface.
+ * <p>
+ * This adapter allows legacy implementations to work with the new streaming API by:
+ * <ul>
+ *     <li>Converting {@link BlockingStream} to {@link MessageStream}</li>
+ *     <li>Handling {@link StreamingCondition} criteria filtering</li>
+ * </ul>
+ *
+ * @param <E> The type of {@link EventMessage} streamed by this source.
+ * @author Mateusz Nowak
+ * @since 5.0.0
+ */
+@Deprecated(since = "5.0.0")
+public class LegacyStreamableEventSource<E extends EventMessage<?>> implements StreamableEventSource<E> {
+
+    private final StreamableMessageSource<E> delegate;
+
+    /**
+     * Creates a new {@code LegacyStreamableEventSource} that adapts the given {@code delegate}.
+     *
+     * @param delegate The {@link StreamableMessageSource} to adapt.
+     */
+    public LegacyStreamableEventSource(@Nonnull StreamableMessageSource<E> delegate) {
+        Objects.requireNonNull(delegate, "Delegate is required");
+        this.delegate = delegate;
+    }
+
+    @Override
+    public MessageStream<E> open(@Nonnull StreamingCondition condition) {
+        TrackingToken position = condition.position();
+        return new BlockingMessageStream<>(delegate.openStream(position), condition.criteria());
+    }
+
+    @Override
+    public CompletableFuture<TrackingToken> firstToken() {
+        return delegate.firstToken();
+    }
+
+    @Override
+    public CompletableFuture<TrackingToken> latestToken() {
+        return delegate.latestToken();
+    }
+
+    @Override
+    public CompletableFuture<TrackingToken> tokenAt(@Nonnull Instant at) {
+        return delegate.tokenAt(at);
+    }
+
+    /**
+     * A {@code MessageStream} implementation backed by a {@link BlockingStream}. This implementation directly wraps the
+     * BlockingStream and handles filtering based on criteria.
+     *
+     * @param <E> The type of {@link EventMessage} in the stream.
+     */
+    private static class BlockingMessageStream<E extends EventMessage<?>> implements MessageStream<E> {
+
+        private final BlockingStream<E> stream;
+
+        BlockingMessageStream(BlockingStream<E> stream, EventCriteria criteria) {
+            this.stream = stream;
+            if (criteria != AnyEvent.INSTANCE) {
+                throw new IllegalArgumentException(
+                        "Only AnyEvent criteria is supported in this legacy adapter, but received: " + criteria);
+            }
+        }
+
+        @Override
+        public Optional<Entry<E>> next() {
+            try {
+                if (!stream.hasNextAvailable()) {
+                    return Optional.empty();
+                }
+                return Optional.ofNullable(stream.nextAvailable()).map(this::createEntryForMessage);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return Optional.empty();
+            }
+        }
+
+        @Override
+        public Optional<Entry<E>> peek() {
+            return stream.peek().map(this::createEntryForMessage);
+        }
+
+        @Override
+        public void onAvailable(@Nonnull Runnable callback) {
+            stream.setOnAvailableCallback(callback);
+        }
+
+        @Override
+        public Optional<Throwable> error() {
+            // BlockingStream doesn't have an error reporting, so we return empty
+            return Optional.empty();
+        }
+
+        @Override
+        public boolean isCompleted() {
+            // A BlockingStream doesn't have a completion concept in the same way
+            // We can consider it completed if it has no more available messages
+            // This is a best-effort implementation
+            return !stream.hasNextAvailable() && stream.peek().isEmpty();
+        }
+
+        @Override
+        public boolean hasNextAvailable() {
+            return stream.hasNextAvailable();
+        }
+
+        @Override
+        public void close() {
+            stream.close();
+        }
+
+        private Entry<E> createEntryForMessage(E message) {
+            Context context = Context.empty();
+            if (message instanceof TrackedEventMessage<?> trackedMessage) {
+                context = TrackingToken.addToContext(context, trackedMessage.trackingToken());
+            }
+            context = context.withResource(Message.RESOURCE_KEY, message);
+            return new SimpleEntry<>(message, context);
+        }
+    }
+}
---
+++ messaging/src/test/java/org/axonframework/eventstreaming/LegacyStreamableEventSourceTest.java
@@ -0,0 +1,237 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.eventstreaming;
+
+import jakarta.annotation.Nonnull;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.EventTestUtils;
+import org.axonframework.eventhandling.GenericTrackedEventMessage;
+import org.axonframework.eventhandling.GlobalSequenceTrackingToken;
+import org.axonframework.eventhandling.TrackedEventMessage;
+import org.axonframework.eventhandling.TrackingToken;
+import org.axonframework.messaging.MessageStream;
+import org.axonframework.messaging.StreamableMessageSource;
+import org.axonframework.utils.InMemoryStreamableEventSource;
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for the {@link LegacyStreamableEventSource} class, which it's an adapter for the deprecated
+ * {@link StreamableMessageSource}.
+ *
+ * @author Mateusz Nowak
+ * @since 5.0.0
+ */
+class LegacyStreamableEventSourceTest {
+
+    private InMemoryStreamableEventSource legacyEventSource;
+    private LegacyStreamableEventSource<TrackedEventMessage<?>> testSubject;
+
+    @BeforeEach
+    void beforeEach() {
+        legacyEventSource = new InMemoryStreamableEventSource();
+        testSubject = new LegacyStreamableEventSource<>(legacyEventSource);
+    }
+
+    @Nested
+    class ConstructorTest {
+
+        @Test
+        void doNotSupportEventCriteriaOtherThanAny() {
+            assertThatThrownBy(() -> testSubject.open(
+                    StreamingCondition.conditionFor(firstToken(), EventCriteria.havingTags("tag1", "tag2"))))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(
+                            "Only AnyEvent criteria is supported in this legacy adapter, but received: TagFilteredEventCriteria[tags=[Tag[key=tag1, value=tag2]]]");
+        }
+
+        @Test
+        void shouldThrowExceptionWithNullDelegate() {
+            assertThrows(NullPointerException.class, () ->
+                    new LegacyStreamableEventSource<>(null));
+        }
+    }
+
+    @Nested
+    class TrackingTokenTest {
+
+        @Test
+        void firstTokenShouldBeSameAsInDelegate() {
+            var expected = legacyEventSource.createHeadToken();
+            var actual = testSubject.firstToken().join();
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void latestTokenShouldBeSameAsInDelegate() {
+            var expected = legacyEventSource.createTailToken();
+            var actual = testSubject.latestToken().join();
+            assertThat(actual).isEqualTo(expected);
+        }
+    }
+
+    @Nested
+    class MessageStreamTest {
+
+        private MessageStream<TrackedEventMessage<?>> messageStream;
+
+        @BeforeEach
+        void beforeEach() {
+            messageStream = testSubject.open(StreamingCondition.startingFrom(firstToken()));
+        }
+
+        @AfterEach
+        void afterEach() {
+            messageStream.close();
+        }
+
+        @Nested
+        class NextTest {
+
+            @Test
+            void shouldReturnEmptyIfNoMessagesAvailable() {
+                // when
+                var result = messageStream.next();
+
+                // then
+                assertThat(result).isEmpty();
+            }
+
+            @Test
+            void shouldReturnEventIfMessageAvailable() {
+                // given
+                GlobalSequenceTrackingToken testToken = tokenAt(1);
+                var event1 = trackedEventMessage("event-1", testToken);
+                legacyEventSource.publishMessage(event1);
+
+                // when
+                var result = messageStream.next();
+
+                // then
+                assertThat(result).isPresent();
+                assertEvent(result.get().message(), event1);
+                assertThat(TrackingToken.fromContext(result.get())).hasValue(testToken);
+            }
+        }
+
+        @Nested
+        class PeekTest {
+
+            @Test
+            void shouldReturnEmptyIfNoMessagesAvailable() {
+                // when
+                var result = messageStream.peek();
+
+                // then
+                assertThat(result).isEmpty();
+            }
+
+            @Test
+            void shouldReturnEventIfMessageAvailable() {
+                // given
+                GlobalSequenceTrackingToken testToken = tokenAt(1);
+                var event1 = trackedEventMessage("event-1", testToken);
+                legacyEventSource.publishMessage(event1);
+
+                // when
+                var result = messageStream.peek();
+
+                // then
+                assertThat(result).isPresent();
+                assertEvent(result.get().message(), event1);
+                assertThat(TrackingToken.fromContext(result.get())).hasValue(testToken);
+            }
+
+            @Test
+            void shouldNotAdvanceTeStream() {
+                // given
+                GlobalSequenceTrackingToken testToken1 = tokenAt(1);
+                GlobalSequenceTrackingToken testToken2 = tokenAt(1);
+                var event1 = trackedEventMessage("event-1", testToken1);
+                var event2 = trackedEventMessage("event-2", testToken2);
+                legacyEventSource.publishMessage(event1);
+                legacyEventSource.publishMessage(event2);
+
+                // when
+                var next = messageStream.next();
+
+                // then
+                assertThat(next).isPresent();
+                assertEvent(next.get().message(), event1);
+
+                // when
+                var result1 = messageStream.peek();
+                var result2 = messageStream.peek();
+                var result3 = messageStream.peek();
+
+                // then
+                assertThat(result1).isEqualTo(result2);
+                assertThat(result1).isEqualTo(result3);
+            }
+        }
+
+        @Nested
+        class CompletedTest {
+
+            @Test
+            void shouldNotConsiderCompletedIfMessagesAvailable() {
+                // given
+                GlobalSequenceTrackingToken testToken = tokenAt(1);
+                var event1 = trackedEventMessage("event-1", testToken);
+                legacyEventSource.publishMessage(event1);
+
+                // when
+                var result = messageStream.isCompleted();
+
+                // then
+                assertThat(result).isFalse();
+            }
+
+            @Test
+            void shouldConsiderCompletedIfNoMessagesAvailable() {
+                // when
+                var result = messageStream.isCompleted();
+
+                // then
+                assertThat(result).isTrue();
+            }
+        }
+
+        private static void assertEvent(EventMessage<?> actual, EventMessage<?> expected) {
+            assertEquals(expected.getIdentifier(), actual.getIdentifier());
+            assertEquals(expected.getPayload(), actual.getPayload());
+            assertEquals(expected.getTimestamp(), actual.getTimestamp());
+            assertEquals(expected.getMetaData(), actual.getMetaData());
+        }
+    }
+
+    private static GlobalSequenceTrackingToken firstToken() {
+        return tokenAt(0L);
+    }
+
+    @Nonnull
+    private static GlobalSequenceTrackingToken tokenAt(long globalIndex) {
+        return new GlobalSequenceTrackingToken(globalIndex);
+    }
+
+    private TrackedEventMessage<?> trackedEventMessage(String payload, TrackingToken token) {
+        return new GenericTrackedEventMessage<>(token, EventTestUtils.asEventMessage(payload));
+    }
+}
