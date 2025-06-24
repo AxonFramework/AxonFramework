fbac9526bbd78d07a1a3955602177160451d3673
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-06-24 14:27:13 +0200

    Fine tune and add JavaDoc to clarify how these beans allow for a..
    Fine tune and add JavaDoc to clarify how these beans allow for a
    hierarchical Spring Context
    
    #3075

--- spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/autoconfig/AxonAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/autoconfig/AxonAutoConfiguration.java
@@ -33,8 +33,10 @@ import org.springframework.context.annotation.Bean;
  * {@link org.axonframework.configuration.ComponentRegistry} implementations that supports beans to be accessed as
  * components.
  * <p>
- * Lifecycle handlers are managed by the Spring Application context to allow "weaving" of these lifecycles with the
- * Spring lifecycles.
+ * Lifecycle handlers from the {@link org.axonframework.configuration.LifecycleRegistry} are managed by the Spring
+ * Application context to allow "weaving" of these lifecycles with the Spring lifecycles.
+ * <p>
+ * This autoconfiguration will construct the beans such that they allow for a hierarchical Spring Application Context.
  *
  * @author Allard Buijze
  * @author Josh Long
@@ -43,18 +45,40 @@ import org.springframework.context.annotation.Bean;
 @AutoConfiguration
 public class AxonAutoConfiguration {
 
+    /**
+     * Bean creation method registering a {@link SpringComponentRegistry} <b>if</b> the current Application Context does
+     * not have one already.
+     * <p>
+     * By only checking the {@link SearchStrategy#CURRENT current} context, we ensure that every context for a
+     * hierarchical Spring Application Context receives a {@link org.axonframework.configuration.ComponentRegistry}
+     * instance.
+     */
     @Bean
     @ConditionalOnMissingBean(search = SearchStrategy.CURRENT)
     SpringComponentRegistry springComponentRegistry(ApplicationContext applicationContext) {
         return new SpringComponentRegistry(applicationContext);
     }
 
+    /**
+     * Bean creation method registering a {@link SpringLifecycleRegistry} when there is none <b>anywhere</b>.
+     * <p>
+     * By checking {@link SearchStrategy#ALL everywhere}, we ensure that there only is a single
+     * {@link org.axonframework.configuration.LifecycleRegistry} at all times. Even when a hierarchical Spring
+     * Application Context is in place.
+     */
     @Bean
     @ConditionalOnMissingBean(search = SearchStrategy.ALL)
     SpringLifecycleRegistry springLifecycleRegistry() {
         return new SpringLifecycleRegistry();
     }
 
+    /**
+     * Bean creation method registering a {@link SpringAxonApplication} when there is none <b>anywhere</b>.
+     * <p>
+     * By checking {@link SearchStrategy#ALL everywhere}, we ensure that there only is a single main
+     * {@link org.axonframework.configuration.ApplicationConfigurer} at all times. Even when a hierarchical Spring
+     * Application Context is in place.
+     */
     @Bean
     @ConditionalOnMissingBean(search = SearchStrategy.ALL)
     SpringAxonApplication axonApplication(SpringComponentRegistry springComponentRegistry,
@@ -62,6 +86,15 @@ public class AxonAutoConfiguration {
         return new SpringAxonApplication(springComponentRegistry, springLifecycleRegistry);
     }
 
+    /**
+     * Bean creation method registering a {@link AxonConfiguration} if the Application Context does not have one
+     * already
+     * <b>and</b> if the current Application Context has a {@link SpringAxonApplication}.
+     * <p>
+     * By only checking the {@link SearchStrategy#CURRENT current} context for a {@code SpringAxonApplication}, we
+     * ensure that there only is a single main {@link AxonConfiguration} at all times. Even when a hierarchical Spring
+     * Application Context is in place.
+     */
     @Bean
     @ConditionalOnMissingBean
     @ConditionalOnBean(value = SpringAxonApplication.class, search = SearchStrategy.CURRENT)
@@ -70,10 +103,12 @@ public class AxonAutoConfiguration {
     }
 
     /**
-     * This bean is <b>only</b> created when a hierarchical Spring Application Context is being used. Only then will
-     * there be a parent Application Context with a bean of type {@link AxonConfiguration}. Furthermore, only then do we
-     * need another {@link Configuration} instance in the child Application Context, to be able to access Axon's
-     * components.
+     * Bean creation method registering a {@link Configuration} <b>if</b> the current Application Context does not have
+     * one already.
+     * <p>
+     * This bean is <b>only</b> created when a hierarchical Spring Application Context is being used, since only then
+     * will it <b>not</b> clash with the {@link #axonApplicationConfiguration(SpringAxonApplication)}. This holds since
+     * the returned {@link AxonConfiguration} is a {@code Configuration} instance.
      */
     @Bean
     @ConditionalOnMissingBean(search = SearchStrategy.CURRENT)
