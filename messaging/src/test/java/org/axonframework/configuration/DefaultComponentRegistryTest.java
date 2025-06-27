751d7d5690edf528f39bee791cb3113fa18cbce5
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-06-27 16:39:55 +0200

    Remove lame piece of text
    Remove lame piece of text

--- messaging/src/test/java/org/axonframework/configuration/DefaultComponentRegistryTest.java
+++ messaging/src/test/java/org/axonframework/configuration/DefaultComponentRegistryTest.java
@@ -34,7 +34,7 @@ import static org.mockito.Mockito.*;
 
 /**
  * Test class validating the {@link DefaultComponentRegistry}.
- *asdfasdf
+ *
  * @author Allard Buijze
  */
 class DefaultComponentRegistryTest {
