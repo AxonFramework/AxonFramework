4c0e66a6741b01817d8ae855abef6702d8a1d8b7
Mateusz Nowak <mateusz@nakodach.pl>
2025-07-15 12:02:46 +0200

    Revert "feat(event-sourcing): experiment - EventProcessingPipeline"
    This reverts commit a21e0fbbbc6509e26cef3fada717eb5561fc70d3.

--- messaging/src/main/java/org/axonframework/eventhandling/SubscribingEventProcessor.java
+++ messaging/src/main/java/org/axonframework/eventhandling/SubscribingEventProcessor.java
@@ -17,14 +17,21 @@
 package org.axonframework.eventhandling;
 
 import jakarta.annotation.Nonnull;
+import org.axonframework.common.AxonConfigurationException;
 import org.axonframework.common.Registration;
+import org.axonframework.common.transaction.NoTransactionManager;
 import org.axonframework.common.transaction.TransactionManager;
+import org.axonframework.lifecycle.Phase;
 import org.axonframework.messaging.MessageHandlerInterceptor;
 import org.axonframework.messaging.SubscribableMessageSource;
 import org.axonframework.messaging.unitofwork.TransactionalUnitOfWorkFactory;
+import org.axonframework.monitoring.MessageMonitor;
+import org.axonframework.monitoring.NoOpMessageMonitor;
 
 import java.util.List;
 import java.util.function.Consumer;
+
+import static org.axonframework.common.BuilderUtils.assertNonNull;
 
 /**
  * Event processor implementation that {@link EventBus#subscribe(Consumer) subscribes} to the {@link EventBus} for
@@ -41,42 +48,66 @@ public class SubscribingEventProcessor implements EventProcessor {
     private final SubscribableMessageSource<? extends EventMessage<?>> messageSource;
     private final EventProcessingStrategy processingStrategy;
     private final TransactionalUnitOfWorkFactory transactionalUnitOfWorkFactory;
-    private final EventProcessingPipeline pipeline;
+    private final EventProcessorOperations eventProcessorOperations;
 
     private volatile Registration eventBusRegistration;
 
-    public SubscribingEventProcessor(
-            SubscribableMessageSource<? extends EventMessage<?>> messageSource,
-            EventProcessingStrategy processingStrategy,
-            TransactionManager transactionManager,
-            EventProcessingPipeline pipeline
-    ) {
-        this.messageSource = messageSource;
-        this.processingStrategy = processingStrategy;
-        this.transactionalUnitOfWorkFactory = new TransactionalUnitOfWorkFactory(transactionManager);
-        this.pipeline = pipeline;
+    /**
+     * Instantiate a {@code SubscribingEventProcessor} based on the fields contained in the {@link Builder}.
+     * <p>
+     * Will assert that the Event Processor {@code name}, {@link EventHandlerInvoker} and
+     * {@link SubscribableMessageSource} are not {@code null}, and will throw an {@link AxonConfigurationException} if
+     * any of them is {@code null}.
+     *
+     * @param builder the {@link Builder} used to instantiate a {@code SubscribingEventProcessor} instance
+     */
+    protected SubscribingEventProcessor(Builder builder) {
+        builder.validate();
+        this.messageSource = builder.messageSource;
+        this.processingStrategy = builder.processingStrategy;
+        this.transactionalUnitOfWorkFactory = new TransactionalUnitOfWorkFactory(builder.transactionManager);
+        this.eventProcessorOperations = new EventProcessorOperations.Builder()
+                .name(builder.name())
+                .eventHandlingComponent(builder.eventHandlingComponent())
+                .errorHandler(builder.errorHandler())
+                .spanFactory(builder.spanFactory())
+                .messageMonitor(builder.messageMonitor())
+                .build();
+    }
+
+    /**
+     * Instantiate a Builder to be able to create a {@code SubscribingEventProcessor}.
+     * <p>
+     * {@link ErrorHandler} is defaulted to a {@link PropagatingErrorHandler}, the {@link MessageMonitor} defaults to a
+     * {@link NoOpMessageMonitor}, the {@link EventProcessingStrategy} defaults to a
+     * {@link DirectEventProcessingStrategy}, the {@link EventProcessorSpanFactory} defaults to a
+     * {@link DefaultEventProcessorSpanFactory} backed by a {@link org.axonframework.tracing.NoOpSpanFactory}, and the
+     * {@link TransactionManager} defaults to the {@link NoTransactionManager#INSTANCE}. The Event Processor
+     * {@code name}, {@link EventHandlerInvoker} and {@link SubscribableMessageSource} are <b>hard requirements</b> and
+     * as such should be provided.
+     *
+     * @return a Builder to be able to create a {@code SubscribingEventProcessor}
+     */
+    public static Builder builder() {
+        return new Builder();
     }
 
     @Override
     public String getName() {
-        return pipeline.getProcessorName();
+        return eventProcessorOperations.name();
     }
 
     @Override
     public List<MessageHandlerInterceptor<? super EventMessage<?>>> getHandlerInterceptors() {
-        // Interceptors are managed by the pipeline; expose if needed
-        if (pipeline instanceof InterceptorEventProcessingPipelineDecorator decorator) {
-            return decorator.getInterceptors();
-        }
-        return List.of();
+        return eventProcessorOperations.handlerInterceptors();
     }
 
-    @Override
-    public Registration registerHandlerInterceptor(
-            @Nonnull MessageHandlerInterceptor<? super EventMessage<?>> handlerInterceptor) {
-        throw new UnsupportedOperationException("Dynamic interceptor registration is not supported in this refactored version. Pass interceptors at construction time.");
-    }
-
+    /**
+     * Start this processor. This will register the processor with the {@link EventBus}.
+     * <p>
+     * Upon start up of an application, this method will be invoked in the
+     * {@link Phase#LOCAL_MESSAGE_HANDLER_REGISTRATIONS} phase.
+     */
     @Override
     public void start() {
         if (eventBusRegistration != null) {
@@ -98,15 +129,30 @@ public class SubscribingEventProcessor implements EventProcessor {
         return false;
     }
 
+    /**
+     * Process the given messages. A Unit of Work must be created for this processing.
+     * <p>
+     * This implementation creates a Batching unit of work for the given batch of {@code eventMessages}.
+     *
+     * @param eventMessages The messages to process
+     */
     protected void process(List<? extends EventMessage<?>> eventMessages) {
-        var unitOfWork = transactionalUnitOfWorkFactory.create();
         try {
-            pipeline.processInUnitOfWork(eventMessages, unitOfWork, List.of()).join();
+            var unitOfWork = transactionalUnitOfWorkFactory.create();
+            eventProcessorOperations.processInUnitOfWork(eventMessages, unitOfWork);
+        } catch (RuntimeException e) {
+            throw e;
         } catch (Exception e) {
             throw new EventProcessingException("Exception occurred while processing events", e);
         }
     }
 
+    /**
+     * Shut down this processor. This will deregister the processor with the {@link EventBus}.
+     * <p>
+     * Upon shutdown of an application, this method will be invoked in the
+     * {@link Phase#LOCAL_MESSAGE_HANDLER_REGISTRATIONS} phase.
+     */
     @Override
     public void shutDown() {
         if (eventBusRegistration != null) {
@@ -115,7 +161,138 @@ public class SubscribingEventProcessor implements EventProcessor {
         eventBusRegistration = null;
     }
 
+    /**
+     * Returns the message source from which this processor receives its events
+     *
+     * @return the MessageSource from which the processor receives its events
+     */
     public SubscribableMessageSource<? extends EventMessage<?>> getMessageSource() {
         return messageSource;
+    }
+
+    @Override
+    public Registration registerHandlerInterceptor(
+            @Nonnull MessageHandlerInterceptor<? super EventMessage<?>> handlerInterceptor) {
+        return eventProcessorOperations.registerHandlerInterceptor(handlerInterceptor);
+    }
+
+    /**
+     * Builder class to instantiate a {@link SubscribingEventProcessor}.
+     * <p>
+     * {@link ErrorHandler} is defaulted to a {@link PropagatingErrorHandler}, the {@link MessageMonitor} defaults to a
+     * {@link EventProcessorSpanFactory} is defaulted to a {@link DefaultEventProcessorSpanFactory} backed by a
+     * {@link org.axonframework.tracing.NoOpSpanFactory}, the {@link MessageMonitor} defaults to a
+     * {@link NoOpMessageMonitor}, the {@link EventProcessingStrategy} defaults to a
+     * {@link DirectEventProcessingStrategy} and the {@link TransactionManager} defaults to the
+     * {@link NoTransactionManager#INSTANCE}. The Event Processor {@code name}, {@link EventHandlerInvoker} and
+     * {@link SubscribableMessageSource} are <b>hard requirements</b> and as such should be provided.
+     */
+    public static class Builder extends EventProcessorBuilder {
+
+        private SubscribableMessageSource<? extends EventMessage<?>> messageSource;
+        private EventProcessingStrategy processingStrategy = DirectEventProcessingStrategy.INSTANCE;
+        private TransactionManager transactionManager = NoTransactionManager.INSTANCE;
+
+        public Builder() {
+            super();
+        }
+
+        @Override
+        public Builder name(@Nonnull String name) {
+            super.name(name);
+            return this;
+        }
+
+        @Override
+        public Builder eventHandlerInvoker(@Nonnull EventHandlerInvoker eventHandlerInvoker) {
+            super.eventHandlerInvoker(eventHandlerInvoker);
+            return this;
+        }
+
+        @Override
+        public Builder eventHandlingComponent(@Nonnull EventHandlingComponent eventHandlingComponent) {
+            super.eventHandlingComponent(eventHandlingComponent);
+            return this;
+        }
+
+        @Override
+        public Builder errorHandler(@Nonnull ErrorHandler errorHandler) {
+            super.errorHandler(errorHandler);
+            return this;
+        }
+
+        @Override
+        public Builder messageMonitor(@Nonnull MessageMonitor<? super EventMessage<?>> messageMonitor) {
+            super.messageMonitor(messageMonitor);
+            return this;
+        }
+
+        @Override
+        public Builder spanFactory(@Nonnull EventProcessorSpanFactory spanFactory) {
+            super.spanFactory(spanFactory);
+            return this;
+        }
+
+        /**
+         * Sets the {@link SubscribableMessageSource} (e.g. the {@link EventBus}) to which this {@link EventProcessor}
+         * implementation will subscribe itself to receive {@link EventMessage}s.
+         *
+         * @param messageSource the {@link SubscribableMessageSource} (e.g. the {@link EventBus}) to which this
+         *                      {@link EventProcessor} implementation will subscribe itself to receive
+         *                      {@link EventMessage}s
+         * @return the current Builder instance, for fluent interfacing
+         */
+        public Builder messageSource(@Nonnull SubscribableMessageSource<? extends EventMessage<?>> messageSource) {
+            assertNonNull(messageSource, "SubscribableMessageSource may not be null");
+            this.messageSource = messageSource;
+            return this;
+        }
+
+        /**
+         * Sets the {@link EventProcessingStrategy} determining whether events are processed directly or asynchronously.
+         * Defaults to a {@link DirectEventProcessingStrategy}.
+         *
+         * @param processingStrategy the {@link EventProcessingStrategy} determining whether events are processed
+         *                           directly or asynchronously
+         * @return the current Builder instance, for fluent interfacing
+         */
+        public Builder processingStrategy(@Nonnull EventProcessingStrategy processingStrategy) {
+            assertNonNull(processingStrategy, "EventProcessingStrategy may not be null");
+            this.processingStrategy = processingStrategy;
+            return this;
+        }
+
+        /**
+         * Sets the {@link TransactionManager} used when processing {@link EventMessage}s.
+         *
+         * @param transactionManager the {@link TransactionManager} used when processing {@link EventMessage}s
+         * @return the current Builder instance, for fluent interfacing
+         */
+        public Builder transactionManager(@Nonnull TransactionManager transactionManager) {
+            assertNonNull(transactionManager, "TransactionManager may not be null");
+            this.transactionManager = transactionManager;
+            return this;
+        }
+
+        /**
+         * Initializes a {@link SubscribingEventProcessor} as specified through this Builder.
+         *
+         * @return a {@link SubscribingEventProcessor} as specified through this Builder
+         */
+        public SubscribingEventProcessor build() {
+            return new SubscribingEventProcessor(this);
+        }
+
+        /**
+         * Validates whether the fields contained in this Builder are set accordingly.
+         *
+         * @throws AxonConfigurationException if one field is asserted to be incorrect according to the Builder's
+         *                                    specifications
+         */
+        @Override
+        protected void validate() throws AxonConfigurationException {
+            super.validate();
+            assertNonNull(messageSource, "The SubscribableMessageSource is a hard requirement and should be provided");
+        }
     }
 }
