c5f19224aae8e8579b4b032e12d22dbd4e6b55d9
Mateusz Nowak <mateusz@nakodach.pl>
2025-07-16 01:52:31 +0200

    [#3098] feat(event-handling): EventProcessingPipeline & EventHandlingComponent decorators
    Contains (reversed order):
    - EventProcessingPipeline - remove Segment from the interface
    - EventProcessingPipeline - add item to the interface
    - EventProcessingPipeline - and handlers - delegate as the last parameter
    - do not use DefaultEventProcessingPipeline
    - egmentMatcher just used in the DefaultWorkPackageEventFilter
    - remove SegmentMatchingEventProcessingPipeline
    - remove InterceptingEventProcessingPipeline
    - EventProcessingModuleTest - disabled tests based on EventProcessorOperations
    - fix WorkPackageTest tests - align with the new BatchProcessor behavior
    - processors use EventProcessingPipeline interface instead of the default implementation
    - DefaultEventProcessingPipeline - extract interceptors list
    - add MessageHandlerInterceptors class
    - EventProcessorOperations renamed to DefaultEventProcessingPipeline
    - EventProcessingPipeline - add TrackingEventProcessingPipeline
    - EventProcessingPipeline - add ErrorHandlingEventProcessingPipeline
    - EventProcessingPipeline - add HandlingEventProcessingPipeline impl
    - EventHandlingComponent - add TrackingEventHandlingComponent impl
    - EventHandlingComponent - add delegating, intercepting and monitoring impls

---
+++ messaging/src/main/java/org/axonframework/eventhandling/pipeline/TracingEventProcessingPipeline.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.eventhandling.pipeline;
+
+import jakarta.annotation.Nonnull;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.messaging.Message;
+import org.axonframework.messaging.MessageStream;
+import org.axonframework.messaging.unitofwork.ProcessingContext;
+import org.axonframework.tracing.Span;
+import org.axonframework.tracing.SpanScope;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * An {@link EventProcessingPipeline} that tracks the processing of the event batch using a {@link Span} provider.
+ * <p>
+ * It delegates the actual event processing to another {@link EventProcessingPipeline} while tracking the event batch.
+ *
+ * @author Allard Buijze
+ * @author Mateusz Nowak
+ * @author Mitchell Herrijgers
+ * @author Steven van Beelen
+ * @since 5.0.0
+ */
+public class TracingEventProcessingPipeline implements EventProcessingPipeline {
+
+    private final EventProcessingPipeline next;
+    private final Function<List<? extends EventMessage<?>>, Span> spanProvider;
+
+    /**
+     * Constructs the pipeline with given {@code next} pipeline to delegate calls to and a
+     * {@code spanProvider} to create spans for the event batch.
+     *
+     * @param next         The instance to delegate calls to.
+     * @param spanProvider The provider of {@link Span} to track the event batch.
+     */
+    public TracingEventProcessingPipeline(
+            @Nonnull Function<List<? extends EventMessage<?>>, Span> spanProvider,
+            @Nonnull EventProcessingPipeline next
+    ) {
+        this.next = Objects.requireNonNull(next, "Next may not be null");
+        this.spanProvider = Objects.requireNonNull(spanProvider, "Span provider may not be null");
+    }
+
+    @Override
+    public MessageStream.Empty<Message<Void>> process(
+            List<? extends EventMessage<?>> events,
+            ProcessingContext context
+    ) {
+        Span span = spanProvider.apply(events);
+        span.start();
+        try (SpanScope ignored = span.makeCurrent()) { // works as long as the MessageStream doesn't change threads
+            return next.process(events, context)
+                       .whenComplete(span::end)
+                       .onErrorContinue(ex -> {
+                           span.recordException(ex);
+                           span.end();
+                           return MessageStream.failed(ex);
+                       }).ignoreEntries().cast();
+        } catch (Exception e) {
+            span.recordException(e);
+            span.end();
+            return MessageStream.failed(e);
+        }
+    }
+}
