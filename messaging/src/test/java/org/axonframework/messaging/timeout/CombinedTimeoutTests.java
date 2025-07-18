580aa9d2099f4a2c8ac15d446d2eedd49882ab23
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2025-07-18 09:24:13 +0100

    Enhance timeout handling and improve exception management in Axon tasks

--- messaging/src/test/java/org/axonframework/messaging/timeout/CombinedTimeoutTests.java
+++ messaging/src/test/java/org/axonframework/messaging/timeout/CombinedTimeoutTests.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.axonframework.messaging.timeout;
 
 import org.axonframework.eventhandling.EventMessage;
@@ -24,7 +39,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * {@link UnitOfWorkTimeoutInterceptor} are tested in isolation, but this test class combines them to ensure that the
  * timeout behavior works as expected when both are used together.
  */
-public class CombinedTimeoutTests {
+class CombinedTimeoutTests {
 
     /**
      * Simple test where the message handling member takes longer than the timeout specified, and the batch fits inside
