1fe20e1a4f4ba68d8792ed181fa61a0e939e8452
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-07-23 14:50:16 +0200

    Minor JavaDoc changes to accompany the dif between client and stream id
    Minor JavaDoc changes to accompany the dif between client and stream id

--- axon-server-connector/src/main/java/org/axonframework/axonserver/connector/TopologyChangeListener.java
+++ axon-server-connector/src/main/java/org/axonframework/axonserver/connector/TopologyChangeListener.java
@@ -20,9 +20,9 @@ import java.util.function.Consumer;
  * A {@code TopologyChange} includes more information to better comprehend the actual switch, being:
  * <ul>
  *     <li>{@link TopologyChange#getContext() The context} - The context from which the change originates.</li>
- *     <li>{@link TopologyChange#getClientId() The client id} - The identifier of the client (e.g., the Axon Framework node) for which a topology change occurred.</li>
- *     <li>{@link TopologyChange#getClientStreamId() The client stream id} - The identifier of the stream/connection for a client (e.g., the Axon Framework node) for which a topology change occurred.</li>
- *     <li>{@link TopologyChange#getComponentName() The component name} - The name of the component (e.g., the Axon Framework node) for which a topology change occurred.</li>
+ *     <li>{@link TopologyChange#getClientId() The client id} - The {@link AxonServerConfiguration#getClientId() identifier of the client} for which a topology change occurred.</li>
+ *     <li>{@link TopologyChange#getClientStreamId() The client stream id} - The identifier of the stream/connection for a client (e.g., the Axon Framework node) for which a topology change occurred, defined by Axon Server.</li>
+ *     <li>{@link TopologyChange#getComponentName() The component name} - The {@link AxonServerConfiguration#getComponentName() name of the component} for which a topology change occurred.</li>
  *     <li>{@link TopologyChange#getCommand() The command subscription identifier} <b>or</b> {@link TopologyChange#getQuery() The query subscription identifier} - The identifier of the handler that was added or removed</li>
  * </ul>
  * Here is an example {@code TopologyChange} for a command subscription:
@@ -38,7 +38,7 @@ import java.util.function.Consumer;
  * }</pre>
  * <p>
  * An example use case of this functional interface, is to get notified when a new instance connects or disconnects. A
- * newly connecting instance would mean a new {@link TopologyChange#getClientId()} entered, while a disconnect is
+ * newly connecting instance would mean a new {@link TopologyChange#getClientStreamId()} entered, while a disconnect is
  * signaled by the {@code RESET_ALL} {@link TopologyChange#getUpdateType()}. Such a coarse topology change means the
  * routing of commands will change, which impacts {@link org.axonframework.common.caching.Cache} hits for your
  * aggregates or sagas, for example.
