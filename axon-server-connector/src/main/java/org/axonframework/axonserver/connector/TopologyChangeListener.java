b7123543fe827568989b8c8b191ea3ee2036ed39
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-07-24 14:16:14 +0200

    React on a TopologyChange i.o. a gRPC TopologyChange
    React on a TopologyChange i.o. a gRPC TopologyChange

--- axon-server-connector/src/main/java/org/axonframework/axonserver/connector/TopologyChangeListener.java
+++ axon-server-connector/src/main/java/org/axonframework/axonserver/connector/TopologyChangeListener.java
@@ -1,45 +1,47 @@
 package org.axonframework.axonserver.connector;
 
-import io.axoniq.axonserver.grpc.control.TopologyChange;
-
 import java.util.function.Consumer;
 
 /**
  * A functional interface representing a listener that is notified when a {@link TopologyChange} occurs to a specific
  * {@link AxonServerConfiguration#getContext() context}.
  * <p>
- * A {@code TopologyChange} can reflect any of the following {@link TopologyChange#getUpdateType() updates}:
+ * A {@code TopologyChange} can reflect any of the following {@link TopologyChange#type() updates}:
  * <ol>
- *     <li>{@code ADD_COMMAND_HANDLER} - A <b>command</b> handler was added by a specific instance to the context the listener is attached to.</li>
- *     <li>{@code REMOVE_COMMAND_HANDLER} - A <b>command</b> handler was removed from a specific instance for the context the listener is attached to.</li>
- *     <li>{@code ADD_QUERY_HANDLER} - A <b>query</b> handler was added by a specific instance to the context the listener is attached to.</li>
- *     <li>{@code REMOVE_QUERY_HANDLER} - A <b>query</b> handler was removed from a specific instance for the context the listener is attached to.</li>
- *     <li>{@code RESET_ALL} - This signals the connection broke down of a specific instance for the context the listener is attached to.</li>
+ *     <li>{@link org.axonframework.axonserver.connector.TopologyChange.Type#COMMAND_HANDLER_ADDED} - A <b>command</b> handler was added by a specific instance to the context the listener is attached to.</li>
+ *     <li>{@link org.axonframework.axonserver.connector.TopologyChange.Type#COMMAND_HANDLER_REMOVED} - A <b>command</b> handler was removed from a specific instance for the context the listener is attached to.</li>
+ *     <li>{@link org.axonframework.axonserver.connector.TopologyChange.Type#QUERY_HANDLER_ADDED} - A <b>query</b> handler was added by a specific instance to the context the listener is attached to.</li>
+ *     <li>{@link org.axonframework.axonserver.connector.TopologyChange.Type#QUERY_HANDLER_REMOVED} - A <b>query</b> handler was removed from a specific instance for the context the listener is attached to.</li>
+ *     <li>{@link org.axonframework.axonserver.connector.TopologyChange.Type#RESET} - This signals the connection broke down of a specific instance for the context the listener is attached to.</li>
  * </ol>
  * <p>
  * A {@code TopologyChange} includes more information to better comprehend the actual change, being:
  * <ul>
- *     <li>{@link TopologyChange#getContext() The context} - The context from which the change originates.</li>
- *     <li>{@link TopologyChange#getClientId() The client id} - The {@link AxonServerConfiguration#getClientId() identifier of the client} for which a topology change occurred.</li>
- *     <li>{@link TopologyChange#getClientStreamId() The client stream id} - The identifier of the stream/connection for a client (e.g., the Axon Framework node) for which a topology change occurred, defined by Axon Server.</li>
- *     <li>{@link TopologyChange#getComponentName() The component name} - The {@link AxonServerConfiguration#getComponentName() name of the component} for which a topology change occurred.</li>
- *     <li>{@link TopologyChange#getCommand() The command subscription identifier} <b>or</b> {@link TopologyChange#getQuery() The query subscription identifier} - The identifier of the handler that was added or removed</li>
+ *     <li>{@link TopologyChange#type() The type} - The type of update.</li>
+ *     <li>{@link TopologyChange#context() The context} - The context from which the change originates.</li>
+ *     <li>{@link TopologyChange#clientId() The client id} - The {@link AxonServerConfiguration#getClientId() identifier of the client} for which a topology change occurred.</li>
+ *     <li>{@link TopologyChange#clientStreamId() The client stream id} - The identifier of the stream/connection for a client (e.g., the Axon Framework node) for which a topology change occurred, defined by Axon Server.</li>
+ *     <li>{@link TopologyChange#componentName() The component name} - The {@link AxonServerConfiguration#getComponentName() name of the component} for which a topology change occurred.</li>
+ *     <li>{@link TopologyChange#handler() The handler subscription} - The handler subscription containing the {@link TopologyChange.HandlerSubscription#name()} and an optional {@link TopologyChange.HandlerSubscription#loadFactor()} that is only present for command handler subscriptions.</li>
  * </ul>
- * Here is an example {@code TopologyChange} for a command subscription:
+ * Here is an example {@link TopologyChange#toString()} for a {@link org.axonframework.axonserver.connector.TopologyChange.Type#COMMAND_HANDLER_ADDED}:
  * <pre>{@code
- * context: "axoniq-university"
- * client_id: "149466@axoniq-uni"
- * client_stream_id: "149466@axoniq-uni.2bb5c0b4-59d9-4396-ad14-a32059f71d9a"
- * component_name: "AxonIQ University"
- * command {
- *   name: "io.axoniq.demo.university.faculty.write.createcourse.CreateCourse"
- *   load_factor: 100
+ * TopologyChange{
+ *     type=COMMAND_HANDLER_ADDED,
+ *     context='axoniq-university',
+ *     clientId='149466@axoniq-uni',
+ *     clientStreamId='149466@axoniq-uni.2bb5c0b4-59d9-4396-ad14-a32059f71d9a',
+ *     componentName='AxonIQ University',
+ *     handlerSubscription=HandlerSubscription{
+ *         name='io.axoniq.demo.university.faculty.write.createcourse.CreateCourse',
+ *         loadFactor=100
+ *     }
  * }
  * }</pre>
  * <p>
  * An example use case of this functional interface, is to get notified when a new instance connects or disconnects. A
- * newly connecting instance would mean a new {@link TopologyChange#getClientStreamId()} entered, while a disconnect is
- * signaled by the {@code RESET_ALL} {@link TopologyChange#getUpdateType()}. Such a coarse topology change means the
+ * newly connecting instance would mean a new {@link TopologyChange#clientStreamId()} entered, while a disconnect is
+ * signaled by the {@link TopologyChange.Type#RESET} {@link TopologyChange#type()}. Such a coarse topology change means the
  * routing of commands will change, which impacts {@link org.axonframework.common.caching.Cache} hits for your
  * aggregates or sagas, for example.
  * <p>
@@ -56,11 +58,11 @@ import java.util.function.Consumer;
  * @since 4.12.0
  */
 @FunctionalInterface
-public interface TopologyChangeListener extends Consumer<TopologyChange> {
+public interface TopologyChangeListener extends Consumer<io.axoniq.axonserver.grpc.control.TopologyChange> {
 
     @Override
-    default void accept(TopologyChange change) {
-        onChange(change);
+    default void accept(io.axoniq.axonserver.grpc.control.TopologyChange change) {
+        onChange(new TopologyChange(change));
     }
 
     /**
