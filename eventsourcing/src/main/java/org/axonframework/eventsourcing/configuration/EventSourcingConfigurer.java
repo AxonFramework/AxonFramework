3b8fcaa4dd75e6ddf109b1739a7775c85b06868d
Mateusz Nowak <mateusz@nakodach.pl>
2025-08-03 00:53:35 +0200

    feat(test): revert creating StreamableEventSource in EventSourcingConfigurer

--- eventsourcing/src/main/java/org/axonframework/eventsourcing/configuration/EventSourcingConfigurer.java
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/configuration/EventSourcingConfigurer.java
@@ -34,7 +34,6 @@ import org.axonframework.eventsourcing.Snapshotter;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.eventsourcing.eventstore.TagResolver;
-import org.axonframework.eventstreaming.StreamableEventSource;
 import org.axonframework.modelling.configuration.ModellingConfigurer;
 import org.axonframework.modelling.configuration.StatefulCommandHandlingModule;
 
@@ -174,13 +173,7 @@ public class EventSourcingConfigurer implements ApplicationConfigurer {
      * @return The current instance of the {@code Configurer} for a fluent API.
      */
     public EventSourcingConfigurer registerEventStore(@Nonnull ComponentBuilder<EventStore> eventStoreFactory) {
-        delegate.componentRegistry(cr -> cr.registerComponent(EventStore.class, (config) -> {
-            var eventStore = eventStoreFactory.build(config);
-            if (eventStore instanceof StreamableEventSource<?> streamableEventSource) {
-                cr.registerComponent(StreamableEventSource.class, (c) -> streamableEventSource);
-            }
-            return eventStore;
-        }));
+        delegate.componentRegistry(cr -> cr.registerComponent(EventStore.class, eventStoreFactory));
         return this;
     }
 
