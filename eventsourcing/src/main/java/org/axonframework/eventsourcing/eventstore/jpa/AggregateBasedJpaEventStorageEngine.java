d9d4c0e2b3753a837bacae1bc715d4b62deb09b8
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-08-06 17:25:02 +0200

    Replace Serializer for the Converter
    Replace Serializer for the Converter. Meanwhile, drop the
    GenericDomainEventMessage mapping from the flow entirely, as that's
    redundant.
    
    #3102

--- eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/jpa/AggregateBasedJpaEventStorageEngine.java
+++ eventsourcing/src/main/java/org/axonframework/eventsourcing/eventstore/jpa/AggregateBasedJpaEventStorageEngine.java
@@ -50,8 +50,7 @@ import org.axonframework.eventstreaming.StreamingCondition;
 import org.axonframework.messaging.Context;
 import org.axonframework.messaging.MessageStream;
 import org.axonframework.messaging.MessageType;
-import org.axonframework.messaging.MetaData;
-import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.Converter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -95,7 +94,7 @@ public class AggregateBasedJpaEventStorageEngine implements EventStorageEngine {
 
     private final EntityManagerProvider entityManagerProvider;
     private final TransactionManager transactionManager;
-    private final Serializer eventSerializer;
+    private final Converter converter;
     private final PersistenceExceptionResolver persistenceExceptionResolver;
 
     private final LegacyJpaEventStorageOperations legacyJpaOperations;
@@ -108,21 +107,20 @@ public class AggregateBasedJpaEventStorageEngine implements EventStorageEngine {
      * @param entityManagerProvider The {@link jakarta.persistence.EntityManager} provided for this storage solution.
      * @param transactionManager    The transaction manager, ensuring all operations to the storage solution occur
      *                              transactionally.
-     * @param eventSerializer       The event serializer, de-/serializing events.
+     * @param converter             The converter used to convert an {@link EventMessage} it's
+     *                              {@link EventMessage#payload()} into a {@code byte[]}.
      * @param configurationOverride A unary operator that can customize the {@code AggregateBasedJpaEventStorageEngine}
      *                              under construction.
      */
-    public AggregateBasedJpaEventStorageEngine(
-            @Nonnull EntityManagerProvider entityManagerProvider,
-            @Nonnull TransactionManager transactionManager,
-            @Nonnull Serializer eventSerializer,
-            @Nonnull UnaryOperator<Customization> configurationOverride
-    ) {
-        this.entityManagerProvider = requireNonNull(entityManagerProvider, "entityManagerProvider may not be null");
-        this.transactionManager = requireNonNull(transactionManager, "transactionManager may not be null");
-        this.eventSerializer = requireNonNull(eventSerializer, "eventSerializer may not be null");
-
-        var customization = requireNonNull(configurationOverride, "configurationOverride may not be null")
+    public AggregateBasedJpaEventStorageEngine(@Nonnull EntityManagerProvider entityManagerProvider,
+                                               @Nonnull TransactionManager transactionManager,
+                                               @Nonnull Converter converter,
+                                               @Nonnull UnaryOperator<Customization> configurationOverride) {
+        this.entityManagerProvider =
+                requireNonNull(entityManagerProvider, "The entityManagerProvider may not be null.");
+        this.transactionManager = requireNonNull(transactionManager, "The transactionManager may not be null.");
+        this.converter = requireNonNull(converter, "The converter may not be null.");
+        var customization = requireNonNull(configurationOverride, "The configurationOverride may not be null.")
                 .apply(Customization.withDefaultValues());
 
         this.legacyJpaOperations = new LegacyJpaEventStorageOperations(transactionManager,
@@ -212,68 +210,42 @@ public class AggregateBasedJpaEventStorageEngine implements EventStorageEngine {
     ) {
         var entityManager = entityManagerProvider.getEntityManager();
         events.stream()
-              .map(taggedEvent -> toDomainEventMessage(taggedEvent, aggregateSequencer))
-              .map(domainEventMessage -> new DomainEventEntry(domainEventMessage, eventSerializer))
+              .map(taggedEvent -> toDomainEventEntry(taggedEvent, aggregateSequencer, converter))
               .forEach(entityManager::persist);
     }
 
-    private static DomainEventMessage<?> toDomainEventMessage(
-            TaggedEventMessage<?> taggedEvent,
-            AggregateSequencer aggregateSequencer
-    ) {
+    private static DomainEventEntry toDomainEventEntry(TaggedEventMessage<?> taggedEvent,
+                                                       AggregateSequencer aggregateSequencer,
+                                                       Converter converter) {
         var aggregateIdentifier = resolveAggregateIdentifier(taggedEvent.tags());
         var aggregateType = resolveAggregateType(taggedEvent.tags());
         var event = taggedEvent.event();
         var isAggregateEvent =
                 aggregateIdentifier != null && aggregateType != null && !taggedEvent.tags().isEmpty();
         if (isAggregateEvent) {
-            var nextSequence = aggregateSequencer.incrementAndGetSequenceOf(aggregateIdentifier);
-            return new GenericDomainEventMessage<>(
-                    aggregateType,
-                    aggregateIdentifier,
-                    nextSequence,
-                    event.identifier(),
-                    event.type(),
-                    event.payload(),
-                    event.metaData(),
-                    event.timestamp()
-            );
+            return new DomainEventEntry(event.identifier(),
+                                        event.type().name(),
+                                        event.type().version(),
+                                        event.payloadAs(byte[].class, converter),
+                                        converter.convert(event.metaData(), byte[].class),
+                                        // TODO I'd rather do something different with the metadata...
+                                        event.timestamp(),
+                                        aggregateType,
+                                        aggregateIdentifier,
+                                        aggregateSequencer.incrementAndGetSequenceOf(aggregateIdentifier));
         } else {
             // returns non-aggregate event, so the sequence is always 0
-            return new GenericDomainEventMessage<>(null,
-                                                   event.identifier(),
-                                                   0L,
-                                                   event,
-                                                   event::timestamp);
+            return new DomainEventEntry(event.identifier(),
+                                        event.type().name(),
+                                        event.type().version(),
+                                        event.payloadAs(byte[].class, converter),
+                                        converter.convert(event.metaData(), byte[].class),
+                                        // TODO I'd rather do something different with the metadata...
+                                        event.timestamp(),
+                                        null,
+                                        event.identifier(),
+                                        0L);
         }
-    }
-
-    private DomainEventMessage<?> convertToDomainEventMessage(DomainEventData<?> event) {
-        return new GenericDomainEventMessage<>(
-                event.getType(),
-                event.getAggregateIdentifier(),
-                event.getSequenceNumber(),
-                convertToEventMessage(event),
-                event.getTimestamp()
-        );
-    }
-
-    private GenericEventMessage<?> convertToEventMessage(EventData<?> event) {
-        var payload = event.getPayload();
-        var revision = payload.getType().getRevision();
-        var payloadClass = eventSerializer.classForType(payload.getType());
-        var messageType = revision == null
-                ? new MessageType(payloadClass)
-                : new MessageType(payloadClass, revision);
-        var metadata = event.getMetaData();
-        MetaData metaData = eventSerializer.convert(metadata.getData(), MetaData.class);
-        return new GenericEventMessage<>(
-                event.getEventIdentifier(),
-                messageType,
-                payload.getData(),
-                metaData,
-                event.getTimestamp()
-        );
     }
 
     @Override
@@ -318,6 +290,22 @@ public class AggregateBasedJpaEventStorageEngine implements EventStorageEngine {
                              ))
                              .cast();
         return new AggregateSource(markerReference, source);
+    }
+
+    private DomainEventMessage<?> convertToDomainEventMessage(DomainEventData<?> event) {
+        return new GenericDomainEventMessage<>(event.getType(),
+                                               event.getAggregateIdentifier(),
+                                               event.getSequenceNumber(),
+                                               convertToEventMessage(event),
+                                               event.getTimestamp());
+    }
+
+    private GenericEventMessage<?> convertToEventMessage(EventData<?> event) {
+        return new GenericEventMessage<>(event.getEventIdentifier(),
+                                         new MessageType(event.type(), event.version()),
+                                         event.payload(),
+                                         event.metaData(),
+                                         event.getTimestamp());
     }
 
     private static Context setMarkerAndBuildContext(String aggregateIdentifier,
@@ -414,7 +402,7 @@ public class AggregateBasedJpaEventStorageEngine implements EventStorageEngine {
     public void describeTo(@Nonnull ComponentDescriptor descriptor) {
         descriptor.describeProperty("entityManagerProvider", entityManagerProvider);
         descriptor.describeProperty("transactionManager", transactionManager);
-        descriptor.describeProperty("eventSerializer", eventSerializer);
+        descriptor.describeProperty("converter", converter);
         descriptor.describeProperty("persistenceExceptionResolver", persistenceExceptionResolver);
         descriptor.describeProperty("legacyJpaOperations", legacyJpaOperations);
         descriptor.describeProperty("tokenOperations", tokenOperations);
