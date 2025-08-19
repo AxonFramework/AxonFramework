cf9fce6882e9b5ea551b6a4062d7c0d4ded4e8a8
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-08-19 15:42:57 +0200

    Drop obsolete bean creation methods due to new approach
    Drop obsolete bean creation methods due to new approach. The
    SpringComponentRegistry ensures that any defaults Axon Framework sets
    area automatically part of the ApplicationContext already. As such,
    specifying them separately, as done here, is no longer required.
    
    #3102

--- spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/autoconfig/LegacyAxonAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/org/axonframework/springboot/autoconfig/LegacyAxonAutoConfiguration.java
@@ -16,63 +16,28 @@
 
 package org.axonframework.springboot.autoconfig;
 
-import jakarta.annotation.Nonnull;
 import org.axonframework.axonserver.connector.TagsConfiguration;
-import org.axonframework.commandhandling.CommandBus;
-import org.axonframework.commandhandling.InterceptingCommandBus;
-import org.axonframework.commandhandling.SimpleCommandBus;
-import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.axonframework.commandhandling.gateway.DefaultCommandGateway;
 import org.axonframework.common.AxonConfigurationException;
 import org.axonframework.common.AxonThreadFactory;
-import org.axonframework.common.FutureUtils;
 import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.config.LegacyConfiguration;
-import org.axonframework.eventhandling.EventBus;
-import org.axonframework.eventhandling.EventBusSpanFactory;
 import org.axonframework.eventhandling.EventMessage;
-import org.axonframework.eventhandling.EventSink;
-import org.axonframework.eventhandling.SimpleEventBus;
 import org.axonframework.eventhandling.TrackedEventMessage;
 import org.axonframework.eventhandling.async.SequencingPolicy;
 import org.axonframework.eventhandling.async.SequentialPerAggregatePolicy;
-import org.axonframework.eventhandling.gateway.DefaultEventGateway;
-import org.axonframework.eventhandling.gateway.EventGateway;
 import org.axonframework.eventsourcing.Snapshotter;
 import org.axonframework.eventsourcing.SnapshotterSpanFactory;
-import org.axonframework.eventsourcing.eventstore.LegacyEmbeddedEventStore;
-import org.axonframework.eventsourcing.eventstore.LegacyEventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.LegacyEventStore;
-import org.axonframework.messaging.ClassBasedMessageTypeResolver;
-import org.axonframework.messaging.MessageTypeResolver;
 import org.axonframework.messaging.StreamableMessageSource;
 import org.axonframework.messaging.SubscribableMessageSource;
 import org.axonframework.messaging.annotation.HandlerDefinition;
 import org.axonframework.messaging.annotation.ParameterResolverFactory;
-import org.axonframework.messaging.correlation.CorrelationDataProvider;
-import org.axonframework.messaging.correlation.MessageOriginProvider;
-import org.axonframework.messaging.interceptors.CorrelationDataInterceptor;
-import org.axonframework.queryhandling.DefaultQueryGateway;
-import org.axonframework.queryhandling.LoggingQueryInvocationErrorHandler;
-import org.axonframework.queryhandling.QueryBus;
-import org.axonframework.queryhandling.QueryBusSpanFactory;
-import org.axonframework.queryhandling.QueryGateway;
-import org.axonframework.queryhandling.QueryInvocationErrorHandler;
-import org.axonframework.queryhandling.QueryUpdateEmitter;
-import org.axonframework.queryhandling.QueryUpdateEmitterSpanFactory;
-import org.axonframework.queryhandling.SimpleQueryBus;
-import org.axonframework.queryhandling.SimpleQueryUpdateEmitter;
-import org.axonframework.serialization.AnnotationRevisionResolver;
-import org.axonframework.serialization.RevisionResolver;
 import org.axonframework.spring.eventsourcing.SpringAggregateSnapshotter;
-import org.axonframework.springboot.ConverterProperties;
 import org.axonframework.springboot.DistributedCommandBusProperties;
 import org.axonframework.springboot.EventProcessorProperties;
 import org.axonframework.springboot.TagsConfigurationProperties;
-import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -81,8 +46,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.function.Function;
@@ -114,75 +77,6 @@ public class LegacyAxonAutoConfiguration {
     @Bean
     public TagsConfiguration tagsConfiguration() {
         return tagsConfigurationProperties.toTagsConfiguration();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public RevisionResolver revisionResolver() {
-        return new AnnotationRevisionResolver();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public CorrelationDataProvider messageOriginProvider() {
-        return new MessageOriginProvider();
-    }
-
-    @Qualifier("eventStore")
-    @Bean(name = "eventBus")
-    @ConditionalOnMissingBean(EventBus.class)
-    @ConditionalOnBean(LegacyEventStorageEngine.class)
-    public LegacyEmbeddedEventStore eventStore(LegacyEventStorageEngine storageEngine,
-                                               LegacyConfiguration configuration) {
-        return LegacyEmbeddedEventStore.builder()
-                                       .storageEngine(storageEngine)
-                                       .messageMonitor(configuration.messageMonitor(
-                                               LegacyEventStore.class, "eventStore"
-                                       ))
-                                       .spanFactory(configuration.getComponent(EventBusSpanFactory.class))
-                                       .build();
-    }
-
-    @ConditionalOnMissingBean
-    @Bean
-    public CommandGateway commandGateway(CommandBus commandBus) {
-        return new DefaultCommandGateway(commandBus, new ClassBasedMessageTypeResolver());
-    }
-
-    @ConditionalOnMissingBean
-    @Bean
-    public QueryGateway queryGateway(QueryBus queryBus) {
-        return DefaultQueryGateway.builder().queryBus(queryBus).build();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean({LegacyEventStorageEngine.class, EventBus.class})
-    public SimpleEventBus eventBus(LegacyConfiguration configuration) {
-        return SimpleEventBus.builder()
-                             .messageMonitor(configuration.messageMonitor(LegacyEventStore.class, "eventStore"))
-                             .spanFactory(configuration.getComponent(EventBusSpanFactory.class))
-                             .build();
-    }
-
-    @ConditionalOnMissingBean
-    @Bean
-    public EventSink eventSink(EventBus eventBus) {
-        return (context, events) -> {
-            eventBus.publish(events);
-            return FutureUtils.emptyCompletedFuture();
-        };
-    }
-
-    @ConditionalOnMissingBean
-    @Bean
-    public MessageTypeResolver messageTypeResolver() {
-        return new ClassBasedMessageTypeResolver();
-    }
-
-    @ConditionalOnMissingBean
-    @Bean
-    public EventGateway eventGateway(EventSink eventSink, MessageTypeResolver messageTypeResolver) {
-        return new DefaultEventGateway(eventSink, messageTypeResolver);
     }
 
     @ConditionalOnMissingBean(Snapshotter.class)
@@ -294,49 +188,5 @@ public class LegacyAxonAutoConfiguration {
             sequencingPolicy = c -> SequentialPerAggregatePolicy.instance();
         }
         return sequencingPolicy;
-    }
-
-    @ConditionalOnMissingBean(
-            ignoredType = {
-                    "org.axonframework.commandhandling.distributed.DistributedCommandBus",
-                    "org.axonframework.extensions.multitenancy.components.commandhandeling.MultiTenantCommandBus"
-            },
-            value = CommandBus.class
-    )
-    @Qualifier("localSegment")
-    @Bean
-    public CommandBus commandBus(TransactionManager txManager, LegacyConfiguration axonConfiguration) {
-        SimpleCommandBus commandBus = new SimpleCommandBus(txManager);
-        return new InterceptingCommandBus(
-                commandBus,
-                List.of(new CorrelationDataInterceptor<>(axonConfiguration.correlationDataProviders())),
-                Collections.emptyList()
-        );
-    }
-
-    @ConditionalOnMissingBean(value = QueryBus.class)
-    @Qualifier("localSegment")
-    @Bean
-    public SimpleQueryBus queryBus(LegacyConfiguration axonConfiguration, TransactionManager transactionManager) {
-        return SimpleQueryBus.builder()
-                             .messageMonitor(axonConfiguration.messageMonitor(QueryBus.class, "queryBus"))
-                             .transactionManager(transactionManager)
-                             .errorHandler(axonConfiguration.getComponent(
-                                     QueryInvocationErrorHandler.class,
-                                     () -> LoggingQueryInvocationErrorHandler.builder().build()
-                             ))
-                             .queryUpdateEmitter(axonConfiguration.getComponent(QueryUpdateEmitter.class))
-                             .spanFactory(axonConfiguration.getComponent(QueryBusSpanFactory.class))
-                             .build();
-    }
-
-    @Bean
-    public QueryUpdateEmitter queryUpdateEmitter(LegacyConfiguration configuration) {
-        return SimpleQueryUpdateEmitter.builder()
-                                       .updateMessageMonitor(configuration.messageMonitor(
-                                               QueryUpdateEmitter.class, "queryUpdateEmitter"
-                                       ))
-                                       .spanFactory(configuration.getComponent(QueryUpdateEmitterSpanFactory.class))
-                                       .build();
     }
 }
