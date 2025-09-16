53e33e8669369111af88e374c011cf5dd876c272
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-09-16 13:17:50 +0200

    Remove model structure suggestion
    Remove model structure suggestion. It is already mentioned in issue
    #3080, so it is not required here.

--- axon-5-module-structure-suggestion/messaging-core/src/main/java/org/axonframework/messaging/MessageDispatchInterceptor.java
+++
@@ -1,9 +0,0 @@
-package org.axonframework.messaging;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
-
-public interface MessageDispatchInterceptor<M extends Message, R> {
-
-    Function<ProcessingContext, CompletableFuture<R>> intercept(Function<ProcessingContext, CompletableFuture<R>> dispatcher);
-}
--- axon-5-module-structure-suggestion/query-messaging/src/main/java/org/axonframework/query/messaging/QueryResponseMessage.java
+++
@@ -1,7 +0,0 @@
-package org.axonframework.query.messaging;
-
-import org.axonframework.messaging.Message;
-
-public interface QueryResponseMessage extends Message {
-
-}
