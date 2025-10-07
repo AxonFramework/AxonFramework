9845543be1abc038e1522effb80f9a0246c1573b
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-10-07 11:58:15 +0200

    Correct use of correlation and causation
    The MessageOriginProvider used the correlation identifier to refer to
    the previous message that "caused" it to be constructed. However, in
    most messaging-based systems, this is called the causation identifier.
    As such, we should align with the default. This essentially switches the
     old traceId to be the correlationId. The old correlationId is, instead,
      no called the causationId.
    
    #3553

--- messaging/src/main/java/org/axonframework/messaging/correlation/MessageOriginProvider.java
+++ messaging/src/main/java/org/axonframework/messaging/correlation/MessageOriginProvider.java
@@ -26,6 +26,12 @@ import java.util.Objects;
 /**
  * A {@code CorrelationDataProvider} implementation that provides the {@link Message#identifier() identifier} of a
  * {@link Message} to other {@code Messages} that are created as result of processing the given {@code message}.
+ * <p>
+ * By defaults uses the {@link #DEFAULT_CORRELATION_KEY} as the {@link Message#metadata() metadata} key for the
+ * "correlation identifier," which refers to the original {@link Message#identifier() message identifier} starting the
+ * entire business transaction / workflow. The {@link #DEFAULT_CAUSATION_KEY} is used as the
+ * {@link Message#metadata() metadata} key for the "causation identifier," referring to the
+ * {@link Message#identifier() message identifier} preceding it.
  *
  * @author Rene de Waele
  * @since 3.0.0
@@ -37,41 +43,41 @@ public class MessageOriginProvider implements CorrelationDataProvider {
      */
     public static final String DEFAULT_CORRELATION_KEY = "correlationId";
     /**
-     * The default {@link Message#metadata() metadata} key for the trace identifier of a {@link Message}.
+     * The default {@link Message#metadata() metadata} key for the causation identifier of a {@link Message}.
      */
-    public static final String DEFAULT_TRACE_KEY = "traceId";
+    public static final String DEFAULT_CAUSATION_KEY = "causationId";
 
     private final String correlationKey;
-    private final String traceKey;
+    private final String causationKey;
 
     /**
      * Initializes a {@code MessageOriginProvider} using the {@link #DEFAULT_CORRELATION_KEY} and
-     * {@link #DEFAULT_TRACE_KEY} as the {@code correlationKey} and {@code traceKey} respectively.
+     * {@link #DEFAULT_CAUSATION_KEY} as the {@code correlationKey} and {@code causationKey} respectively.
      */
     public MessageOriginProvider() {
-        this(DEFAULT_CORRELATION_KEY, DEFAULT_TRACE_KEY);
+        this(DEFAULT_CORRELATION_KEY, DEFAULT_CAUSATION_KEY);
     }
 
     /**
      * Initializes a {@code MessageOriginProvider} that uses the given {@code correlationKey}.
      *
-     * @param correlationKey The key used to store the identifier of a {@link Message} in the {@link Message#metadata()}
-     *                       of a resulting {@code Message}.
-     * @param traceKey       The key used to store the identifier of the original {@link Message} giving rise to the
+     * @param correlationKey The key used to store the identifier of the original {@link Message} giving rise to the
      *                       current {@code Message}.
+     * @param causationKey   The key used to store the identifier of a {@link Message} in the {@link Message#metadata()}
+     *                       of a resulting {@code Message}.
      */
     public MessageOriginProvider(@Nonnull String correlationKey,
-                                 @Nonnull String traceKey) {
+                                 @Nonnull String causationKey) {
         this.correlationKey = Objects.requireNonNull(correlationKey, "Correlation key must not be null.");
-        this.traceKey = Objects.requireNonNull(traceKey, "Trace key must not be null.");
+        this.causationKey = Objects.requireNonNull(causationKey, "Causation key must not be null.");
     }
 
     @Nonnull
     @Override
     public Map<String, String> correlationDataFor(@Nonnull Message message) {
         Map<String, String> result = new HashMap<>();
-        result.put(correlationKey, message.identifier());
-        result.put(traceKey, message.metadata().getOrDefault(traceKey, message.identifier()));
+        result.put(correlationKey, message.metadata().getOrDefault(correlationKey, message.identifier()));
+        result.put(causationKey, message.identifier());
         return result;
     }
 }
