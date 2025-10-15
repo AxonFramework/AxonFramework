cc0aacfc7749682579ffcd0480c5d19876ed9105
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-10-15 17:23:29 +0200

    Add converting QueryGateway
    Add converting QueryGateway
    
    #613

---
+++ messaging/src/main/java/org/axonframework/queryhandling/ConvertingQueryGateway.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.axonframework.queryhandling;
+
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import org.axonframework.common.infra.ComponentDescriptor;
+import org.axonframework.messaging.conversion.MessageConverter;
+import org.axonframework.messaging.unitofwork.ProcessingContext;
+import org.reactivestreams.Publisher;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * A {@link QueryGateway} implementation that pulls any query responses through a {@link MessageConverter}.
+ *
+ * @author Allard Buijze
+ * @author Mitchell Herrijgers
+ * @since 5.0.0
+ */ // TODO #613 implement
+public class ConvertingQueryGateway implements QueryGateway {
+
+    private final QueryGateway delegate;
+    private final MessageConverter converter;
+
+    public ConvertingQueryGateway(@Nonnull QueryGateway delegate,
+                                  @Nonnull MessageConverter converter) {
+        this.delegate = requireNonNull(delegate, "The delegate must not be null.");
+        this.converter = requireNonNull(converter, "The MessageConverter must not be null.");
+    }
+
+    @Nonnull
+    @Override
+    public <R> CompletableFuture<R> query(@Nonnull Object query, @Nonnull Class<R> responseType,
+                                          @Nullable ProcessingContext context) {
+        return delegate.query(query, responseType, context);
+    }
+
+    @Nonnull
+    @Override
+    public <R> CompletableFuture<List<R>> queryMany(@Nonnull Object query, @Nonnull Class<R> responseType,
+                                                    @Nullable ProcessingContext context) {
+        return null;
+    }
+
+    @Nonnull
+    @Override
+    public <R> Publisher<R> streamingQuery(@Nonnull Object query, @Nonnull Class<R> responseType,
+                                           @Nullable ProcessingContext context) {
+        return null;
+    }
+
+    @Nonnull
+    @Override
+    public <I, U> SubscriptionQueryResponse<I, U> subscriptionQuery(@Nonnull Object query,
+                                                                    @Nonnull Class<I> initialResponseType,
+                                                                    @Nonnull Class<U> updateResponseType,
+                                                                    @Nullable ProcessingContext context,
+                                                                    int updateBufferSize) {
+        return null;
+    }
+
+    @Override
+    public void describeTo(@Nonnull ComponentDescriptor descriptor) {
+
+    }
+}
