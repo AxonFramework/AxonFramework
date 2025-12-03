4a8acf6f95390be6bdbbaf01171fb82610698bfe
Mateusz Nowak <mateusz@nakodach.pl>
2025-12-03 10:22:12 +0100

    ✅ test: use `default` AxonServer context as production application

--- examples/university-demo-OLD/src/test/java/io/axoniq/demo/university/UniversityApplicationTest.java
+++ examples/university-demo-OLD/src/test/java/io/axoniq/demo/university/UniversityApplicationTest.java
@@ -35,7 +35,7 @@ public abstract class UniversityApplicationTest {
         boolean axonServerEnabled = configuration.axonServerEnabled();
         if (axonServerEnabled) {
             try {
-                AxonServerContainerUtils.purgeEventsFromAxonServer("localhost", 8024, "university", true);
+                AxonServerContainerUtils.purgeEventsFromAxonServer("localhost", 8024, "default", true);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
