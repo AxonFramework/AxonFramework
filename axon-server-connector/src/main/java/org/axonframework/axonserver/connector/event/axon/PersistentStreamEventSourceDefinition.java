c552719da7151171aa336f19c1d8b8a265c826db
Mateusz Nowak <mateusz@nakodach.pl>
2025-12-18 14:17:14 +0100

    [#3520] feat(axon-server-connector): add PersistentStreamEventSource: definition,factory,builder

---
+++ axon-server-connector/src/main/java/org/axonframework/axonserver/connector/event/axon/PersistentStreamEventSourceDefinition.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2010-2025. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.axonframework.axonserver.connector.event.axon;
+
+import io.axoniq.axonserver.connector.event.PersistentStream;
+import io.axoniq.axonserver.connector.event.PersistentStreamProperties;
+import org.axonframework.common.configuration.Configuration;
+import org.axonframework.messaging.core.SubscribableEventSource;
+import org.axonframework.messaging.eventhandling.configuration.SubscribableEventSourceDefinition;
+
+import java.util.Objects;
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ * Definition of a {@link PersistentStreamMessageSource}.
+ * <p>
+ * Used to create {@link PersistentStreamMessageSource} instances with a specific Axon
+ * {@link Configuration configuration}. This definition encapsulates all the configuration parameters needed
+ * to create a persistent stream message source, allowing for deferred creation when the configuration is available.
+ * <p>
+ * This class is typically used in conjunction with Spring Boot auto-configuration or programmatic configuration
+ * to define persistent streams that will be instantiated when the application context is fully initialized.
+ *
+ * @author Marc Gathier
+ * @author Mateusz Nowak
+ * @since 4.10.0
+ */
+public class PersistentStreamEventSourceDefinition implements SubscribableEventSourceDefinition {
+
+    private final String name;
+    private final PersistentStreamProperties persistentStreamProperties;
+    private final ScheduledExecutorService scheduler;
+    private final int batchSize;
+    private final String context;
+    private final PersistentStreamMessageSourceFactory messageSourceFactory;
+
+    /**
+     * Instantiates a {@code PersistentStreamEventSourceDefinition} instance based on the given parameters.
+     *
+     * @param name                       The name of the persistent stream. It's a unique identifier of the
+     *                                   {@link PersistentStream} connection with Axon Server. Usage of the same name
+     *                                   will overwrite the existing connection.
+     * @param persistentStreamProperties The properties to create the persistent stream.
+     * @param scheduler                  Scheduler used for persistent stream operations.
+     * @param batchSize                  The batch size for collecting events.
+     * @param context                    The context in which this persistent stream exists (or needs to be created).
+     *                                   May be {@code null}.
+     * @param messageSourceFactory       The factory used to create the {@link PersistentStreamMessageSource}.
+     */
+    public PersistentStreamEventSourceDefinition(String name,
+                                                 PersistentStreamProperties persistentStreamProperties,
+                                                 ScheduledExecutorService scheduler,
+                                                 int batchSize,
+                                                 String context,
+                                                 PersistentStreamMessageSourceFactory messageSourceFactory) {
+        this.name = Objects.requireNonNull(name, "name must not be null");
+        this.persistentStreamProperties = Objects.requireNonNull(persistentStreamProperties,
+                                                                  "persistentStreamProperties must not be null");
+        this.scheduler = Objects.requireNonNull(scheduler, "scheduler must not be null");
+        this.batchSize = batchSize;
+        this.context = context;
+        this.messageSourceFactory = Objects.requireNonNull(messageSourceFactory, "messageSourceFactory must not be null");
+    }
+
+    @Override
+    public SubscribableEventSource create(Configuration configuration) {
+        return messageSourceFactory.build(name,
+                                          persistentStreamProperties,
+                                          scheduler,
+                                          batchSize,
+                                          context,
+                                          configuration);
+    }
+
+    /**
+     * Returns the name of this persistent stream definition.
+     *
+     * @return The name of the persistent stream.
+     */
+    public String getName() {
+        return name;
+    }
+}
