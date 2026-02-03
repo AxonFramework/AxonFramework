59dea73e872075c1a5ab53b2256e9f73504a90ec
Steven van Beelen <steven.vanbeelen@axoniq.io>
2026-02-03 12:30:54 +0100

    Adjust resolver definition to no longer use the Message RoutingKey
    Adjust resolver definition to no longer use the Message RoutingKey.
    Instead, we rely on the @EntityMember(routingKey = "[...]") attribute
    entirely
    
    #3642

--- modelling/src/main/java/org/axonframework/modelling/entity/annotation/RoutingKeyCommandTargetResolverDefinition.java
+++ modelling/src/main/java/org/axonframework/modelling/entity/annotation/RoutingKeyCommandTargetResolverDefinition.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2025. Axon Framework
+ * Copyright (c) 2010-2026. Axon Framework
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package org.axonframework.modelling.entity.annotation;
 
 import jakarta.annotation.Nonnull;
-import org.axonframework.messaging.commandhandling.annotation.RoutingKey;
 import org.axonframework.common.AxonConfigurationException;
 import org.axonframework.modelling.entity.child.CommandTargetResolver;
 
@@ -29,14 +28,12 @@ import static java.lang.String.format;
 import static org.axonframework.common.ReflectionUtils.getMemberValueType;
 
 /**
- * Definition for creating {@link CommandTargetResolver} instances based on the routing key definitions.
+ * Definition for creating {@link CommandTargetResolver} instances based on the
+ * {@link EntityMember#routingKey routing key attribute}.
  * <p>
- * The routing key of the message is determined by the {@link EntityMember#routingKey} annotation on the declaring
- * member in the parent entity, or the {@link RoutingKey} annotation on the child entity's member if absent. The routing
- * key of the child entity is determined by the {@link RoutingKey} on a member of the child entity class.
- * <p>
- * The routing key of the message and of the entity are then matched to determine if a child entity should handle a
- * given message.
+ * The routing key of <b>both</b> the message and entity is determined by the {@link EntityMember#routingKey attribute}
+ * on the declaring member in the parent entity. The routing key of the message and of the entity are then matched to
+ * determine if a child entity should handle a given message.
  *
  * @author Mitchell Herrijgers
  * @see RoutingKeyEventTargetMatcher
@@ -44,31 +41,25 @@ import static org.axonframework.common.ReflectionUtils.getMemberValueType;
  */
 public class RoutingKeyCommandTargetResolverDefinition implements CommandTargetResolverDefinition {
 
-
     @Nonnull
     @Override
-    public <E> CommandTargetResolver<E> createCommandTargetResolver(
-            @Nonnull AnnotatedEntityMetamodel<E> metamodel,
-            @Nonnull Member member) {
-        Optional<String> messageRoutingField = RoutingKeyUtils.getMessageRoutingKey((AnnotatedElement) member);
-        Optional<String> entityRoutingField = RoutingKeyUtils.getEntityRoutingKey(metamodel.entityType());
-        if (messageRoutingField.isPresent() && entityRoutingField.isPresent()) {
-            return new RoutingKeyCommandTargetResolver<>(metamodel,
-                                                         entityRoutingField.get(),
-                                                         messageRoutingField.get());
+    public <E> CommandTargetResolver<E> createCommandTargetResolver(@Nonnull AnnotatedEntityMetamodel<E> entity,
+                                                                    @Nonnull Member member) {
+        Optional<String> optionalRoutingKey = RoutingKeyUtils.getMessageRoutingKey((AnnotatedElement) member);
+        if (optionalRoutingKey.isPresent()) {
+            String routingKey = optionalRoutingKey.get();
+            return new RoutingKeyCommandTargetResolver<>(entity, routingKey, routingKey);
         }
-        if (entityRoutingField.isPresent()) {
-            // Default the message routing key to the entity routing key if it is not explicitly defined.
-            return new RoutingKeyCommandTargetResolver<>(metamodel, entityRoutingField.get(), entityRoutingField.get());
-        }
-        // Only a message routing key (or none at all) is not enough to create a matcher.
+
+        // No routing key found, perhaps we are dealing with a single occurrence entity member.
         Class<?> memberValueType = getMemberValueType(member);
         if (Iterable.class.isAssignableFrom(memberValueType)) {
             throw new AxonConfigurationException(
-                    format("Member [%s] of type [%s] is a collection type, but the child does not define a @RoutingKey. "
-                                   + "Please implement a custom CommandTargetResolver for this collection type or add @RoutingKey to a field or method to identify the child entity correctly.",
-                           member,
-                           memberValueType));
+                    format("Member [%s] of type [%s] is a collection type, but the child does not define a routing key. "
+                                   + "Please set the \"routingKey\" property on the @EntityMember annotation "
+                                   + "or implement a custom CommandTargetResolver for this collection type.",
+                           member, memberValueType)
+            );
         }
         // If the member is not a collection type, we can assume it is a single entity.
         // This does not require an explicit @RoutingKey, as there might only be one in the aggregate.
