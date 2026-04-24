fb1973a26273c3a150a9aa7b5e4f1084c4c0afe3
Jan Galinski <jan.galinski@holisticon.de>
2026-04-24 09:42:48 +0200

    feat(3879): remove pure-functional command handler from this PR
    - might be introduced later

--- examples/university-demo-kotlin/src/test/kotlin/faculty/write/SubscribeStudentToCourseTest.kt
+++ examples/university-demo-kotlin/src/test/kotlin/faculty/write/SubscribeStudentToCourseTest.kt
@@ -17,18 +17,13 @@
 package org.axonframework.examples.university.faculty.write
 
 import org.axonframework.examples.university.TestFixtures
-import org.axonframework.examples.university.UniversityKotlinApplication
-import org.axonframework.examples.university.faculty.events.CourseCreated
-import org.axonframework.examples.university.faculty.events.StudentEnrolledInFaculty
 import org.axonframework.examples.university.faculty.events.StudentSubscribedToCourse
 import org.axonframework.examples.university.faculty.write.create_course.CreateCourse
 import org.axonframework.examples.university.faculty.write.create_course.registerCreateCourse
-import org.axonframework.examples.university.faculty.write.create_course_functional.registerCreateCourseFunctional
 import org.axonframework.examples.university.faculty.write.enroll_student.EnrollStudent
 import org.axonframework.examples.university.faculty.write.enroll_student.registerEnrollStudent
 import org.axonframework.examples.university.faculty.write.subscribe_student.SubscribeStudentToCourse
 import org.axonframework.examples.university.faculty.write.subscribe_student.registerSubscribeStudentToCourse
-import org.axonframework.examples.university.faculty.write.subscribe_student_polymorph.registerSubscribeStudentToCoursePolymorph
 import org.axonframework.examples.university.shared.ids.CourseId
 import org.axonframework.examples.university.shared.ids.StudentId
 import org.axonframework.extension.kotlin.test.exception
@@ -37,9 +32,6 @@ import org.axonframework.test.extension.AxonFrameworkExtension
 import org.axonframework.test.extension.AxonTestFixtureProvider
 import org.axonframework.test.extension.ProvidedAxonTestFixture
 import org.axonframework.test.fixture.AxonTestFixture
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
